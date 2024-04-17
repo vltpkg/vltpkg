88affbfc7937662bc9ca1d8cd12da9a7a6a7629b
isaacs <i@izs.me>
2024-04-16 22:29:52 -0700

    tar: benchmark vs pacote

---
+++ src/tar/scripts/benchmark.js
@@ -0,0 +1,54 @@
+// run the download-packages.js script before this one
+
+import { mkdirSync, readdirSync, readFileSync, rmSync } from 'fs'
+import pacote from 'pacote'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { Pool } from '../dist/esm/pool.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const artifactDir = resolve(__dirname, 'artifacts')
+const targetDir = resolve(__dirname, 'extract')
+
+const artifacts = readdirSync(artifactDir)
+  .sort(() => Math.random() - 0.5)
+  .slice(0, 1000)
+
+const reset = () => {
+  console.time('reset')
+  try {
+    rmSync(targetDir, { recursive: true })
+  } catch {}
+  mkdirSync(targetDir, { recursive: true })
+  console.timeEnd('reset')
+}
+
+reset()
+console.error(`extracting ${artifacts.length} artifacts with pacote`)
+console.time('pacote extract')
+const pacotePromises = []
+for (const a of artifacts) {
+  const tgz = resolve(artifactDir, a)
+  const target = resolve(targetDir, a.replace(/\.tgz$/, ''))
+  pacotePromises.push(pacote.extract(tgz, target))
+}
+await Promise.all(pacotePromises)
+console.timeEnd('pacote extract')
+
+reset()
+console.error(
+  `extracting ${artifacts.length} artifacts with @vltpkg/tar`,
+)
+console.time('@vltpkg/tar extract')
+const p = new Pool()
+const vltPromises = []
+for (const a of artifacts) {
+  const tgz = resolve(artifactDir, a)
+  const target = resolve(targetDir, a.replace(/\.tgz$/, ''))
+  vltPromises.push(p.unpack(readFileSync(tgz), target))
+}
+await Promise.all(vltPromises)
+console.timeEnd('@vltpkg/tar extract')
+
+reset()
