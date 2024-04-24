1b8ed4be628699df3ab389805e15bcbba776845f
isaacs <i@izs.me>
2024-04-24 10:00:18 -0700

    tar: handle root dirs other than 'package/'

---
+++ src/tar/test/find-tar-dir.ts
@@ -0,0 +1,11 @@
+import t from 'tap'
+import { findTarDir } from '../src/find-tar-dir.js'
+
+t.equal(findTarDir('x', undefined), undefined)
+t.equal(findTarDir('x/', undefined), 'x/')
+t.equal(findTarDir('x/', 'y/'), 'y/')
+t.equal(findTarDir('../x/', undefined), undefined)
+t.equal(findTarDir('/x/', undefined), undefined)
+t.equal(findTarDir(undefined, undefined), undefined)
+t.equal(findTarDir('./x/', undefined), './x/')
+t.equal(findTarDir('./x/', './x/'), './x/')
