fc9e8f3a5723cedad2cfd1bce59b58f48c3cfde2
isaacs <i@izs.me>
2024-05-07 20:39:13 -0700

    add @vltpkg/promise-spawn

---
+++ src/promise-spawn/README.md
@@ -0,0 +1,123 @@
+# `@vltpkg/promise-spawn`
+
+Spawn a process and return a promise that resolves when the
+process closes. Fork of
+[`@npmcli/promise-spawn`](http://npm.im/@npmcli/promise-spawn)
+
+## Differences from `@npmcli/promise-spawn`
+
+- A `SpawnPromise(cmd, args, options)` class is added that
+  handles most of the functionality.
+- `promiseSpawn.open()` is removed
+- When run as root, it just runs the command as root, it doesn't
+  try to infer the uid/gid based on the owner of the cwd..
+- No special handling for `shell: true` processes, and thus, no
+  escaping of arguments in that case. (It's just passed through
+  to Node's `spawn()` method.)
+- Fully type-aware, even down to inferring the presence and type
+  of `stdout` and `stderr` properties, as well as the properties
+  added via the optional `extra` argument.
+
+## USAGE
+
+```js
+import { promiseSpawn, SpawnPromise } from '@vltpkg/promise-spawn'
+
+promiseSpawn(
+  'ls',
+  ['-laF', 'some/dir/*.js'],
+  {
+    cwd: '/tmp/some/path', // defaults to process.cwd()
+    stdioString: true, // stdout/stderr as strings rather than buffers
+    stdio: 'pipe', // any node spawn stdio arg is valid here
+    // any other arguments to node child_process.spawn can go here as well,
+  },
+  {
+    extra: 'things',
+    to: 'decorate',
+    the: 'result',
+  },
+)
+  .then(result => {
+    // {status === 0, signal === null, stdout, stderr, and all the extras}
+    console.log('ok!', result)
+  })
+  .catch(er => {
+    // er has all the same properties as the result, set appropriately
+    console.error('failed!', er)
+  })
+```
+
+## API
+
+### `promiseSpawn(cmd, args, opts, extra)` -> `Promise`
+
+Run the command, return a Promise that resolves/rejects based on the
+process result.
+
+Result or error will be decorated with the properties in the `extra`
+object. You can use this to attach some helpful info about _why_ the
+command is being run, if it makes sense for your use case.
+
+If `stdio` is set to anything other than `'inherit'`, then the result/error
+will be decorated with `stdout` and `stderr` values. If `stdioString` is
+set to `true`, these will be strings. Otherwise they will be Buffer
+objects.
+
+Returned promise is decorated with the `stdin` stream if the process is set
+to pipe from `stdin`. Writing to this stream writes to the `stdin` of the
+spawned process.
+
+#### Options
+
+- `stdioString` Boolean, default `true`. Return stdout/stderr
+  output as trimmed strings rather than buffers.
+- `acceptFail` Boolean, default `false`. If true, then a process
+  that closes with `status` other than 0, or `signal` other than
+  `null`, will reject the promise. If true, then failure exits
+  resolve the promise normally. This is useful when you need to
+  run a process where an exit status of `>0` is informative, to
+  avoid creating an Error object for it.
+- Any other options for `child_process.spawn` can be passed as
+  well.
+
+## TypeScript Inference Caveat (and workaround)
+
+If you provide a complex stdio option like `['pipe', 'inherit']`,
+then this will of course mean that `stdin` is set to a writable
+stream, `stderr` is set to a readable stream (because that's the
+default), but `stdout` is set to `null`.
+
+The types will accurately infer this from the type of the
+argument. However, observe this incorrect result:
+
+```
+const result = await promiseSpawn(cmd, args, {
+  stdio: ['pipe', 'inherit'],
+})
+result.stdout
+//     ^? string <-- WRONG
+result.stderr
+//     ^? string
+```
+
+TS will infer the `options.stdio` property to be `('pipe' |
+'inherit')[]`. Since the second item of such an array _might_ be
+set to `'pipe'` at some point, TS will infer the return value to
+include `{ stdout: string }`.
+
+To get around this, typecast the field to its literal value. This
+is a bit noisy, but works fine:
+
+```js
+const result = await promiseSpawn(cmd, args, {
+  stdio: ['pipe', 'inherit'] as ['pipe', 'inherit'],
+})
+result.stdout
+//     ^? null <-- CORRECT!
+result.stderr
+//     ^? string
+```
+
+When given a single argument to apply to all `stdio` fields, this
+inference happens correctly by default.
---
+++ src/promise-spawn/test/index.ts
@@ -0,0 +1,573 @@
+import cp from 'child_process'
+import spawk from 'spawk'
+import t from 'tap'
+const mockCP = { ...cp }
+// make spawk play nice with ESM
+const { promiseSpawn } = await t.mockImport<
+  typeof import('../src/index.js')
+>('../src/index.js', {
+  child_process: mockCP,
+  spawk: await t.mockImport('spawk', { child_process: mockCP }),
+})
+
+spawk.preventUnmatched()
+t.afterEach(() => {
+  spawk.clean()
+})
+
+t.test('defaults to returning strings', async t => {
+  const proc = spawk.spawn('pass', [], {}).stdout(Buffer.from('OK\n'))
+
+  const result = await promiseSpawn('pass', [])
+  t.hasStrict(result, {
+    status: 0,
+    signal: null,
+    stdout: 'OK',
+    stderr: '',
+  })
+
+  //@ts-expect-error - no extra props added
+  result.extra
+  //@ts-expect-error - it's a string by default
+  result.stdout.byteLength
+  // not null, no error here
+  result.stdout.length
+  //@ts-expect-error - it's a string by default
+  result.stderr.byteLength
+  // not null, no error here
+  result.stderr.length
+
+  t.ok(proc.called)
+})
+
+t.test('extra context is returned', async t => {
+  const proc = spawk.spawn('pass', [], {}).stdout(Buffer.from('OK\n'))
+
+  const result = await promiseSpawn(
+    'pass',
+    [],
+    {},
+    { extra: 'property' },
+  )
+  t.hasStrict(result, {
+    status: 0,
+    signal: null,
+    stdout: 'OK',
+    stderr: '',
+    extra: 'property',
+  })
+
+  // no error, property infered by TS
+  result.extra = 'hello'
+  //@ts-expect-error - only the listed properties added
+  result.somethingElse = 'whatever'
+  //@ts-expect-error - it's a string by default
+  result.stdout.byteLength
+  // not null, no error here
+  result.stdout.length
+  //@ts-expect-error - it's a string by default
+  result.stderr.byteLength
+  // not null, no error here
+  result.stderr.length
+
+  t.ok(proc.called)
+})
+
+t.test('stdioString false returns buffers', async t => {
+  const proc = spawk.spawn('pass', [], {}).stdout(Buffer.from('OK\n'))
+
+  const result = await promiseSpawn('pass', [], {
+    stdioString: false,
+  })
+  t.hasStrict(result, {
+    status: 0,
+    signal: null,
+    stdout: Buffer.from('OK\n'),
+    stderr: Buffer.from(''),
+  })
+
+  //@ts-expect-error - no props added
+  result.extra = 'hello'
+  // inferred as Buffer
+  result.stdout.byteLength
+  // not null, no error here
+  result.stdout.length
+  // inferred as Buffer
+  result.stderr.byteLength
+  // not null, no error here
+  result.stderr.length
+
+  t.ok(proc.called)
+})
+
+t.test(
+  'stdout and stderr are null when stdio is inherit',
+  async t => {
+    const proc = spawk
+      .spawn('pass', [], { stdio: 'inherit' })
+      .stdout(Buffer.from('OK\n'))
+
+    const result = await promiseSpawn('pass', [], {
+      stdio: 'inherit',
+    })
+    t.hasStrict(result, {
+      status: 0,
+      signal: null,
+      stdout: null,
+      stderr: null,
+    })
+    //@ts-expect-error - inferred as null
+    result.stdout = 'hello'
+    //@ts-expect-error - inferred as null
+    result.stderr = 'hello'
+
+    t.ok(proc.called)
+  },
+)
+
+t.test(
+  'stdout and stderr are null when stdio is inherit and stdioString is false',
+  async t => {
+    const proc = spawk
+      .spawn('pass', [], { stdio: 'inherit' })
+      .stdout(Buffer.from('OK\n'))
+
+    const result = await promiseSpawn('pass', [], {
+      stdio: 'inherit',
+      stdioString: false,
+    })
+    t.hasStrict(result, {
+      status: 0,
+      signal: null,
+      stdout: null,
+      stderr: null,
+    })
+
+    //@ts-expect-error - inferred as null
+    result.stdout = 'hello'
+    //@ts-expect-error - inferred as null
+    result.stderr = 'hello'
+
+    t.ok(proc.called)
+  },
+)
+
+t.test(
+  'stdout is null when stdio is [pipe, inherit, pipe]',
+  async t => {
+    const proc = spawk
+      .spawn('pass', [], { stdio: ['pipe', 'inherit', 'pipe'] })
+      .stdout(Buffer.from('OK\n'))
+
+    const p = promiseSpawn('pass', [], {
+      stdio: ['pipe', 'inherit', 'pipe'] as [
+        'pipe',
+        'inherit',
+        'pipe',
+      ],
+    })
+    const result = await p
+    t.hasStrict(result, {
+      status: 0,
+      signal: null,
+      stdout: null,
+      stderr: '',
+    })
+
+    const inNotNull: typeof p.stdin extends null ? true : false =
+      false
+    inNotNull
+
+    //@ts-expect-error - inferred as null
+    result.stdout = 'hello'
+    // inferred as string
+    result.stderr = 'hello'
+    t.ok(proc.called)
+  },
+)
+
+t.test(
+  'stderr is null when stdio is [pipe, pipe, inherit]',
+  async t => {
+    const proc = spawk
+      .spawn('pass', [], { stdio: ['pipe', 'pipe', 'inherit'] })
+      .stdout(Buffer.from('OK\n'))
+
+    const p = promiseSpawn('pass', [], {
+      stdio: ['pipe', 'pipe', 'inherit'] as [
+        'pipe',
+        'pipe',
+        'inherit',
+      ],
+    })
+    const result = await p
+    t.hasStrict(result, {
+      status: 0,
+      signal: null,
+      stdout: 'OK',
+      stderr: null,
+    })
+
+    const inNotNull: typeof p.stdin extends null ? true : false =
+      false
+    inNotNull
+
+    //@ts-expect-error - inferred as null
+    result.stderr = 'hello'
+    // inferred as string
+    result.stdout = 'hello'
+
+    t.ok(proc.called)
+  },
+)
+
+t.test('exposes stdin', async t => {
+  const proc = spawk.spawn('stdin', [], {})
+  const p = promiseSpawn('stdin', [])
+  process.nextTick(() => {
+    //@ts-ignore
+    p.process.stdin.pipe(p.process.stdout)
+    p.stdin.end('hello')
+  })
+
+  const result = await p
+  t.hasStrict(result, {
+    status: 0,
+    signal: null,
+    stdout: 'hello',
+    stderr: '',
+  })
+
+  t.ok(proc.called)
+})
+
+t.test('exposes process', async t => {
+  //@ts-ignore
+  const proc = spawk.spawn('proc', [], {}).exitOnSignal('SIGKILL')
+
+  const p = promiseSpawn('proc', [])
+  process.nextTick(() => p.process.kill('SIGKILL'))
+
+  // there are no signals in windows, so we expect a different result
+  if (process.platform === 'win32') {
+    await t.rejects(p, {
+      message: 'command failed',
+      status: 1,
+      signal: null,
+      stdout: '',
+      stderr: '',
+    })
+  } else {
+    await t.rejects(
+      p,
+      Object.assign(new Error('command failed'), {
+        signal: 'SIGKILL',
+        stdout: '',
+        stderr: '',
+      }),
+    )
+  }
+
+  t.ok(proc.called)
+})
+
+t.test('rejects when spawn errors', async t => {
+  const proc = spawk
+    .spawn('notfound', [], {})
+    //@ts-ignore
+    .spawnError(new Error('command not found'))
+
+  await t.rejects(promiseSpawn('notfound', []), {
+    message: 'command not found',
+    stdout: '',
+    stderr: '',
+  })
+
+  t.ok(proc.called)
+})
+
+t.test('spawn error includes extra', async t => {
+  const proc = spawk
+    .spawn('notfound', [], {})
+    //@ts-ignore
+    .spawnError(new Error('command not found'))
+
+  await t.rejects(
+    promiseSpawn('notfound', [], {}, { extra: 'property' }),
+    {
+      message: 'command not found',
+      stdout: '',
+      stderr: '',
+      extra: 'property',
+    },
+  )
+
+  t.ok(proc.called)
+})
+
+t.test('spawn error respects stdioString', async t => {
+  const proc = spawk
+    .spawn('notfound', [], {})
+    //@ts-ignore
+    .spawnError(new Error('command not found'))
+
+  await t.rejects(
+    promiseSpawn('notfound', [], { stdioString: false }),
+    {
+      message: 'command not found',
+      stdout: Buffer.from(''),
+      stderr: Buffer.from(''),
+    },
+  )
+
+  t.ok(proc.called)
+})
+
+t.test('spawn error respects stdio as inherit', async t => {
+  const proc = spawk
+    .spawn('notfound', [], { stdio: 'inherit' })
+    //@ts-ignore
+    .spawnError(new Error('command not found'))
+
+  await t.rejects(
+    promiseSpawn('notfound', [], { stdio: 'inherit' }),
+    {
+      message: 'command not found',
+      stdout: null,
+      stderr: null,
+    },
+  )
+
+  t.ok(proc.called)
+})
+
+t.test('rejects when command fails', async t => {
+  const proc = spawk
+    .spawn('fail', [], {})
+    .stderr(Buffer.from('Error!\n'))
+    .exit(1)
+
+  await t.rejects(promiseSpawn('fail', []), {
+    message: 'command failed',
+    status: 1,
+    stdout: '',
+    stderr: 'Error!',
+  })
+
+  t.ok(proc.called)
+})
+
+t.test('does not rejects when command fails if acceptFail', async t => {
+  const proc = spawk
+    .spawn('fail', [], {})
+    .stderr(Buffer.from('Error!\n'))
+    .exit(1)
+
+  t.strictSame(await promiseSpawn('fail', [], { acceptFail: true }), {
+  cmd: 'fail',
+  args: [],
+  status: 1,
+  signal: null,
+  stdout: '',
+  stderr: 'Error!'
+}
+)
+
+  t.ok(proc.called)
+})
+
+t.test('failed command returns extra', async t => {
+  const proc = spawk
+    .spawn('fail', [], {})
+    .stderr(Buffer.from('Error!\n'))
+    .exit(1)
+
+  await t.rejects(
+    promiseSpawn('fail', [], {}, { extra: 'property' }),
+    {
+      message: 'command failed',
+      status: 1,
+      stdout: '',
+      stderr: 'Error!',
+      extra: 'property',
+    },
+  )
+
+  t.ok(proc.called)
+})
+
+t.test('failed command respects stdioString', async t => {
+  const proc = spawk
+    .spawn('fail', [], {})
+    .stderr(Buffer.from('Error!\n'))
+    .exit(1)
+
+  await t.rejects(promiseSpawn('fail', [], { stdioString: false }), {
+    message: 'command failed',
+    status: 1,
+    stdout: Buffer.from(''),
+    stderr: Buffer.from('Error!\n'),
+  })
+
+  t.ok(proc.called)
+})
+
+t.test('failed command respects stdio as inherit', async t => {
+  const proc = spawk
+    .spawn('fail', [], { stdio: 'inherit' })
+    .stderr(Buffer.from('Error!\n'))
+    .exit(1)
+
+  await t.rejects(promiseSpawn('fail', [], { stdio: 'inherit' }), {
+    message: 'command failed',
+    status: 1,
+    stdout: null,
+    stderr: null,
+  })
+
+  t.ok(proc.called)
+})
+
+t.test('rejects when signal kills child', async t => {
+  const proc = spawk.spawn('signal', [], {}).signal('SIGFAKE')
+
+  const p = promiseSpawn('signal', [])
+  // there are no signals in windows, so we expect a different result
+  if (process.platform === 'win32') {
+    await t.rejects(p, {
+      message: 'command failed',
+      status: 1,
+      signal: null,
+      stdout: '',
+      stderr: '',
+    })
+  } else {
+    await t.rejects(p, {
+      code: null,
+      signal: 'SIGFAKE',
+      stdout: '',
+      stderr: '',
+    })
+  }
+
+  t.ok(proc.called)
+})
+
+t.test('signal death includes extra', async t => {
+  const proc = spawk.spawn('signal', [], {}).signal('SIGFAKE')
+
+  const p = promiseSpawn('signal', [], {}, { extra: 'property' })
+  // there are no signals in windows, so we expect a different result
+  if (process.platform === 'win32') {
+    await t.rejects(p, {
+      message: 'command failed',
+      status: 1,
+      signal: null,
+      stdout: '',
+      stderr: '',
+      extra: 'property',
+    })
+  } else {
+    await t.rejects(p, {
+      code: null,
+      signal: 'SIGFAKE',
+      stdout: '',
+      stderr: '',
+      extra: 'property',
+    })
+  }
+
+  t.ok(proc.called)
+})
+
+t.test('signal death respects stdioString', async t => {
+  const proc = spawk.spawn('signal', [], {}).signal('SIGFAKE')
+
+  const p = promiseSpawn('signal', [], { stdioString: false })
+  // there are no signals in windows, so we expect a different result
+  if (process.platform === 'win32') {
+    await t.rejects(p, {
+      message: 'command failed',
+      status: 1,
+      signal: null,
+      stdout: Buffer.from(''),
+      stderr: Buffer.from(''),
+    })
+  } else {
+    await t.rejects(p, {
+      code: null,
+      signal: 'SIGFAKE',
+      stdout: Buffer.from(''),
+      stderr: Buffer.from(''),
+    })
+  }
+
+  t.ok(proc.called)
+})
+
+t.test('signal death respects stdio as inherit', async t => {
+  const proc = spawk
+    .spawn('signal', [], { stdio: 'inherit' })
+    .signal('SIGFAKE')
+
+  const p = promiseSpawn('signal', [], { stdio: 'inherit' })
+  // there are no signals in windows, so we expect a different result
+  if (process.platform === 'win32') {
+    await t.rejects(p, {
+      message: 'command failed',
+      status: 1,
+      signal: null,
+      stdout: null,
+      stderr: null,
+    })
+  } else {
+    await t.rejects(p, {
+      code: null,
+      signal: 'SIGFAKE',
+      stdout: null,
+      stderr: null,
+    })
+  }
+
+  t.ok(proc.called)
+})
+
+t.test('rejects when stdout errors', async t => {
+  const proc = spawk.spawn('stdout-err', [], {})
+
+  const p = promiseSpawn('stdout-err', [])
+  process.nextTick(() =>
+    p.process.stdout.emit('error', new Error('stdout err')),
+  )
+
+  await t.rejects(
+    p,
+    Object.assign(new Error('stdout err'), {
+      cmd: 'stdout-err',
+      args: [],
+      stdout: '',
+      stderr: '',
+    }),
+  )
+
+  t.ok(proc.called)
+})
+
+t.test('rejects when stderr errors', async t => {
+  const proc = spawk.spawn('stderr-err', [], {})
+
+  const p = promiseSpawn('stderr-err', [])
+  process.nextTick(() =>
+    p.process.stderr.emit('error', new Error('stderr err')),
+  )
+
+  await t.rejects(p, {
+    message: 'stderr err',
+    cmd: 'stderr-err',
+    args: [],
+    stdout: '',
+    stderr: '',
+  })
+
+  t.ok(proc.called)
+})
