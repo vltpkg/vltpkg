6f8b4d4c1a7a18312a91c9c681fda159be05bf48
Darcy Clarke <darcy@darcyclarke.me>
2024-05-13 03:25:07 +0000

    fix: start.sh

--- .devcontainer/codespaceRunner/library-scripts/start.sh
+++ .devcontainer/codespaceRunner/library-scripts/start.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-GH_OWNER=$GITHUB_USER
-GH_REPOSITORY=$GITHUB_REPOSITORY
-GH_TOKEN=$GITHUB_TOKEN
-
 HOSTNAME=$(hostname)
 RUNNER_SUFFIX="runner"
 RUNNER_NAME="${HOSTNAME}-${RUNNER_SUFFIX}"
 USER_NAME_LABEL=$( (git config --get user.name) | sed -e 's/ //g')
-REPO_NAME_LABEL="$GH_REPOSITORY"
+REPO_NAME_LABEL="vltpkg"
 
-# REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-/home/vscode/actions-runner/config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${GH_TOKEN} --name ${RUNNER_NAME}  --labels ${USER_NAME_LABEL},${REPO_NAME_LABEL}
+/home/vscode/actions-runner/config.sh --unattended --url https://github.com/${GITHUB_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}  --labels ${USER_NAME_LABEL},${REPO_NAME_LABEL}
 /home/vscode/actions-runner/run.sh
