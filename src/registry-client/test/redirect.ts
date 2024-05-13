173acde8a4ce2051c7d4c2504a73a3d16eba8604
isaacs <i@izs.me>
2024-05-12 17:47:34 -0700

    registry-client: follow redirects
    - Add redirection behavior, expand to support all 3xx status codes.
    - Update tap to support matching errors on the 'cause' property.
    - Update error-cause to add 'max' and 'min' fields.

---
+++ src/registry-client/test/redirect.ts
@@ -0,0 +1,156 @@
+import t from 'tap'
+import { CacheEntry } from '../src/cache-entry.js'
+import {
+  isRedirect,
+  redirect,
+  RedirectResponse,
+} from '../src/redirect.js'
+
+t.compareOptions = { includeGetters: true }
+
+t.test('isRedirect', t => {
+  t.equal(
+    isRedirect(
+      new CacheEntry(
+        301,
+        [Buffer.from('location'), Buffer.from('somewhere')],
+        '',
+      ),
+    ),
+    true,
+  )
+  t.equal(isRedirect(new CacheEntry(308, [], '')), false)
+  t.equal(
+    isRedirect(
+      new CacheEntry(
+        200,
+        [Buffer.from('location'), Buffer.from('somewhere')],
+        '',
+      ),
+    ),
+    false,
+  )
+
+  t.end()
+})
+
+t.test('redirect', t => {
+  t.throws(
+    () =>
+      redirect(
+        {
+          maxRedirections: 3,
+          redirections: new Set([
+            'https://example.com/a',
+            'https://example.com/b',
+            'https://example.com/c',
+          ]),
+        },
+        new CacheEntry(301, [
+          Buffer.from('location'),
+          Buffer.from('/x'),
+        ]) as RedirectResponse,
+        new URL('https://example.com/c'),
+      ),
+    {
+      message: 'Maximum redirections exceeded',
+      cause: {
+        max: 3,
+        url: URL,
+        found: [
+          'https://example.com/a',
+          'https://example.com/b',
+          'https://example.com/c',
+        ],
+      },
+    },
+  )
+  t.throws(
+    () =>
+      redirect(
+        {
+          maxRedirections: 30,
+          redirections: new Set([
+            'https://example.com/a',
+            'https://example.com/b',
+            'https://example.com/c',
+          ]),
+        },
+        new CacheEntry(301, [
+          Buffer.from('location'),
+          Buffer.from('/a'),
+        ]) as RedirectResponse,
+        new URL('https://example.com/c'),
+      ),
+    {
+      message: 'Redirection cycle detected',
+      cause: {
+        max: 30,
+        url: URL,
+        found: [
+          'https://example.com/a',
+          'https://example.com/b',
+          'https://example.com/c',
+        ],
+      },
+    },
+  )
+
+  t.matchSnapshot(
+    redirect(
+      {
+        maxRedirections: 30,
+        redirections: new Set([
+          'https://example.com/a',
+          'https://example.com/b',
+          'https://example.com/c',
+        ]),
+      },
+      new CacheEntry(308, [
+        Buffer.from('location'),
+        Buffer.from('/x'),
+      ]) as RedirectResponse,
+      new URL('https://example.com/c'),
+    ),
+    'return redirect settings',
+  )
+
+  t.matchSnapshot(
+    redirect(
+      {
+        method: 'POST',
+        body: 'hello',
+        maxRedirections: 30,
+        redirections: new Set([
+          'https://example.com/a',
+          'https://example.com/b',
+          'https://example.com/c',
+        ]),
+      },
+      new CacheEntry(303, [
+        Buffer.from('location'),
+        Buffer.from('/x'),
+      ]) as RedirectResponse,
+      new URL('https://example.com/c'),
+    ),
+    '303, strip body and make it a GET'
+  )
+
+  t.matchSnapshot(
+    redirect(
+      {
+        method: 'POST',
+        body: 'hello',
+        maxRedirections: 0,
+      },
+      new CacheEntry(303, [
+        Buffer.from('location'),
+        Buffer.from('/x'),
+      ]) as RedirectResponse,
+      new URL('https://example.com/c'),
+    ),
+    'no redirections, just return []'
+  )
+
+  t.end()
+})
