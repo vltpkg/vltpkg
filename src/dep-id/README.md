6bea00da24778729ce685656321f2818cb070361
isaacs <i@izs.me>
2024-05-15 15:36:25 -0700

    dep-id module

---
+++ src/dep-id/README.md
@@ -0,0 +1,51 @@
+# `@vltpkg/dep-id`
+
+A library for serializing dependencies into terse string
+identifiers, and turning those serialized identifiers back into
+`Spec` objects.
+
+## USAGE
+
+```js
+import {
+  getId,
+  getTuple,
+  hydrate,
+  hydrateTuple,
+  joinDepIDTuple,
+  splitDepID,
+} from '@vltpkg/dep-id'
+import { manifest } from '@vltpkg/package-info'
+
+{
+  // default registry
+  const spec = Spec.parse('x@latest')
+  const mani = await manifest(spec)
+  const id = getId(spec, mani) // registry;;x@1.2.3
+}
+
+{
+  // not default registry
+  const spec = Spec.parse('x@vlt:y@latest', {
+    registries: { vlt: 'http://vlt.sh' },
+  })
+  const mani = await manifest(spec)
+  const id = getId(spec, mani) // registry;vlt;y@latest
+}
+
+{
+  // git, hosted
+  const spec = Spec.parse('x@github:a/b#branch')
+  const mani = await manifest(spec)
+  const id = getId(spec, mani) // git;github:a/b;branch
+}
+
+// Hydrate by providing a name, and options for the spec creation
+const spec = hydrate('git;github:a/b;branch', 'x') // x@github:a/b#branch
+```
+
+Note: multiple different spec/manifest combinations _can_ result
+in the same identifier. For example, the specifiers
+`x@npm:y@latest` and `asdf@npm:y@1.x` might both ultimately
+resolve to the same package, so they only need to appear in the
+store once.
---
+++ src/dep-id/tap-snapshots/test/index.ts.test.cjs
@@ -0,0 +1,259 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/index.ts > TAP > valid specs > x@1 > hydrated 1`] = `
+x@x@1.2.3
+`
+
+exports[`test/index.ts > TAP > valid specs > x@1 > must match snapshot 1`] = `
+Array [
+  "registry;;x@1.2.3",
+  Array [
+    "registry",
+    "",
+    "x@1.2.3",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > x@1.2.3 > hydrated 1`] = `
+x@x@1.2.3
+`
+
+exports[`test/index.ts > TAP > valid specs > x@1.2.3 > must match snapshot 1`] = `
+Array [
+  "registry;;x@1.2.3",
+  Array [
+    "registry",
+    "",
+    "x@1.2.3",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > x@file://x.tgz > hydrated 1`] = `
+x@file://{CWD}/x.tgz
+`
+
+exports[`test/index.ts > TAP > valid specs > x@file://x.tgz > must match snapshot 1`] = `
+Array [
+  "file;x.tgz",
+  Array [
+    "file",
+    "x.tgz",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > x@git+ssh://host.com/x.git > hydrated 1`] = `
+x@git+ssh://host.com/x.git#
+`
+
+exports[`test/index.ts > TAP > valid specs > x@git+ssh://host.com/x.git > must match snapshot 1`] = `
+Array [
+  "git;git%2Bssh%3A%2F%2Fhost.com%2Fx.git;",
+  Array [
+    "git",
+    "git+ssh://host.com/x.git",
+    "",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > x@git+ssh://host.com/x.git#branch > hydrated 1`] = `
+x@git+ssh://host.com/x.git#branch
+`
+
+exports[`test/index.ts > TAP > valid specs > x@git+ssh://host.com/x.git#branch > must match snapshot 1`] = `
+Array [
+  "git;git%2Bssh%3A%2F%2Fhost.com%2Fx.git;branch",
+  Array [
+    "git",
+    "git+ssh://host.com/x.git",
+    "branch",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > x@git+ssh://host.com/x.git#semver:1 > hydrated 1`] = `
+x@git+ssh://host.com/x.git#semver:1
+`
+
+exports[`test/index.ts > TAP > valid specs > x@git+ssh://host.com/x.git#semver:1 > must match snapshot 1`] = `
+Array [
+  "git;git%2Bssh%3A%2F%2Fhost.com%2Fx.git;semver%3A1",
+  Array [
+    "git",
+    "git+ssh://host.com/x.git",
+    "semver:1",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > x@github:a/b > hydrated 1`] = `
+x@github:a/b#
+`
+
+exports[`test/index.ts > TAP > valid specs > x@github:a/b > must match snapshot 1`] = `
+Array [
+  "git;github%3Aa%2Fb;",
+  Array [
+    "git",
+    "github:a/b",
+    "",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > x@github:a/b#branch > hydrated 1`] = `
+x@github:a/b#branch
+`
+
+exports[`test/index.ts > TAP > valid specs > x@github:a/b#branch > must match snapshot 1`] = `
+Array [
+  "git;github%3Aa%2Fb;branch",
+  Array [
+    "git",
+    "github:a/b",
+    "branch",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > x@github:a/b#semver:1 > hydrated 1`] = `
+x@github:a/b#semver:1
+`
+
+exports[`test/index.ts > TAP > valid specs > x@github:a/b#semver:1 > must match snapshot 1`] = `
+Array [
+  "git;github%3Aa%2Fb;semver%3A1",
+  Array [
+    "git",
+    "github:a/b",
+    "semver:1",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > x@https://x.com/x.tgz > hydrated 1`] = `
+x@https://x.com/x.tgz
+`
+
+exports[`test/index.ts > TAP > valid specs > x@https://x.com/x.tgz > must match snapshot 1`] = `
+Array [
+  "remote;https%3A%2F%2Fx.com%2Fx.tgz",
+  Array [
+    "remote",
+    "https://x.com/x.tgz",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > x@workspace:* > hydrated 1`] = `
+*@workspace:x
+`
+
+exports[`test/index.ts > TAP > valid specs > x@workspace:* > must match snapshot 1`] = `
+Array [
+  "workspace;x;*",
+  Array [
+    "workspace",
+    "x",
+    "*",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > y@npm:x@1 > hydrated 1`] = `
+x@npm:x@1.2.3
+`
+
+exports[`test/index.ts > TAP > valid specs > y@npm:x@1 > must match snapshot 1`] = `
+Array [
+  "registry;npm;x@1.2.3",
+  Array [
+    "registry",
+    "npm",
+    "x@1.2.3",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > y@npm:x@1.2.3 > hydrated 1`] = `
+x@npm:x@1.2.3
+`
+
+exports[`test/index.ts > TAP > valid specs > y@npm:x@1.2.3 > must match snapshot 1`] = `
+Array [
+  "registry;npm;x@1.2.3",
+  Array [
+    "registry",
+    "npm",
+    "x@1.2.3",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > y@npm:x@github:a/b#branch > hydrated 1`] = `
+x@github:a/b#branch
+`
+
+exports[`test/index.ts > TAP > valid specs > y@npm:x@github:a/b#branch > must match snapshot 1`] = `
+Array [
+  "git;github%3Aa%2Fb;branch",
+  Array [
+    "git",
+    "github:a/b",
+    "branch",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > y@registry:https://registry.npmjs.org/#x@1 > hydrated 1`] = `
+x@x@1.2.3
+`
+
+exports[`test/index.ts > TAP > valid specs > y@registry:https://registry.npmjs.org/#x@1 > must match snapshot 1`] = `
+Array [
+  "registry;;x@1.2.3",
+  Array [
+    "registry",
+    "",
+    "x@1.2.3",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > y@registry:https://registry.npmjs.org#x@1 > hydrated 1`] = `
+x@x@1.2.3
+`
+
+exports[`test/index.ts > TAP > valid specs > y@registry:https://registry.npmjs.org#x@1 > must match snapshot 1`] = `
+Array [
+  "registry;;x@1.2.3",
+  Array [
+    "registry",
+    "",
+    "x@1.2.3",
+  ],
+]
+`
+
+exports[`test/index.ts > TAP > valid specs > y@registry:https://x.com#x@1 > hydrated 1`] = `
+x@registry:https://x.com/#x@1.2.3
+`
+
+exports[`test/index.ts > TAP > valid specs > y@registry:https://x.com#x@1 > must match snapshot 1`] = `
+Array [
+  "registry;https%3A%2F%2Fx.com%2F;x@1.2.3",
+  Array [
+    "registry",
+    "https://x.com/",
+    "x@1.2.3",
+  ],
+]
+`
