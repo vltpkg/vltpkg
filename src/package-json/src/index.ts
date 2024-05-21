0b95f311e137ac4da702f60870583a8fa41418ec
Ruy Adorno <ruy@vlt.sh>
2024-05-21 09:38:59 -0400

    package-json: simplify module
    - Renamed internal `this.cache` variable
    - Use a single try/catch block for both read and parse operations
    
    PR-URL: https://github.com/vltpkg/vltpkg/pull/35
    Credit: @ruyadorno
    Close: #35
    Reviewed-by: @isaacs

--- src/package-json/src/index.ts
+++ src/package-json/src/index.ts
@@ -8,36 +8,26 @@ export class PackageJson {
   /**
    * Reads and parses contents of a `package.json` file at a directory `dir`.
    */
-  localPackageJsonCache: Map<string, Manifest> = new Map()
+  cache: Map<string, Manifest> = new Map()
 
   read(dir: string): Manifest {
-    const cachedPackageJson = this.localPackageJsonCache.get(dir)
+    const cachedPackageJson = this.cache.get(dir)
     if (cachedPackageJson) {
       return cachedPackageJson
     }
 
     const filename = resolve(dir, 'package.json')
-    let content: string = ''
     try {
-      content = readFileSync(filename, { encoding: 'utf8' })
+      const res: Manifest = asManifest(
+        parse(readFileSync(filename, { encoding: 'utf8' })),
+      )
+      this.cache.set(dir, res)
+      return res
     } catch (err) {
       throw error('Could not read package.json file', {
         path: filename,
         cause: err as Error,
       })
     }
-
-    let res: Manifest
-    try {
-      res = asManifest(parse(content))
-    } catch (err) {
-      throw error('Invalid package.json file', {
-        path: filename,
-        cause: err as Error,
-      })
-    }
-
-    this.localPackageJsonCache.set(dir, res)
-    return res
   }
 }
