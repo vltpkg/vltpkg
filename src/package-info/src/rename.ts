e52cfebf9e12930c0e2f1c280c9be18eddb232c9
isaacs <i@izs.me>
2024-05-23 08:42:37 -0700

    package-info: handle windows dir rename EPERM

---
+++ src/package-info/src/rename.ts
@@ -0,0 +1,40 @@
+/**
+ * On posix systems, rename is atomic and will clobber anything in its way
+ * However, on Windows, it can fail with the rather unhelpful EPERM error if
+ * the target directory is not removed in time or is currently in use.
+ *
+ * While true atomic semantics is not available on Windows in this case, we can
+ * at least implement the posix overwrite semantics by explicitly removing the
+ * target when this error occurs.
+ *
+ * This is only relevant when renaming *directories*, since files will
+ * generally not raise problems. When/if we rename directories outside of
+ * package-info, this can be moved to its own shared module.
+ *
+ * @module
+ */
+const { platform } = process
+import { PathLike } from 'node:fs'
+import { rename as fsRename, rm } from 'node:fs/promises'
+export const rename =
+  platform !== 'win32' ? fsRename : (
+    async function (
+      oldPath: PathLike,
+      newPath: PathLike,
+    ): Promise<void> {
+      let retries = 3
+      const retry = async (er: unknown): Promise<void> => {
+        if (
+          retries > 0 &&
+          (er as NodeJS.ErrnoException)?.code === 'EPERM'
+        ) {
+          retries --
+          await rm(newPath, { recursive: true, force: true })
+          return fsRename(oldPath, newPath).then(() => {}, retry)
+        } else {
+          throw er
+        }
+      }
+      return fsRename(oldPath, newPath).then(() => {}, retry)
+    }
+  )
---
+++ src/package-info/test/rename.ts
@@ -0,0 +1,69 @@
+import t from 'tap'
+import * as FSP from 'node:fs/promises'
+import {PathLike, RmOptions} from 'node:fs'
+const { rename: fsRename } = FSP
+
+t.test('posix', async t => {
+  t.intercept(process, 'platform', { value: 'posix' })
+  const { rename } = await t.mockImport<typeof import('../src/rename.js')>('../src/rename.js')
+  t.equal(rename, fsRename, 'use node rename on posix')
+})
+
+t.test('win32, retry works', async t => {
+  t.intercept(process, 'platform', { value: 'win32' })
+  let calledRm = false
+  const { rename } = await t.mockImport<typeof import('../src/rename.js')>('../src/rename.js', {
+    'node:fs/promises': t.createMock(FSP, {
+      rm: async (target: PathLike, options?: RmOptions) => {
+        calledRm = true
+        return FSP.rm(target, options)
+      },
+      rename: async (oldPath: PathLike, newPath: PathLike) => {
+        if (!calledRm) throw Object.assign(new Error('x'), { code: 'EPERM' })
+        return FSP.rename(oldPath, newPath)
+      }
+    })
+  })
+  t.not(rename, fsRename, 'use custom rename on windows')
+  const dir = t.testdir({
+    oldPath: {
+      file: 'hello'
+    },
+    newPath: {
+      otherFile: 'goodbye'
+    },
+  })
+
+  await rename(`${dir}/oldPath`, `${dir}/newPath`)
+
+  t.equal(calledRm, true)
+})
+
+t.test('win32, retry fails', async t => {
+  t.intercept(process, 'platform', { value: 'win32' })
+  let calledRm = 0
+  const { rename } = await t.mockImport<typeof import('../src/rename.js')>('../src/rename.js', {
+    'node:fs/promises': t.createMock(FSP, {
+      rm: async (target: PathLike, options?: RmOptions) => {
+        calledRm++
+        return FSP.rm(target, options)
+      },
+      rename: async () => {
+        throw Object.assign(new Error('x'), { code: 'EPERM' })
+      }
+    })
+  })
+  t.not(rename, fsRename, 'use custom rename on windows')
+  const dir = t.testdir({
+    oldPath: {
+      file: 'hello'
+    },
+    newPath: {
+      otherFile: 'goodbye'
+    },
+  })
+
+  await t.rejects( rename(`${dir}/oldPath`, `${dir}/newPath`), { code: 'EPERM' })
+
+  t.equal(calledRm, 3, 'tried 3 times, then gave up')
+})
