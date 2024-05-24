8d16741465f754e3a62e0d925b4fc0d772e74b97
isaacs <i@izs.me>
2024-05-24 10:30:03 -0700

    Add @vltpkg/workspace module

---
+++ src/workspaces/README.md
@@ -0,0 +1,110 @@
+# `@vltpkg/workspaces`
+
+Utility for working with vlt workspaces.
+
+## USAGE
+
+```js
+import { Monorepo, Workspace } from '@vltpkg/workspaces'
+
+const m = await Monorepo.load()
+// Can also provide a path option, defaults to process.cwd()
+// m = await Monorepo.load(someOtherPath)
+// Constructor can be used directly, but less convenient:
+//   const m = new Monorepo()
+//   await m.load()
+
+const appWS = m.get('@acme/app')
+console.log(appWS.path) // packages/apps/app
+console.log(appWS.groups) // ['apps', 'frontend']
+console.log(await appWS.manifest()) // { name: "@acme/app", version: etc }
+console.log(appWS.inGroup('frontend')) // true
+console.log(appWS.inGroup('datalayer')) // false
+assert.equal(m.get('packages/apps/app'), appWS) // can get by path
+
+for (const workspace of m) {
+  // iterate over the workspaces in topological dependency order
+}
+
+// run a script or some other async operation, in topological
+// dependency graph order. Eg, for a project containing
+// workspaces a, b, c, d, where a depends on b and c, and b
+// and c both depend on d, it will run first for d, then for b
+// and c in parallel, then for a when c and b are both resolved.
+// depResults is a map of [ws, result] of all dependency
+// workspaces that were completed prior to this one
+// AbortSignal can be used to stop the operation.
+m.run(async (ws, signal, depResults) => {
+  await doSomethingAsync(ws, signal)
+})
+
+// methods that return a set of Workspace objects actually return
+// Monorepo object with all the same methods.
+console.log(m.group('app')) // Monorepo object containing only those keys
+m.group('app').run(async ([name, ws]) => {
+  // logs all the workspaces in the 'app' group
+  console.log(name, ws)
+})
+
+// Since loading everything can be slow, and some actions don't
+// require loading the entire set, you can use this method if you
+// want to just want to load a single workspace or those matching
+// a specific pattern.
+const apps = new Monorepo()
+apps.load({ groups: 'apps' })
+const { manifest } = app.get('packages/my-app')
+
+// get workspace names in no particular order
+for (const name of m.names()) {
+  // @vltpkg/cache, @vltpkg/semver, etc.
+}
+
+// get workspace paths in no particular order
+for (const path of m.paths()) {
+  // apps/webapp, utils/is-even, etc.
+}
+```
+
+Configuration is stored in the project root at
+`vlt-workspaces.json`. The type of the object in the file must
+be:
+
+```ts
+type WorkspaceConfiguration =
+  | string
+  | string[]
+  | { [group: string]: string | string[] }
+```
+
+If it's an object, each key is a group name, and each value is a
+path, glob, or array of paths and globs, which specify the
+location of the workspace projects. Glob matches are only
+considered if they are a directory containing a `package.json`
+file.
+
+A `string` or `string[]` in the file is interpreted as
+`{"packages": <value>}`. Ie, the default group is `packages`.
+
+Workspaces are allowed to be in multiple groups, so something
+like this is fine:
+
+```json
+{
+  "apps": "apps/*",
+  "frontend": ["utils/frontend", "apps/website"],
+  "utils": ["utils/**"]
+}
+```
+
+If the glob patterns result in a situation where one workspace
+folder is contained within another workspace folder, an error
+will be raised.
+
+## Run Order
+
+The `monorepo.run` method can run an async function for each
+workspace in the project, visiting each exactly once.
+
+When workspaces depend on one another, it will walk dependencies
+before dependents, _unless_ there is a cycle, as that would be
+impossible, and vlt does not prevent workspace dependency cycles.
---
+++ src/workspaces/src/index.ts
@@ -0,0 +1,576 @@
+import { error } from '@vltpkg/error-cause'
+import { PackageJson } from '@vltpkg/package-json'
+import { Manifest } from '@vltpkg/types'
+import { readFileSync, statSync } from 'fs'
+import { GlobOptionsWithFileTypesFalse, globSync } from 'glob'
+import { DepResults, graphRun, graphRunSync } from 'graph-run'
+import { basename, resolve } from 'path'
+import { Path, PathScurry } from 'path-scurry'
+import { parse } from 'polite-json'
+
+/**
+ * The object passed to the constructor or {@link Monorepo#load} to limit which
+ * {@link Workspace Workspaces} get loaded.
+ */
+export type LoadQuery = {
+  /**
+   * A glob pattern string, or an array of them. Only workspaces found
+   * in paths matched will be loaded.
+   */
+  paths?: string | string[]
+  /**
+   * A string, or an array of strings. If set, only workspaces in the
+   * specified groups named will be included, if set.
+   */
+  groups?: string | string[]
+}
+
+/**
+ * Canonical form of the {@link WorkspaceConfig}, used
+ * internally for consistency.
+ */
+export type WorkspaceConfigObject = {
+  [group: string]: string[]
+}
+
+/**
+ * Allowed datatype in the `vlt-workspaces.json` file.
+ */
+export type WorkspaceConfig =
+  | string
+  | string[]
+  | WorkspaceConfigObject
+
+/**
+ * Turn a {@link WorkspaceConfig} into a
+ * {@link WorkspaceConfigObject}, or throw if it's not valid.
+ */
+export const asWSConfig = (
+  conf: unknown,
+  path?: string,
+): WorkspaceConfigObject => {
+  assertWSConfig(conf, path)
+  return (
+    typeof conf === 'string' ? { packages: [conf] }
+    : Array.isArray(conf) ? { packages: conf }
+    : conf
+  )
+}
+
+/**
+ * Throw if the provided value is not a valid {@link WorkspaceConfig}
+ */
+export const assertWSConfig: (
+  conf: unknown,
+  path?: string,
+) => asserts conf is WorkspaceConfig = (
+  conf: unknown,
+  path?: string,
+) => {
+  if (typeof conf === 'string') return conf
+
+  if (Array.isArray(conf)) {
+    for (const c of conf) {
+      if (typeof c !== 'string') {
+        throw error('Invalid workspace definition', {
+          path,
+          found: c,
+          wanted: 'string',
+        })
+      }
+    }
+    return
+  }
+
+  if (conf && typeof conf === 'object') {
+    for (const [group, value] of Object.entries(conf)) {
+      if (typeof value === 'string') continue
+      if (Array.isArray(value)) {
+        for (const c of value) {
+          if (typeof c !== 'string') {
+            throw error('Invalid workspace definition', {
+              path,
+              name: group,
+              found: c,
+              wanted: 'string',
+            })
+          }
+        }
+        continue
+      }
+      throw error('Invalid workspace definition', {
+        path,
+        name: group,
+        found: value,
+        wanted: 'string | string[]',
+      })
+    }
+    return
+  }
+
+  throw error('Invalid workspace definition', {
+    path,
+    found: conf,
+    wanted:
+      'string | string[] | { [group: string]: string | string[] }',
+  })
+}
+
+export interface MonorepoOptions {
+  /**
+   * A {@link PackageJson} object, for sharing manifest caches
+   */
+  packageJson?: PackageJson
+  /**
+   * A {@link PathScurry} object, for use in globs
+   */
+  scurry?: PathScurry
+  /** Parsed normalized contents of a `vlt-workspaces.json` file */
+  config?: WorkspaceConfigObject
+  /**
+   * If set, then {@link Monorepo#load} will be called immediately with
+   * this argument.
+   */
+  load?: LoadQuery
+}
+
+/**
+ * Class representing a Monorepo containing multiple workspaces.
+ *
+ * Does not automatically look up the root, but that can be provided by
+ * running `Config.load()`, since it stops seeking the route when a
+ * `vlt-workspaces.json` file is encountered.
+ */
+export class Monorepo {
+  /** The project root where vlt-workspaces.json is found */
+  cwd: string
+  /** Scurry object to cache all filesystem calls (mostly globs) */
+  scurry: PathScurry
+
+  // maps both name and path to the workspace objects
+  #workspaces = new Map<string, Workspace>()
+  #groups = new Map<string, Set<Workspace>>()
+  #config?: WorkspaceConfigObject
+  packageJson: PackageJson
+
+  /**
+   * Number of {@link Workspace} objects loaded in this Monorepo
+   */
+  get size(): number {
+    return [...this.#workspaces.values()].length
+  }
+
+  constructor(cwd: string, options: MonorepoOptions = {}) {
+    this.cwd = resolve(cwd)
+    this.scurry = options.scurry ?? new PathScurry(cwd)
+    this.packageJson = options.packageJson ?? new PackageJson()
+    if (options.load) this.load(options.load)
+  }
+
+  /**
+   * Load the workspace definitions from vlt-workspaces.json,
+   * canonicalizing the result into the effective `{[group:string]:string[]}`
+   * form.
+   *
+   * Eg:
+   * - `"src/*"` => `{packages:["src/*"]}`
+   * - `{"apps": "src/*"}` => `{apps: ["src/*"]}`
+   */
+  get config(): WorkspaceConfigObject {
+    if (this.#config) return this.#config
+    const file = resolve(this.cwd, 'vlt-workspaces.json')
+    let confData: string
+    try {
+      confData = readFileSync(file, 'utf8')
+    } catch (er) {
+      throw error('Not in a monorepo, no vlt-workspaces.json found', {
+        path: this.cwd,
+        cause: er as Error,
+      })
+    }
+    let parsed: unknown
+    try {
+      parsed = parse(confData)
+    } catch (er) {
+      throw error('Invalid vlt-workspaces.json file', {
+        path: this.cwd,
+        cause: er as Error,
+      })
+    }
+    this.#config = asWSConfig(parsed, file)
+    return this.#config
+  }
+
+  /**
+   * Iterating the Monorepo object yields the workspace objects, in as close to
+   * topological dependency order as possible.
+   */
+  *[Symbol.iterator](): Generator<Workspace, void, void> {
+    const [ws] = [...this.#workspaces.values()]
+    if (!ws) return
+    // leverage the fact that graphRun returns results in
+    // as close to topological order as possible.
+    for (const workspace of this.runSync(() => {}).keys()) {
+      yield workspace
+    }
+  }
+
+  /**
+   * Iterating the Monorepo object yields the workspace objects, in as close to
+   * topological dependency order as possible.
+   */
+  async *[Symbol.asyncIterator](): AsyncGenerator<
+    Workspace,
+    void,
+    void
+  > {
+    const [ws] = [...this.#workspaces.values()]
+    if (!ws) return
+    for (const workspace of (await this.run(() => {})).keys()) {
+      yield workspace
+    }
+  }
+
+  /**
+   * By default, loads all workspaces reachable in the Monorepo.
+   *
+   * If provided with one (`string`)or more (`string[]`) group names in
+   * the {@link LoadQuery#groups} field, then only Workspaces in the named
+   * group(s) will be considered. Note that group names are unique string
+   * matches, not globs.
+   *
+   * If provided with a set of arbitrary path arguments, then only paths
+   * patching the provided pattern(s) will be included.
+   *
+   * These two options intersect, so
+   * `load({groups:'foo', paths:'./foo/[xy]*'})` will only load the workspaces
+   * in the group `foo` that match the paths glob.
+   */
+  load(query: LoadQuery = {}): this {
+    const paths = new Set(
+      typeof query.paths === 'string' ?
+        [query.paths]
+      : query.paths ?? [],
+    )
+    const groups = new Set(
+      typeof query.groups === 'string' ?
+        [query.groups]
+      : query.groups ?? [],
+    )
+
+    const groupsExpanded: { [k: string]: Set<string> } = {}
+    for (const [group, pattern] of Object.entries(this.config)) {
+      if (groups.size && !groups.has(group)) continue
+      groupsExpanded[group] = this.#glob(pattern)
+    }
+    const filter = paths.size ? this.#glob([...paths]) : paths
+
+    // if we specified paths, but none matched, nothing to do
+    if (paths.size && !filter.size) return this
+    for (const [group, matches] of Object.entries(groupsExpanded)) {
+      for (const path of matches) {
+        if (filter.size && !filter.has(path)) continue
+        this.#loadWS(path, group)
+      }
+    }
+
+    return this
+  }
+
+  // Either load a workspace from disk, or from our internal set,
+  // and assign it to the named group
+  #loadWS(path: string, group?: string): Workspace {
+    const loaded = this.#workspaces.get(path)
+    if (loaded) return loaded
+    const manifest = this.packageJson.read(resolve(this.cwd, path))
+    const ws = new Workspace(path, manifest)
+    if (group) ws.groups.push(group)
+    this.#workspaces.set(ws.path, ws)
+    this.#workspaces.set(ws.name, ws)
+    for (const name of ws.groups) {
+      const group = this.#groups.get(name) ?? new Set()
+      group.add(ws)
+      this.#groups.set(name, group)
+    }
+    return ws
+  }
+
+  // can't be cached, because it's dependent on the matches set
+  // but still worthwhile to have it defined in one place
+  #globOptions(matches: Set<string>): GlobOptionsWithFileTypesFalse {
+    // if the entry or any of its parent dirs are already matched,
+    // then we should not explore further down that directory tree.
+    // if we hit the cwd (or a root) then stop searching.
+    const inMatches = (p?: Path): boolean => {
+      return (
+        !!p?.relativePosix() &&
+        (matches.has(p.relativePosix()) || inMatches(p.parent))
+      )
+    }
+
+    return {
+      root: this.cwd,
+      cwd: this.cwd,
+      posix: true,
+      scurry: this.scurry,
+      withFileTypes: false,
+      ignore: {
+        childrenIgnored: p =>
+          basename(p.relativePosix()) === 'node_modules' ||
+          inMatches(p),
+        // ignore if fails to load package.json
+        ignored: p => {
+          p.lstatSync()
+          const rel = p.relativePosix()
+          if (!rel) return true
+          const maybeDelete: string[] = []
+          for (const m of matches) {
+            if (rel.startsWith(m + '/')) return true
+            if (m.startsWith(rel + '/')) {
+              maybeDelete.push(m)
+            }
+          }
+          if (!p.isDirectory()) return true
+          const pj = p.resolve('package.json').lstatSync()
+          if (!pj?.isFile()) return true
+          try {
+            this.packageJson.read(p.fullpath())
+          } catch (er) {
+            return true
+          }
+          for (const m of maybeDelete) {
+            matches.delete(m)
+          }
+          matches.add(rel)
+          return false
+        },
+      },
+    }
+  }
+
+  #glob(pattern: string | string[]) {
+    const matches = new Set<string>()
+    globSync(pattern, this.#globOptions(matches))
+    return matches
+  }
+
+  /**
+   * Return the array of workspace dependencies that are found in
+   * the loaded set, for use in calculating dependency graph order for
+   * build operations.
+   *
+   * This does *not* get the full set of dependencies, or expand any
+   * `workspace:` dependencies that are not loaded.
+   *
+   * Call with the `forceLoad` param set to `true` to attempt a full
+   * load if any deps are not currently loaded.
+   */
+  getDeps(ws: Workspace, forceLoad = false): Workspace[] {
+    // load manifest and find workspace: deps
+    // filter by those loaded
+    const { manifest } = ws
+    const depWorkspaces: Workspace[] = []
+    let didForceLoad = false
+    for (const depType of [
+      'dependencies',
+      'devDependencies',
+      'optionalDependencies',
+      'peerDependencies',
+    ]) {
+      const deps = manifest[depType]
+      if (!deps) continue
+      for (const [dep, spec] of Object.entries(deps)) {
+        if (spec.startsWith('workspace:')) {
+          let depWS = this.#workspaces.get(dep)
+          if (!depWS) {
+            if (!forceLoad) continue
+            if (didForceLoad) continue
+            didForceLoad = true
+            this.load()
+            depWS = this.#workspaces.get(dep)
+            if (!depWS) continue
+          }
+          depWorkspaces.push(depWS)
+        }
+      }
+    }
+    return depWorkspaces
+  }
+
+  /** @todo */
+  onCycle(
+    _ws: Workspace,
+    _cycle: Workspace[],
+    _depPath: Workspace[],
+  ) {
+    // XXX - process logging? Need to say something like:
+    // Cyclical workspace dependency warning!
+    // When evaluating dependency ${ws.name} via ${
+    //   path.map(ws => ws.name).join(' -> ')
+    // }, a dependency cycle was detected: ${
+    //   cycle.map(ws => ws.name).join(' -> ')
+    // }. Operation will continue, but dependency order not guaranteed.`
+  }
+
+  /**
+   * Return the set of workspaces in the named group.
+   * If the group is not one we know about, then undefined is returned.
+   */
+  group(group: string) {
+    return this.#groups.get(group)
+  }
+
+  /**
+   * Get a loaded workspace by path or name.
+   *
+   * Note that this can only return workspaces that were ingested via a
+   * previous call to {@link Monorepo#load}.
+   */
+  get(nameOrPath: string) {
+    return this.#workspaces.get(nameOrPath)
+  }
+
+  /**
+   * get the list of all loaded workspace names
+   */
+  *names() {
+    for (const [key, ws] of this.#workspaces) {
+      if (key === ws.name) yield key
+    }
+  }
+
+  /**
+   * get the list of all loaded workspace names
+   */
+  *paths() {
+    for (const [key, ws] of this.#workspaces) {
+      if (key === ws.path) yield key
+    }
+  }
+
+  /**
+   * get the workspace objects in no particular order.
+   * this is ever so slightly faster than iterating, because it doesn't
+   * explore the graph to yield results in topological dependency order,
+   * and should be used instead when order doesn't matter.
+   */
+  values() {
+    return this.#workspaces.values()
+  }
+
+  /**
+   * Get all the keys (package names and paths) for loaded workspaces.
+   * Union of {@link Monorepo#names} and {@link Monorepo#paths}
+   */
+  keys() {
+    return this.#workspaces.keys()
+  }
+
+  /**
+   * Run an operation asynchronously over all loaded workspaces
+   *
+   * If the `forceLoad` param is true, then it will attempt to do a full load
+   * when encountering a `workspace:` dependency that isn't loaded.
+   *
+   * Note that because the return type appears in the parameters of the
+   * operation function, it must be set explicitly either in the operation
+   * function signature or by calling `run<MyType>` or it'll fall back to
+   * `unknown`, similar to `Array.reduce()`, and for the same reason.
+   */
+  async run<R>(
+    operation: (
+      s: Workspace,
+      signal: AbortSignal,
+      depResults: DepResults<Workspace, R>,
+    ) => Promise<R> | R,
+    forceLoad = false,
+  ) {
+    const [ws, ...rest] = [...this.#workspaces.values()]
+    if (!ws) {
+      throw error('No workspaces loaded', undefined, this.run)
+    }
+
+    return graphRun<Workspace, R>({
+      graph: [ws, ...rest],
+      getDeps: ws => this.getDeps(ws, forceLoad),
+      visit: async (ws, signal, _, depResults) =>
+        await operation(ws, signal, depResults),
+      onCycle: (ws, cycle, path) => this.onCycle(ws, cycle, path),
+    })
+  }
+
+  /**
+   * Run an operation synchronously over all loaded workspaces
+   *
+   * If the `forceLoad` param is true, then it will attempt to do a full load
+   * when encountering a `workspace:` dependency that isn't loaded.
+   *
+   * Note that because the return type appears in the parameters of the
+   * operation function, it must be set explicitly either in the operation
+   * function signature or by calling `runSync<MyType>` or it'll fall back to
+   * `unknown`, similar to `Array.reduce()`, and for the same reason.
+   */
+  runSync<R>(
+    operation: (
+      s: Workspace,
+      signal: AbortSignal,
+      depResults: DepResults<Workspace, R>,
+    ) => R,
+    forceLoad = false,
+  ) {
+    const [ws, ...rest] = [...this.#workspaces.values()]
+    if (!ws) {
+      throw error('No workspaces loaded', undefined, this.run)
+    }
+
+    return graphRunSync<Workspace, R>({
+      graph: [ws, ...rest],
+      getDeps: ws => this.getDeps(ws, forceLoad),
+      visit: (ws, signal, _, depResults) =>
+        operation(ws, signal, depResults),
+      onCycle: (ws, cycle, path) => this.onCycle(ws, cycle, path),
+    })
+  }
+
+  /**
+   * Convenience method to instantiate and load in one call.
+   * Returns undefined if the cwd is not a monorepo workspaces
+   * root, otherwise returns the loaded Monorepo.
+   */
+  static maybeLoad(cwd: string, options: MonorepoOptions = { load: {} }) {
+    try {
+      if (!statSync(resolve(cwd, 'vlt-workspaces.json')).isFile()) {
+        return
+      }
+    } catch {
+      return
+    }
+    const { load = {} } = options
+    return new Monorepo(cwd, { ...options, load })
+  }
+
+  /**
+   * Convenience method to instantiate and load in one call.
+   * Throws if called on a directory that is not a workspaces root.
+   */
+  static load(cwd: string, options: MonorepoOptions = { load: {} }) {
+    const { load = {} } = options
+    return new Monorepo(cwd, { ...options, load })
+  }
+}
+
+/**
+ * Class representing a single Workspace in a {@link Monorepo}
+ */
+export class Workspace {
+  path: string
+  manifest: Manifest
+  groups: string[] = []
+  name: string
+
+  constructor(path: string, manifest: Manifest) {
+    this.path = path
+    this.manifest = manifest
+    this.name = manifest.name ?? path
+  }
+}
