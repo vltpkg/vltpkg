7318f696a0c0971f3a4831415d8eb15dacccca98
isaacs <i@izs.me>
2024-05-26 21:14:14 -0700

    package-json: cache errors also
    May as well save the read() call if we know it'll fail.

--- src/package-json/src/index.ts
+++ src/package-json/src/index.ts
@@ -1,15 +1,23 @@
+import { error, ErrorCauseObject } from '@vltpkg/error-cause'
+import { asManifest, Manifest } from '@vltpkg/types'
 import { readFileSync } from 'node:fs'
 import { resolve } from 'node:path'
-import { error } from '@vltpkg/error-cause'
-import { Manifest, asManifest } from '@vltpkg/types'
 import { parse } from 'polite-json'
 
 export class PackageJson {
   /**
-   * Reads and parses contents of a `package.json` file at a directory `dir`.
+   * cache of `package.json` loads
    */
   cache: Map<string, Manifest> = new Map()
 
+  /**
+   * cache of load errors
+   */
+  errCache: Map<string, ErrorCauseObject> = new Map()
+
+  /**
+   * Reads and parses contents of a `package.json` file at a directory `dir`.
+   */
   read(dir: string): Manifest {
     const cachedPackageJson = this.cache.get(dir)
     if (cachedPackageJson) {
@@ -17,6 +25,15 @@ export class PackageJson {
     }
 
     const filename = resolve(dir, 'package.json')
+
+    const fail = (err: ErrorCauseObject) =>
+      error('Could not read package.json file', err, this.read)
+
+    const cachedError = this.errCache.get(dir)
+    if (cachedError) {
+      throw fail(cachedError as ErrorCauseObject)
+    }
+
     try {
       const res: Manifest = asManifest(
         parse(readFileSync(filename, { encoding: 'utf8' })),
@@ -24,10 +41,12 @@ export class PackageJson {
       this.cache.set(dir, res)
       return res
     } catch (err) {
-      throw error('Could not read package.json file', {
+      const ec: ErrorCauseObject = {
         path: filename,
         cause: err as Error,
-      })
+      }
+      this.errCache.set(dir, ec)
+      throw fail(ec)
     }
   }
 }
