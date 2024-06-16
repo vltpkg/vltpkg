48358245202d899f372a7f5052b067f5b3adab61
isaacs <i@izs.me>
2024-06-15 18:45:41 -0700

    cache-unzip: set content-encoding/length
    This keeps content-length and content-encoding accurate when reading
    from the cache.

--- src/cache-unzip/test/unzip.ts
+++ src/cache-unzip/test/unzip.ts
@@ -79,13 +79,25 @@ t.test('unzip some stuff in the cache', async t => {
   })
 
   const c = new Cache({ path: t.testdirName })
+  const resultHead = Buffer.concat([
+    Buffer.from([0, 0, 0, 63]),
+    Buffer.from([0, 0, 0]),
+    Buffer.from([0, 0, 0, 'content-encoding'.length + 4]),
+    Buffer.from('content-encoding'),
+    Buffer.from([0, 0, 0, 'identity'.length + 4]),
+    Buffer.from('identity'),
+    Buffer.from([0, 0, 0, 'content-length'.length + 4]),
+    Buffer.from('content-length'),
+    Buffer.from([0, 0, 0, String('yes gzipped'.length).length + 4]),
+    Buffer.from(String('yes gzipped'.length)),
+  ])
   t.same(
     await c.fetch('gz1'),
-    Buffer.concat([head10, Buffer.from('yes gzipped')]),
+    Buffer.concat([resultHead, Buffer.from('yes gzipped')]),
   )
   t.same(
     await c.fetch('gz2'),
-    Buffer.concat([head10, Buffer.from('yes gzipped')]),
+    Buffer.concat([resultHead, Buffer.from('yes gzipped')]),
   )
   t.same(
     await c.fetch('plain1'),
@@ -94,5 +106,67 @@ t.test('unzip some stuff in the cache', async t => {
   t.same(
     await c.fetch('plain2'),
     Buffer.concat([head10, Buffer.from('not gzipped')]),
+  )
+})
+
+t.test('unzip an entry that had headers', async t => {
+  const unz = '{"hello":"world"}'
+  const z = gzipSync(unz)
+  const zheaders = Buffer.concat([
+    Buffer.from([0, 0, 0, 'content-encoding'.length + 4]),
+    Buffer.from('content-encoding'),
+    Buffer.from([0, 0, 0, 'gzip'.length + 4]),
+    Buffer.from('gzip'),
+    Buffer.from([0, 0, 0, 'content-length'.length + 4]),
+    Buffer.from('content-length'),
+    Buffer.from([0, 0, 0, String(z.byteLength).length + 4]),
+    Buffer.from(String(z.byteLength)),
+  ])
+  const zlen = zheaders.byteLength + 7
+  const zipped = Buffer.concat([
+    Buffer.from([0, 0, 0, zlen]),
+    Buffer.from('200'),
+    zheaders,
+    z,
+  ], zlen + z.byteLength)
+
+  const resultHead = Buffer.concat([
+    Buffer.from([0, 0, 0, 'content-encoding'.length + 4]),
+    Buffer.from('content-encoding'),
+    Buffer.from([0, 0, 0, 'identity'.length + 4]),
+    Buffer.from('identity'),
+    Buffer.from([0, 0, 0, 'content-length'.length + 4]),
+    Buffer.from('content-length'),
+    Buffer.from([0, 0, 0, String(unz.length).length + 4]),
+    Buffer.from(String(unz.length)),
+  ])
+  const rlen = resultHead.byteLength + 7
+  const result = Buffer.concat([
+    Buffer.from([0, 0, 0, rlen]),
+    Buffer.from('200'),
+    resultHead,
+    Buffer.from(unz),
+  ], rlen + unz.length)
+
+  const cache = new Cache({ path: t.testdir() })
+  cache.set('gz1', zipped)
+  await cache.promise()
+  const res = spawnSync(
+    process.execPath,
+    [script, t.testdirName, 'gz1'],
+    { stdio: 'inherit' },
+  )
+  t.matchOnlyStrict(res, {
+    status: 0,
+    signal: null,
+    output: [null, null, null],
+    pid: Number,
+    stdout: null,
+    stderr: null,
+  })
+  const c = new Cache({ path: t.testdirName })
+  t.same(
+    await c.fetch('gz1'),
+    result,
   )
 })
