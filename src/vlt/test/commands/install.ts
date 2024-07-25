0386201f2fc18e8fe70fe6aefa61ad4aee61972f
isaacs <i@izs.me>
2024-07-25 11:59:47 -0700

    vlt: wip install command

--- src/vlt/test/commands/install.ts
+++ src/vlt/test/commands/install.ts
@@ -2,10 +2,27 @@
 import t from 'tap'
 import { LoadedConfig } from '../../src/config/index.js'
 
+const reifies: any[] = []
+
 const { usage, command } = await t.mockImport<
   typeof import('../../src/commands/install.js')
->('../../src/commands/install.js')
+>('../../src/commands/install.js', {
+  '@vltpkg/graph': {
+    buildIdeal: async () => 'buildideal result',
+    actual: {
+      load: () => 'actual.load result',
+    },
+    reify: async (reifyOptions: any) => {
+      reifies.push(reifyOptions)
+    },
+  },
+})
 t.type(usage, 'string')
-t.capture(console, 'log')
-t.capture(console, 'error')
-command({ positionals: [] } as unknown as LoadedConfig)
+await command({ positionals: [] } as unknown as LoadedConfig)
+t.strictSame(reifies, [
+  {
+    actual: 'actual.load result',
+    graph: 'buildideal result',
+    loadManifests: true,
+  },
+])
