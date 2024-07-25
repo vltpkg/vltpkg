d34d693e867b539605b2d3f9f5cfa5fe473ca581
isaacs <i@izs.me>
2024-07-25 11:59:47 -0700

    vlt: wip install command

--- src/vlt/src/commands/install.ts
+++ src/vlt/src/commands/install.ts
@@ -1,9 +1,20 @@
+import { actual, buildIdeal, reify } from '@vltpkg/graph'
 import { LoadedConfig } from '../config/index.js'
 
 export const usage = `vlt install [package ...]
 Install the specified package, updating dependencies appropriately`
 
 export const command = async (conf: LoadedConfig) => {
-  console.log('todo: update deps, reify')
-  console.error(conf.positionals)
+  // TODO: interpret arguments to add deps, or install in certain workspaces
+  const graph = await buildIdeal({
+    ...conf.options,
+    loadManifests: true,
+  })
+  const act = actual.load({ ...conf.options, loadManifests: true })
+  await reify({
+    ...conf.options,
+    actual: act,
+    graph,
+    loadManifests: true,
+  })
 }
