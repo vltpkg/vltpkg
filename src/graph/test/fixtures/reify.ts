7531062ac4f28ba00a3d89fef0cdcea6ed40859f
isaacs <i@izs.me>
2024-08-04 13:42:26 -0700

    graph: test fixtures for reification

---
+++ src/graph/test/fixtures/reify.ts
@@ -0,0 +1,160 @@
+// fixture implementation for reify tests
+
+// This is a little bit complicated, but allows us to modify this test
+// without manually fetching fixtures or having to fetch anything in CI.
+// The json maps spec strings to resolution files, and then if the file
+// is there, we serve it up. Otherwise, we fetch from the actual real
+// world location, and stash it in the fixtures dir.
+
+import {
+  PackageInfoClientRequestOptions,
+  Resolution,
+} from '@vltpkg/package-info'
+import { Spec } from '@vltpkg/spec'
+import t from 'tap'
+
+import { PackageInfoClient } from '@vltpkg/package-info'
+import { Manifest } from '@vltpkg/types'
+import {
+  existsSync,
+  lstatSync,
+  mkdirSync,
+  readFileSync,
+  symlinkSync,
+  writeFileSync,
+} from 'fs'
+import { statSync } from 'node:fs'
+import { basename, dirname, resolve } from 'path'
+import { extract } from 'tar'
+import { fileURLToPath } from 'url'
+
+const realPackageInfo = new PackageInfoClient({})
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+export const fixtureDir = resolve(__dirname, 'reify')
+
+const fixtureMapFile = resolve(fixtureDir, 'map.json')
+const fixtureMap = JSON.parse(
+  readFileSync(fixtureMapFile, 'utf8'),
+) as Record<string, string>
+
+const resolutionsMapFile = resolve(fixtureDir, 'resolutions.json')
+const resolutionsMap = JSON.parse(
+  readFileSync(resolutionsMapFile, 'utf8'),
+) as Record<string, Omit<Resolution, 'spec'>>
+
+const fixtureExists = (fixture: string) =>
+  existsSync(resolve(fixtureDir, fixture) + '.tgz') &&
+  existsSync(resolve(fixtureDir, fixture) + '.json')
+
+const fixtureName = async (spec: Spec) =>
+  basename((await mockPackageInfo.resolve(spec)).resolved, '.tgz')
+
+let fixtureMapChanged = false
+let resolutionsMapChanged = false
+
+export const fixtureManifest = (name: string): Manifest =>
+  JSON.parse(readFileSync(resolve(fixtureDir, name) + '.json', 'utf8'))
+
+export const mockPackageInfo = {
+  resolve: async (
+    spec: string | Spec,
+    options: PackageInfoClientRequestOptions = {},
+  ): Promise<Resolution> => {
+    spec = typeof spec === 'string' ? Spec.parse(spec) : spec
+    const res =
+      resolutionsMap[String(spec)] ??
+      (await realPackageInfo.resolve(spec, options))
+    delete (res as { spec?: Spec }).spec
+    if (!resolutionsMap[String(spec)]) {
+      resolutionsMap[String(spec)] = res
+      resolutionsMapChanged = true
+    }
+    return { ...res, spec }
+  },
+
+  manifest: async (
+    spec: string | Spec,
+    _options: PackageInfoClientRequestOptions = {},
+  ) => {
+    spec = Spec.parse(String(spec)).final
+    const fixture =
+      fixtureMap[String(spec)] ?? (await fixtureName(spec))
+    if (!fixtureExists(fixture)) await addFixture(spec)
+    const maniFile = resolve(fixtureDir, fixture) + '.json'
+    if (!fixtureMap[String(spec)]) {
+      fixtureMap[String(spec)] = fixture
+      fixtureMapChanged = true
+    }
+    return JSON.parse(readFileSync(maniFile, 'utf8')) as Manifest
+  },
+
+  extract: async (
+    spec: string | Spec,
+    target: string,
+    _options: PackageInfoClientRequestOptions = {},
+  ) => {
+    spec = Spec.parse(String(spec)).final
+    const fixture =
+      fixtureMap[String(spec)] ?? (await fixtureName(spec))
+    if (!fixtureExists(fixture)) await addFixture(spec)
+    const artifact = resolve(fixtureDir, fixture + '.tgz')
+    fixtureMap[String(spec)] = fixture
+    if (lstatSync(artifact).isSymbolicLink()) {
+      symlinkSync(artifact, target)
+    } else {
+      mkdirSync(target, { recursive: true })
+      extract({ sync: true, file: artifact, strip: 1, cwd: target })
+    }
+  },
+} as unknown as PackageInfoClient
+
+const addFixture = async (spec: Spec) => {
+  const fixture = await fixtureName(spec)
+  if (fixtureExists(fixture)) {
+    return fixture
+  }
+
+  const res = await mockPackageInfo.resolve(spec)
+  // get the artifact
+  const artifact = resolve(fixtureDir, fixture + '.tgz')
+  if (/^https?:/.test(res.resolved)) {
+    const response = await fetch(res.resolved)
+    const buf = Buffer.from(await response.arrayBuffer())
+    writeFileSync(artifact, buf)
+  } else if (/^git/.test(res.resolved)) {
+    throw 'git deps have to be added manually'
+  } else {
+    const st = statSync(res.resolved)
+    if (st.isFile()) {
+      writeFileSync(artifact, readFileSync(res.resolved))
+    } else {
+      symlinkSync(res.resolved, artifact)
+    }
+  }
+
+  // get the manifest
+  const maniFile = resolve(fixtureDir, fixture + '.json')
+  writeFileSync(
+    maniFile,
+    JSON.stringify(await realPackageInfo.manifest(spec), null, 2) +
+      '\n',
+  )
+
+  fixtureMap[String(spec)] = fixture
+}
+
+t.teardown(() => {
+  if (fixtureMapChanged) {
+    writeFileSync(
+      fixtureMapFile,
+      JSON.stringify(fixtureMap, null, 2) + '\n',
+    )
+  }
+  if (resolutionsMapChanged) {
+    writeFileSync(
+      resolutionsMapFile,
+      JSON.stringify(resolutionsMap, null, 2) + '\n',
+    )
+  }
+})
