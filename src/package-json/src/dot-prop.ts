c7f97275585b2931c9ce37de4933d1d6e39d7239
Luke Karrys <luke@lukekarrys.com>
2024-08-06 16:57:13 -0700

    vlt: add pkg command

---
+++ src/package-json/src/dot-prop.ts
@@ -0,0 +1,317 @@
+type ArrayArg = unknown[]
+type ObjectArg = Record<PropertyKey, unknown>
+type Arg = ArrayArg | ObjectArg
+
+const DISALLOWED_KEYS = new Set([
+  '__proto__',
+  'prototype',
+  'constructor',
+])
+
+const DIGITS = new Set('0123456789')
+
+const ARRAY_PUSH = Symbol('ARRAY_PUSH')
+
+enum Characters {
+  Escape = '\\',
+  Dot = '.',
+  Empty = '',
+  LeftBracket = '[',
+  RightBracket = ']',
+}
+
+type Character = Characters | (string & {})
+
+enum Parts {
+  Start = 'start',
+  Index = 'index',
+  IndexEnd = 'indexEnd',
+  Property = 'property',
+}
+
+const checkInvalidCharacter = (
+  part: Parts | Character,
+  current: Parts | Character,
+  msg?: string,
+) => {
+  if (current === part) {
+    if (msg === undefined) {
+      switch (current) {
+        case Parts.Index:
+          msg = 'character in an index'
+          break
+        case Parts.IndexEnd:
+          msg = 'character after an index'
+          break
+        /* c8 ignore next 3 */
+        default:
+          msg = ''
+          break
+      }
+    }
+    throw new Error(`Invalid ${msg}`.trim())
+  }
+}
+
+const getPathSegments = (path: string, allowEmptyIndex = false) => {
+  const segments = []
+
+  let currentSegment: Character = Characters.Empty
+  let currentPart = Parts.Start
+  let isIgnoring = false
+
+  for (const character of path.split('') as Character[]) {
+    switch (character) {
+      case Characters.Escape: {
+        checkInvalidCharacter(Parts.Index, currentPart)
+        checkInvalidCharacter(Parts.IndexEnd, currentPart)
+
+        if (isIgnoring) currentSegment += character
+
+        currentPart = Parts.Property
+        isIgnoring = !isIgnoring
+        break
+      }
+
+      case Characters.Dot: {
+        checkInvalidCharacter(Parts.Index, currentPart)
+
+        if (currentPart === Parts.IndexEnd) {
+          currentPart = Parts.Property
+          break
+        }
+
+        if (isIgnoring) {
+          isIgnoring = false
+          currentSegment += character
+          break
+        }
+
+        if (DISALLOWED_KEYS.has(currentSegment)) return []
+
+        segments.push(currentSegment)
+        currentSegment = Characters.Empty
+        currentPart = Parts.Property
+        break
+      }
+
+      case Characters.LeftBracket: {
+        checkInvalidCharacter(Parts.Index, currentPart)
+
+        if (currentPart === Parts.IndexEnd) {
+          currentPart = Parts.Index
+          break
+        }
+
+        if (isIgnoring) {
+          isIgnoring = false
+          currentSegment += character
+          break
+        }
+
+        if (currentPart === Parts.Property) {
+          if (DISALLOWED_KEYS.has(currentSegment)) return []
+
+          segments.push(currentSegment)
+          currentSegment = Characters.Empty
+        }
+
+        currentPart = Parts.Index
+        break
+      }
+
+      case Characters.RightBracket: {
+        if (currentPart === Parts.Index) {
+          if (allowEmptyIndex)
+            checkInvalidCharacter(
+              Characters.Empty,
+              currentSegment,
+              'empty index',
+            )
+          segments.push(
+            currentSegment === Characters.Empty ?
+              ARRAY_PUSH
+            : Number.parseInt(currentSegment, 10),
+          )
+          currentPart = Parts.IndexEnd
+          currentSegment = Characters.Empty
+          break
+        }
+
+        // Falls through
+      }
+
+      default: {
+        if (!DIGITS.has(character))
+          checkInvalidCharacter(Parts.Index, currentPart)
+        checkInvalidCharacter(Parts.IndexEnd, currentPart)
+
+        if (currentPart === Parts.Start) currentPart = Parts.Property
+
+        if (isIgnoring) {
+          isIgnoring = false
+          currentSegment += Characters.Escape
+        }
+
+        currentSegment += character
+      }
+    }
+  }
+
+  if (isIgnoring) currentSegment += Characters.Escape
+
+  checkInvalidCharacter(
+    Parts.Index,
+    currentPart,
+    'index was not closed',
+  )
+
+  if (currentPart === Parts.Property) {
+    if (DISALLOWED_KEYS.has(currentSegment)) return []
+    segments.push(currentSegment)
+  } else if (currentPart === Parts.Start) {
+    segments.push(Characters.Empty)
+  }
+
+  return segments
+}
+
+const isObject = (value: unknown): value is ObjectArg =>
+  value !== null && typeof value === 'object'
+
+const isLast = (arr: ArrayArg, i: number) => i === arr.length - 1
+
+const isStringIndex = (
+  object: unknown,
+  key: PropertyKey,
+): key is string => {
+  if (
+    typeof key !== 'symbol' &&
+    typeof key !== 'number' &&
+    Array.isArray(object)
+  ) {
+    const index = Number.parseInt(key, 10)
+    return (
+      Number.isInteger(index) &&
+      object[index] === object[key as unknown as number]
+    )
+  }
+  return false
+}
+
+const assertNotStringIndex = (object: unknown, key: PropertyKey) => {
+  if (isStringIndex(object, key)) {
+    throw new Error('Cannot use string index')
+  }
+}
+
+export const get = (
+  ogObject: Arg,
+  path: string,
+  defaultValue?: unknown,
+): unknown => {
+  let object: unknown = ogObject
+
+  const pathArray = getPathSegments(path, true)
+  if (!pathArray.length) {
+    return defaultValue
+  }
+
+  for (const [index, key] of pathArray.entries()) {
+    if (isStringIndex(object, key)) {
+      object = isLast(pathArray, index) ? undefined : null
+    } else {
+      object = (object as ObjectArg)[key]
+    }
+
+    if (
+      (object === undefined || object === null) &&
+      !isLast(pathArray, index)
+    ) {
+      // `object` is either `undefined` or `null` so we want to stop the loop,
+      // and if this is not the last bit of the path, and if it didn't return
+      // `undefined` it would return `null` if `object` is `null` but we want
+      // `get({foo: null}, 'foo.bar')` to equal `undefined`, or the supplied
+      // value, not `null`
+      return defaultValue
+    }
+  }
+
+  return object === undefined ? defaultValue : object
+}
+
+export const set = <T extends Arg>(
+  object: T,
+  path: string,
+  value: unknown,
+): T => {
+  const root = object
+  const pathArray = getPathSegments(path)
+
+  for (const [index, key] of pathArray.entries()) {
+    assertNotStringIndex(object, key)
+
+    if (isLast(pathArray, index)) {
+      if (key === ARRAY_PUSH) {
+        ;(object as ArrayArg).push(value)
+      } else {
+        ;(object as ObjectArg)[key as number] = value
+      }
+    } else if (!isObject((object as ObjectArg)[key])) {
+      const next = pathArray[index + 1]
+      ;(object as ObjectArg)[key as number] =
+        typeof next === 'number' || next === ARRAY_PUSH ? [] : {}
+    }
+
+    object = (object as ObjectArg)[key as number] as T
+  }
+
+  return root
+}
+
+export const del = (object: Arg, path: string): boolean => {
+  const pathArray = getPathSegments(path)
+
+  for (const [index, key] of pathArray.entries()) {
+    assertNotStringIndex(object, key)
+
+    if (isLast(pathArray, index)) {
+      if (Array.isArray(object)) {
+        object.splice(key as number, 1)
+      } else {
+        delete object[key as number]
+      }
+      return true
+    }
+
+    object = object[key as number] as Arg
+
+    if (!isObject(object)) {
+      return false
+    }
+  }
+
+  return false
+}
+
+export const has = (object: Arg, path: string): boolean => {
+  const pathArray = getPathSegments(path)
+
+  if (!pathArray.length) {
+    return false
+  }
+
+  for (const key of pathArray) {
+    if (
+      !isObject(object) ||
+      !(key in object) ||
+      isStringIndex(object, key)
+    ) {
+      return false
+    }
+
+    object = object[key] as Arg
+  }
+
+  return true
+}
---
+++ src/vlt/test/commands/pkg.ts
@@ -0,0 +1,154 @@
+import t, { Test } from 'tap'
+import { setupCommand, setupEnv } from '../fixtures/command.js'
+import * as Command from '../../src/commands/pkg.js'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+t.type(Command.usage, 'string')
+
+setupEnv(t)
+
+const setupPkg = async (
+  t: Test,
+  method?: string,
+  packageJson: object = {},
+  { indent = 2 } = {},
+) => {
+  const setup = await setupCommand<typeof Command>(t, {
+    command: 'pkg',
+    argv: method ? [method] : [],
+    testdir: {
+      'package.json': JSON.stringify(packageJson, null, indent),
+    },
+  })
+  return {
+    ...setup,
+    parseLogs: (r: { logs: string }) => JSON.parse(r.logs),
+    readPackageJson: () =>
+      readFileSync(join(setup.dir, 'package.json'), 'utf8'),
+  }
+}
+
+t.test('basic', async t => {
+  const { runCommand } = await setupPkg(t)
+  t.rejects(runCommand(['gett']), {
+    message: 'Unrecognized pkg command',
+    cause: {
+      found: 'gett',
+      validOptions: ['get', 'set', 'rm'],
+    },
+  })
+})
+
+t.test('get', async t => {
+  const pkg = {
+    name: 'package-name',
+    version: '1.0.0',
+    description: 'This is the desc',
+    nested: {
+      keywords: ['one', 'two', 'last', { obj: ['more', 'arrays'] }],
+    },
+  }
+  const { runCommand, parseLogs } = await setupPkg(t, 'get', pkg)
+  t.strictSame(await runCommand().then(parseLogs), pkg)
+  t.strictSame(await runCommand(['name']).then(parseLogs), pkg.name)
+  t.strictSame(
+    await runCommand(['nested.keywords[3].obj[1]']).then(parseLogs),
+    (pkg.nested.keywords[3] as { obj: string[] }).obj[1],
+  )
+  await t.rejects(runCommand(['name', 'version']), {
+    message:
+      'get requires not more than 1 argument. use `pick` to get more than 1.',
+  })
+})
+
+t.test('pick', async t => {
+  const pkg = {
+    name: 'package-name',
+    version: '1.0.0',
+    description: 'This is the desc',
+    nested: {
+      keywords: ['one', 'two', 'last', { obj: ['more', 'arrays'] }],
+    },
+  }
+  const { runCommand, parseLogs } = await setupPkg(t, 'pick', pkg)
+  t.strictSame(await runCommand().then(parseLogs), pkg)
+  t.strictSame(await runCommand(['name']).then(parseLogs), {
+    name: pkg.name,
+  })
+  t.strictSame(
+    await runCommand(['nested.keywords[3].obj[1]']).then(parseLogs),
+    {
+      nested: {
+        keywords: [null, null, null, { obj: [null, 'arrays'] }],
+      },
+    },
+  )
+  t.strictSame(
+    await runCommand(['nested.keywords']).then(parseLogs),
+    {
+      nested: {
+        keywords: ['one', 'two', 'last', { obj: ['more', 'arrays'] }],
+      },
+    },
+  )
+  t.strictSame(
+    await runCommand(['name', 'version']).then(parseLogs),
+    {
+      name: pkg.name,
+      version: pkg.version,
+    },
+  )
+})
+
+t.test('set', async t => {
+  const { runCommand, readPackageJson } = await setupPkg(t, 'set', {
+    name: 'package-name',
+    version: '1.0.0',
+    description: 'This is the desc',
+  })
+  t.strictSame(await runCommand(['name=new-package-name']), {
+    errs: '',
+    logs: '',
+  })
+  t.strictSame(JSON.parse(readPackageJson()), {
+    name: 'new-package-name',
+    version: '1.0.0',
+    description: 'This is the desc',
+  })
+  t.rejects(runCommand(), {
+    message: 'set requires arguments',
+  })
+  t.rejects(runCommand(['name']), {
+    message: 'set arguments must contain `=`',
+  })
+})
+
+t.test('delete', async t => {
+  const { runCommand, readPackageJson } = await setupPkg(t, 'rm', {
+    name: 'package-name',
+    version: '1.0.0',
+    description: 'This is the desc',
+    nested: {
+      keywords: ['one', 'two', 'last', { obj: ['more', 'arrays'] }],
+    },
+  })
+  t.strictSame(await runCommand(['name']), {
+    errs: '',
+    logs: '',
+  })
+  t.strictSame(await runCommand(['nested.keywords[3].obj[0]']), {
+    errs: '',
+    logs: '',
+  })
+  t.strictSame(JSON.parse(readPackageJson()), {
+    version: '1.0.0',
+    description: 'This is the desc',
+    nested: {
+      keywords: ['one', 'two', 'last', { obj: ['arrays'] }],
+    },
+  })
+  t.rejects(runCommand(), {
+    message: 'rm requires arguments',
+  })
+})
