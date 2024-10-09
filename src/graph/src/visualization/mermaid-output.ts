fdbf2cc4dedcb15b0d595c5c1f5a12fce27fb17a
Ruy Adorno <ruy@vlt.sh>
2024-10-09 14:03:56 -0400

    graph: filter mermaid visualization
    Added the ability to filter mermaid output results, along with fixing
    printed package names and edge references.

--- src/graph/src/visualization/mermaid-output.ts
+++ src/graph/src/visualization/mermaid-output.ts
@@ -1,45 +1,113 @@
 import { DepID } from '@vltpkg/dep-id'
 import { Edge } from '../edge.js'
-import { Graph } from '../graph.js'
 import { Node } from '../node.js'
+import { EdgeLike, NodeLike } from '../types.js'
+
+export type MermaidOutputOptions = {
+  edges: EdgeLike[]
+  importers: Set<NodeLike>
+  nodes: NodeLike[]
+}
+
+type TraverseItem = {
+  self: EdgeLike | NodeLike
+  parent: EdgeLike | NodeLike | undefined
+}
 
 let missingCount = 0
 
-function parseNode(seenNodes: Set<DepID>, graph: Graph, node: Node) {
-  if (seenNodes.has(node.id)) {
+const nodeLabel = (node: NodeLike) =>
+  `"${String(node).replaceAll('@', '#64;')}"`
+
+function parseNode(
+  seenNodes: Set<DepID>,
+  includedItems: Map<EdgeLike | NodeLike, boolean>,
+  node: NodeLike,
+) {
+  if (seenNodes.has(node.id) || !includedItems.get(node)) {
     return ''
   }
   seenNodes.add(node.id)
   const edges: string = [...node.edgesOut.values()]
-    .map(e => parseEdge(seenNodes, graph, e))
+    .map(e => parseEdge(seenNodes, includedItems, e))
+    .filter(Boolean)
     .join('\n')
-  return `${encodeURIComponent(node.id)}(${node.id})${edges.length ? '\n' : ''}${edges}`
+  return `${encodeURIComponent(node.id)}(${nodeLabel(node)})${edges.length ? '\n' : ''}${edges}`
 }
 
-function parseEdge(seenNodes: Set<DepID>, graph: Graph, edge: Edge) {
-  const edgeResult =
-    `${encodeURIComponent(edge.from.id)}(${edge.from.id})` +
-    ` -->|${edge.type}| `
+function parseEdge(
+  seenNodes: Set<DepID>,
+  includedItems: Map<EdgeLike | NodeLike, boolean>,
+  edge: EdgeLike,
+) {
+  if (!includedItems.get(edge)) {
+    return ''
+  }
 
+  const edgeResult =
+    `${encodeURIComponent(edge.from.id)}(${nodeLabel(edge.from)})` +
+    ` -->|"${String(edge.spec).replaceAll('@', '#64;')} (${edge.type})"| `
+
+  const missingLabel =
+    edge.type.endsWith('ptional') ? 'Missing Optional' : 'Missing'
   if (!edge.to) {
-    return (
-      edgeResult +
-      `missing-${missingCount++}(Missing package: ${edge.name}@${edge.spec.bareSpec})\n`
-    )
+    return edgeResult + `missing-${missingCount++}(${missingLabel})\n`
   }
 
   return (
     edgeResult +
-    `${encodeURIComponent(edge.to.id)}(${edge.to.id})\n` +
-    parseNode(seenNodes, graph, edge.to)
+    `${encodeURIComponent(edge.to.id)}(${nodeLabel(edge.to)})\n` +
+    parseNode(seenNodes, includedItems, edge.to)
   )
 }
 
-export function mermaidOutput(graph: Graph) {
-  const seenNodes = new Set<DepID>()
-  const importers = [...graph.importers]
+/**
+ * Returns a mermaid string representation of the graph.
+ */
+export function mermaidOutput({
+  edges,
+  importers,
+  nodes,
+}: MermaidOutputOptions) {
+  const seen = new Set<EdgeLike | NodeLike>()
+  const includedItems = new Map<EdgeLike | NodeLike, boolean>()
+  const traverse = new Set<TraverseItem>(
+    [...importers].map(i => ({ self: i, parent: undefined })),
+  )
+
+  for (const item of traverse) {
+    if (seen.has(item.self)) continue
+    seen.add(item.self)
+
+    if (item.self instanceof Edge) {
+      if (edges.includes(item.self)) {
+        includedItems.set(item.self, true)
+      }
+      if (item.self.to) {
+        traverse.add({ self: item.self.to, parent: item.self })
+      }
+    }
+
+    if (item.self instanceof Node) {
+      if (nodes.includes(item.self)) {
+        includedItems.set(item.self, true)
+      }
+      for (const edge of item.self.edgesOut.values()) {
+        traverse.add({ self: edge, parent: item.self })
+      }
+    }
+  }
+
+  for (const item of [...traverse].reverse()) {
+    if (includedItems.has(item.self) && item.parent) {
+      includedItems.set(item.parent, true)
+    }
+  }
+
   return (
     'flowchart TD\n' +
-    importers.map(i => parseNode(seenNodes, graph, i)).join('\n')
+    [...importers]
+      .map(i => parseNode(new Set<DepID>(), includedItems, i))
+      .join('\n')
   )
 }
--- src/graph/tap-snapshots/test/visualization/mermaid-output.ts.test.cjs
+++ src/graph/tap-snapshots/test/visualization/mermaid-output.ts.test.cjs
@@ -5,24 +5,81 @@
  * Make sure to inspect the output below.  Do not ignore changes!
  */
 'use strict'
+exports[`test/visualization/mermaid-output.ts > TAP > actual graph > selected packages > should print selected packages 1`] = `
+flowchart TD
+file%C2%B7.("file(.):my-project#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"bar#64;^1.0.0 (optional)"| %C2%B7%C2%B7bar%401.0.0("bar#64;1.0.0")
+%C2%B7%C2%B7bar%401.0.0("bar#64;1.0.0")
+%C2%B7%C2%B7bar%401.0.0("bar#64;1.0.0") -->|"baz#64;custom:baz#64;^1.0.0 (prod)"| %C2%B7custom%C2%B7baz%401.0.0("custom:baz#64;1.0.0")
+%C2%B7custom%C2%B7baz%401.0.0("custom:baz#64;1.0.0")
+
+
+`
+
+exports[`test/visualization/mermaid-output.ts > TAP > actual graph > should print from an actual loaded graph 1`] = `
+flowchart TD
+file%C2%B7.("file(.):my-project#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"link#64;file:./linked (prod)"| file%C2%B7linked("file(linked):linked#64;1.0.0")
+file%C2%B7linked("file(linked):linked#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"foo#64;^1.0.0 (prod)"| %C2%B7%C2%B7foo%401.0.0("foo#64;1.0.0")
+%C2%B7%C2%B7foo%401.0.0("foo#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"extraneous#64;* (prod)"| %C2%B7%C2%B7extraneous%401.0.0("extraneous#64;1.0.0")
+%C2%B7%C2%B7extraneous%401.0.0("extraneous#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"bar#64;^1.0.0 (optional)"| %C2%B7%C2%B7bar%401.0.0("bar#64;1.0.0")
+%C2%B7%C2%B7bar%401.0.0("bar#64;1.0.0")
+%C2%B7%C2%B7bar%401.0.0("bar#64;1.0.0") -->|"blooo#64;1 (prod)"| %C2%B7%C2%B7blooo%401.0.0("blooo#64;1.0.0")
+%C2%B7%C2%B7blooo%401.0.0("blooo#64;1.0.0")
+%C2%B7%C2%B7bar%401.0.0("bar#64;1.0.0") -->|"baz#64;custom:baz#64;^1.0.0 (prod)"| %C2%B7custom%C2%B7baz%401.0.0("custom:baz#64;1.0.0")
+%C2%B7custom%C2%B7baz%401.0.0("custom:baz#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"aliased#64;custom:foo#64;^1.0.0 (dev)"| %C2%B7custom%C2%B7foo%401.0.0("custom:foo#64;1.0.0")
+%C2%B7custom%C2%B7foo%401.0.0("custom:foo#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"#64;scoped/b#64;^1.0.0 (prod)"| %C2%B7%C2%B7%40scoped%252Fb%401.0.0("#64;scoped/b#64;1.0.0")
+%C2%B7%C2%B7%40scoped%252Fb%401.0.0("#64;scoped/b#64;1.0.0")
+%C2%B7%C2%B7%40scoped%252Fb%401.0.0("#64;scoped/b#64;1.0.0") -->|"#64;scoped/c#64;^1.0.0 (prod)"| %C2%B7%C2%B7%40scoped%252Fc%401.0.0("#64;scoped/c#64;1.0.0")
+%C2%B7%C2%B7%40scoped%252Fc%401.0.0("#64;scoped/c#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"#64;scoped/a#64;^1.0.0 (prod)"| %C2%B7%C2%B7%40scoped%252Fa%401.0.0("#64;scoped/a#64;1.0.0")
+%C2%B7%C2%B7%40scoped%252Fa%401.0.0("#64;scoped/a#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"missing#64;^1.0.0 (prod)"| missing-1(Missing)
+
+workspace%C2%B7packages%252Fworkspace-b("workspace(packages/workspace-b):workspace-b#64;1.0.0")
+workspace%C2%B7packages%252Fworkspace-a("workspace(packages/workspace-a):workspace-a#64;1.0.0")
+workspace%C2%B7packages%252Fworkspace-a("workspace(packages/workspace-a):workspace-a#64;1.0.0") -->|"workspace-b#64;workspace:* (dev)"| workspace%C2%B7packages%252Fworkspace-b("workspace(packages/workspace-b):workspace-b#64;1.0.0")
+workspace%C2%B7packages%252Fworkspace-b("workspace(packages/workspace-b):workspace-b#64;1.0.0")
+workspace%C2%B7packages%252Fworkspace-a("workspace(packages/workspace-a):workspace-a#64;1.0.0") -->|"ipsum#64;^1.0.0 (dev)"| %C2%B7%C2%B7ipsum%401.0.0("ipsum#64;1.0.0")
+%C2%B7%C2%B7ipsum%401.0.0("ipsum#64;1.0.0")
+workspace%C2%B7packages%252Fworkspace-a("workspace(packages/workspace-a):workspace-a#64;1.0.0") -->|"foo#64;^1.0.0 (dev)"| %C2%B7%C2%B7foo%401.0.0("foo#64;1.0.0")
+%C2%B7%C2%B7foo%401.0.0("foo#64;1.0.0")
+`
+
+exports[`test/visualization/mermaid-output.ts > TAP > cycle > should print cycle mermaid output 1`] = `
+flowchart TD
+file%C2%B7.("file(.):my-project#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"a#64;^1.0.0 (prod)"| %C2%B7%C2%B7a%401.0.0("a#64;1.0.0")
+%C2%B7%C2%B7a%401.0.0("a#64;1.0.0")
+%C2%B7%C2%B7a%401.0.0("a#64;1.0.0") -->|"b#64;^1.0.0 (prod)"| %C2%B7%C2%B7b%401.0.0("b#64;1.0.0")
+%C2%B7%C2%B7b%401.0.0("b#64;1.0.0")
+%C2%B7%C2%B7b%401.0.0("b#64;1.0.0") -->|"a#64;^1.0.0 (prod)"| %C2%B7%C2%B7a%401.0.0("a#64;1.0.0")
+
+`
+
 exports[`test/visualization/mermaid-output.ts > TAP > human-readable-output > should print mermaid output 1`] = `
 flowchart TD
-file%C2%B7.(file·.)
-file%C2%B7.(file·.) -->|prod| %C2%B7%C2%B7foo%401.0.0(··foo@1.0.0)
-%C2%B7%C2%B7foo%401.0.0(··foo@1.0.0)
-file%C2%B7.(file·.) -->|prod| %C2%B7%C2%B7bar%401.0.0(··bar@1.0.0)
-%C2%B7%C2%B7bar%401.0.0(··bar@1.0.0)
-%C2%B7%C2%B7bar%401.0.0(··bar@1.0.0) -->|prod| %C2%B7%C2%B7baz%401.0.0(··baz@1.0.0)
-%C2%B7%C2%B7baz%401.0.0(··baz@1.0.0)
-%C2%B7%C2%B7baz%401.0.0(··baz@1.0.0) -->|prod| %C2%B7%C2%B7foo%401.0.0(··foo@1.0.0)
+file%C2%B7.("file(.):my-project#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"foo#64;^1.0.0 (prod)"| %C2%B7%C2%B7foo%401.0.0("foo#64;1.0.0")
+%C2%B7%C2%B7foo%401.0.0("foo#64;1.0.0")
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"bar#64;^1.0.0 (prod)"| %C2%B7%C2%B7bar%401.0.0("bar#64;1.0.0")
+%C2%B7%C2%B7bar%401.0.0("bar#64;1.0.0")
+%C2%B7%C2%B7bar%401.0.0("bar#64;1.0.0") -->|"baz#64;^1.0.0 (prod)"| %C2%B7%C2%B7baz%401.0.0("baz#64;1.0.0")
+%C2%B7%C2%B7baz%401.0.0("baz#64;1.0.0")
+%C2%B7%C2%B7baz%401.0.0("baz#64;1.0.0") -->|"foo#64;^1.0.0 (prod)"| %C2%B7%C2%B7foo%401.0.0("foo#64;1.0.0")
 
-file%C2%B7.(file·.) -->|prod| missing-0(Missing package: missing@^1.0.0)
+file%C2%B7.("file(.):my-project#64;1.0.0") -->|"missing#64;^1.0.0 (prod)"| missing-0(Missing)
 
 `
 
 exports[`test/visualization/mermaid-output.ts > TAP > workspaces > should print workspaces mermaid output 1`] = `
 flowchart TD
-file%C2%B7.(file·.)
-workspace%C2%B7packages%252Fb(workspace·packages%2Fb)
-workspace%C2%B7packages%252Fa(workspace·packages%2Fa)
+file%C2%B7.("file(.):my-project#64;1.0.0")
+workspace%C2%B7packages%252Fb("workspace(packages/b):b#64;1.0.0")
+workspace%C2%B7packages%252Fa("workspace(packages/a):a#64;1.0.0")
 `
