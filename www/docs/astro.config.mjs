d65f9f84363203a6fba3c5d6a9a8e3ffbd1f1aeb
Luke Karrys <luke@lukekarrys.com>
2024-10-25 14:17:02 -0700

    docs: more docs (#182)

--- www/docs/astro.config.mjs
+++ www/docs/astro.config.mjs
@@ -2,14 +2,21 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 import vercelStatic from '@astrojs/vercel/static'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'docs | vlt',
       social: {
-        github: 'https://github.com/withastro/starlight',
+        github: 'https://github.com/vltpkg/vltpkg',
+        discord: 'https://github.com/vltpkg/vltpkg',
       },
+      customCss: [
+        '@fontsource-variable/manrope/index.css',
+        './src/styles/globals.css',
+      ],
       sidebar: [
         {
           label: 'Guides',
@@ -19,7 +26,19 @@ export default defineConfig({
           label: 'Reference',
           autogenerate: { directory: 'reference' },
         },
+        {
+          label: 'Commands',
+          autogenerate: { directory: 'cli/commands' },
+        },
+        {
+          label: 'API',
+          autogenerate: { directory: 'api' },
+        },
       ],
+    }),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
     }),
   ],
   output: 'static',
