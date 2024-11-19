d404dcddb4fd002e4b9996eca05a3985044446f7
Jason Korol <ejkorol@gmail.com>
2024-11-19 11:57:35 -0500

    bugfixes initial route name

--- src/gui/src/components/navigation/header.tsx
+++ src/gui/src/components/navigation/header.tsx
@@ -1,18 +1,23 @@
 import { useEffect, useState } from 'react'
 import { useGraphStore } from '@/state/index.js'
+
 const Header = () => {
-  const [routeName, setRouteName] = useState<string | undefined>(
-    undefined,
-  )
+  const [routeName, setRouteName] = useState<string>('')
   const route = useGraphStore(state => state.activeRoute)
 
   useEffect(() => {
     /**
      * Set a clean route on the state for display
-     * */
-    const cleanedRoute =
-      route.split('/')[1]?.charAt(0).toUpperCase() + route.slice(2)
-    setRouteName(cleanedRoute)
+     */
+    if (location.pathname === '/') {
+      setRouteName('Dashboard')
+    } else if (route) {
+      const parts = route.split('/')
+      const cleanedRoute =
+        (parts[1]?.charAt(0).toUpperCase() || '') +
+        (parts[1]?.slice(1) || '')
+      setRouteName(cleanedRoute)
+    }
   }, [route])
 
   return (
