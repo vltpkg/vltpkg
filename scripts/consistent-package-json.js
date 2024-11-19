7700f1b890502b6f62f1469c6ea1a02e528ce491
Luke Karrys <luke@lukekarrys.com>
2024-11-19 10:44:17 -0700

    docs: do not catalog prod docs dependencies

--- scripts/consistent-package-json.js
+++ scripts/consistent-package-json.js
@@ -42,6 +42,11 @@ const skipCatalog = ({ name, spec, from, type }) => {
   ) {
     return true
   }
+  // prod deps for docs should not be cataloged since it is
+  // not shipped as part of the CLI
+  if (from === '@vltpkg/docs' && type === 'dependencies') {
+    return true
+  }
   return false
 }
 
