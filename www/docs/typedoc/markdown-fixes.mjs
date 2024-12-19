d8ce6da7b708a67aca7da594584e0d2c897a701b
Luke Karrys <luke@lukekarrys.com>
2024-12-18 21:21:06 -0700

    correct page name checks in headings fix

--- www/docs/typedoc/markdown-fixes.mjs
+++ www/docs/typedoc/markdown-fixes.mjs
@@ -8,92 +8,93 @@ import {
   typedocBasePath,
   typedocContentPath,
   entryFileName,
+  modulesFileName,
 } from './constants.mjs'
 
-/**
- * @param {string} page
- * @param {import('mdast').Heading} node
- * @param {number} index
- * @param {import('mdast').Parent} parent
- * @returns {import('unist-util-visit').VisitorResult|void}
- */
-const fixHeadings = (page, node, index, parent) => {
-  if (
-    page === '/index.md' &&
-    node.depth === 2 &&
-    toString(node) === 'Packages'
-  ) {
-    // Remove h2 from root list of packages since astro will add the same h1
-    parent.children.splice(index, 1)
-    return [SKIP, index]
+/** @param {string} page */
+const fixHeadings =
+  page =>
+  /**
+   * @param {import('mdast').Heading} node
+   * @param {number} index
+   * @param {import('mdast').Parent} parent
+   * @returns {import('unist-util-visit').VisitorResult | void}
+   */
+  (node, index, parent) => {
+    if (
+      page === '/index.md' &&
+      node.depth === 2 &&
+      toString(node) === 'Packages'
+    ) {
+      // Remove h2 from root list of packages since astro will add the same h1
+      parent.children.splice(index, 1)
+      return [SKIP, index]
+    }
+
+    if (page.endsWith(`/${entryFileName}.md`) && node.depth === 1) {
+      // remove h1 from the readme page since those get included by astro
+      parent.children.splice(index, 1)
+      return [SKIP, index]
+    }
+
+    if (
+      page.endsWith(`/${modulesFileName}.md`) &&
+      node.depth === 2 &&
+      toString(node) === 'Modules'
+    ) {
+      // change Modules to Entry Points for workspaces will multiple entry points.
+      // I don't think this can be changed within typedoc or the plugins, and I
+      // think this wording makes more sense.
+      visit(
+        node,
+        'text',
+        textNode => void (textNode.value = 'Entry Points'),
+      )
+    }
   }
 
-  if (page.endsWith(`/${entryFileName}`) && node.depth === 1) {
-    // remove h1 from the readme page since those get included by astro
-    parent.children.splice(index, 1)
-    return [SKIP, index]
+/** @param {string} page */
+const fixLinks =
+  page =>
+  /**
+   * @param {import('mdast').Link} node
+   * @returns {import('unist-util-visit').VisitorResult | void}
+   */
+  node => {
+    if (
+      !node.url ||
+      node.url.startsWith('https://') ||
+      node.url.startsWith('http://')
+    ) {
+      return
+    }
+
+    const [path, anchor] = node.url.split('#')
+    const absPath = posix.resolve(posix.dirname(page), path)
+
+    if (page === absPath) {
+      assert(anchor, 'same page links must be a hash')
+      node.url = `#${anchor}`
+      return
+    }
+
+    node.url =
+      '/' +
+      join(typedocBasePath, absPath).replace(/\.md$/, '') +
+      (anchor ? `#${anchor}` : '')
   }
-
-  if (
-    page.endsWith(`/${entryFileName}`) &&
-    node.depth === 2 &&
-    toString(node) === 'Modules'
-  ) {
-    // change Modules to Entry Points for workspaces will multiple entry points.
-    // I don't think this can be changed within typedoc or the plugins, and I
-    // think this wording makes more sense.
-    visit(
-      node,
-      'text',
-      textNode => void (textNode.value = 'Entry Points'),
-    )
-  }
-}
-
-/**
- * @param {string} page
- * @param {import('mdast').Link} node
- * @returns {import('unist-util-visit').VisitorResult|void}
- */
-const fixLinks = (page, node) => {
-  if (
-    !node.url ||
-    node.url.startsWith('https://') ||
-    node.url.startsWith('http://')
-  ) {
-    return
-  }
-
-  const [path, anchor] = node.url.split('#')
-  const absPath = posix.resolve(posix.dirname(page), path)
-
-  if (page === absPath) {
-    assert(anchor, 'same page links must be a hash')
-    node.url = `#${anchor}`
-    return
-  }
-
-  node.url =
-    '/' +
-    join(typedocBasePath, absPath).replace(/\.md$/, '') +
-    (anchor ? `#${anchor}` : '')
-}
 
 /** @type {import('unified').Plugin} */
-function MarkdownFixes() {
+export default function MarkdownFixes() {
   const { compiler } = this
   assert(compiler, 'must have a compiler')
   this.compiler = (tree, file) => {
     const page =
       '/' + relative(join(file.cwd, typedocContentPath), file.path)
 
-    visit(tree, 'link', node => fixLinks(page, node))
-    visit(tree, 'heading', (node, index, parent) =>
-      fixHeadings(page, node, index, parent),
-    )
+    visit(tree, 'link', fixLinks(page))
+    visit(tree, 'heading', fixHeadings(page))
 
     return compiler(tree, file)
   }
 }
-
-export default MarkdownFixes
