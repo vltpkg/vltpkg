80b4cf0800d799cc78b6c8a0ea2f83f5d0436a03
isaacs <i@izs.me>
2025-01-09 10:25:51 -0800

    registry-client: abstract out the is-iterable method

---
+++ src/registry-client/test/is-iterable.ts
@@ -0,0 +1,10 @@
+import t from 'tap'
+import { isIterable } from '../src/is-iterable.js'
+
+t.equal(isIterable([]), true)
+t.equal(isIterable({}), false)
+t.equal(isIterable(Buffer.from('asdf')), true)
+// this is weird, but strings are scalars, even though
+// they are technically iterable when an object.
+t.equal(isIterable('asdf'), false)
+t.equal(isIterable(new String('asdf')), true)
