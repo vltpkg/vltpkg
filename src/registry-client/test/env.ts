a70ea7537b5921a42e6500d0d5ad3bbc4730e14b
isaacs <i@izs.me>
2025-01-09 10:24:18 -0800

    registry-client: test for env setting

---
+++ src/registry-client/test/env.ts
@@ -0,0 +1,52 @@
+import t from 'tap'
+
+t.test('deno', async t => {
+  const g = globalThis as typeof globalThis & { Deno: any }
+  t.intercept(g, 'Deno', { value: {} })
+  t.intercept(process.versions, 'deno', { value: '7.8.9' })
+  const env = await t.mockImport('../src/env.js')
+  t.strictSame(
+    env,
+    Object.assign(Object.create(null), {
+      bun: undefined,
+      deno: '7.8.9',
+      isBun: false,
+      isDeno: true,
+      isNode: false,
+      node: undefined,
+    }),
+  )
+})
+
+t.test('bun', async t => {
+  const g = globalThis as typeof globalThis & { Bun: any }
+  t.intercept(g, 'Bun', { value: {} })
+  t.intercept(process.versions, 'bun', { value: '7.8.9' })
+  const env = await t.mockImport('../src/env.js')
+  t.strictSame(
+    env,
+    Object.assign(Object.create(null), {
+      bun: '7.8.9',
+      deno: undefined,
+      isBun: true,
+      isDeno: false,
+      isNode: false,
+      node: undefined,
+    }),
+  )
+})
+
+t.test('node', async t => {
+  const env = await t.mockImport('../src/env.js')
+  t.strictSame(
+    env,
+    Object.assign(Object.create(null), {
+      bun: undefined,
+      deno: undefined,
+      isBun: false,
+      isDeno: false,
+      isNode: true,
+      node: process.versions.node,
+    }),
+  )
+})
