dbfb91b193de8a8326a0eb1f94e839eead0d015e
Luke Karrys <luke@lukekarrys.com>
2025-01-09 09:26:29 -0700

    docs: use latest typedoc-plugin-markdown instead of custom theme (#308)
    https://github.com/typedoc2md/typedoc-plugin-markdown/issues/742

--- www/docs/typedoc/constants.mjs
+++ www/docs/typedoc/constants.mjs
@@ -2,4 +2,3 @@ export const modulesFileName = 'reference'
 export const entryFileName = 'index'
 export const typedocBasePath = 'packages'
 export const typedocContentPath = `src/content/docs/${typedocBasePath}`
-export const theme = 'vlt-docs-theme'
