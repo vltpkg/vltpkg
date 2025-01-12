7c1482b468b3071225a9cc44f5bc997b5cd6e352
Ruy Adorno <ruy@vlt.sh>
2025-01-11 22:31:08 -0500

    graph: reify uses loaded resolved & integrity
    Add the missing default registry `resolved` values to nodes loaded
    from a lockfile and uses both `resolved` & `integrity` during reify
    in order to skip loading manifests from cache when installing from
    a lockfile.

--- src/graph/tap-snapshots/test/ideal/build.ts.test.cjs
+++ src/graph/tap-snapshots/test/ideal/build.ts.test.cjs
@@ -31,6 +31,7 @@ exports[`test/ideal/build.ts > TAP > build from lockfile > should build an ideal
       Edge spec(foo@^1.0.0) -prod-> to: Node {
         id: '··foo@1.0.0',
         location: './node_modules/.vlt/··foo@1.0.0/node_modules/foo',
+        resolved: 'https://registry.npmjs.org/foo/-/foo-1.0.0.tgz',
         integrity: 'sha512-6/mh1E2u2YgEsCHdY0Yx5oW+61gZU+1vXaoiHHrpKeuRNNgFvS+/jrwHiQhB5apAf5oB7UB7E19ol2R2LKH8hQ=='
       }
     ]
