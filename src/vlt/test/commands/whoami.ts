1ad8aa2425f740cd144b9dc51e7016114de98db0
isaacs <i@izs.me>
2025-01-22 13:02:58 -0800

    vlt: add login, logout, whoami

---
+++ src/vlt/test/commands/whoami.ts
@@ -0,0 +1,26 @@
+import t from 'tap'
+import { type LoadedConfig } from '../../src/types.js'
+
+const { usage, command } = await t.mockImport<
+  typeof import('../../src/commands/whoami.js')
+>('../../src/commands/whoami.js', {
+  '@vltpkg/registry-client': {
+    RegistryClient: class {
+      async request(url: string | URL, config: { cache: false }) {
+        t.equal(String(url), 'https://registry/-/whoami')
+        t.strictSame(config, { cache: false })
+        return {
+          json: () => ({ username: 'username' }),
+        }
+      }
+    },
+  },
+})
+
+t.matchSnapshot(usage())
+const logs = t.capture(console, 'log').args
+await command({
+  options: { registry: 'https://registry/' },
+} as unknown as LoadedConfig)
+
+t.strictSame(logs(), [['username']])
