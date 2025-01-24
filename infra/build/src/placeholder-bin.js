16c62eec7b954011cb68d6493035ae20d5b11ee1
Luke Karrys <luke@lukekarrys.com>
2025-01-24 13:29:38 -0700

    specify in postinstall error if current platform is supported or not

--- infra/build/src/placeholder-bin.js
+++ infra/build/src/placeholder-bin.js
@@ -1,7 +1,27 @@
 #!/usr/bin/env node
 
-console.log(`The correct platform specific binary was not found.
-This could be due to --no-optional or --ignore-scripts being used.
-Try again without either of those flags.`)
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+const { readFileSync } = require('fs')
+const { resolve } = require('path')
+const os = require('os')
+
+const validPlatforms = Object.keys(
+  JSON.parse(readFileSync(resolve(__dirname, 'package.json'), 'utf8'))
+    .optionalDependencies,
+).map(name => name.replace('@vltpkg/cli-', ''))
+
+const currentPlatform = `${os.platform()}-${os.arch()}`
+
+console.error(
+  validPlatforms.includes(currentPlatform) ?
+    `This is a supported platform but the matching binary was not found.
+This could be due to --no-optional or --ignore-scripts being used when installing.
+Try again without either of those flags.
+Otherwise check for error logs from the postinstall script.`
+  : `The current platform is not supported by this package.`,
+)
+console.error(`Valid platforms: ${JSON.stringify(validPlatforms)}`)
+console.error(`Current platform: ${JSON.stringify(currentPlatform)}`)
 
 process.exit(1)
