781aa8cacc61a49272c48bd974cc23bf57b2e745
Jason Korol <ejkorol@gmail.com>
2025-01-30 13:46:42 -0500

    adds icons
    - package managers & runtimes
    - adds snapshot tests for icons

---
+++ src/gui/test/components/icons/yarn.tsx
@@ -0,0 +1,23 @@
+import { afterEach, expect, test } from 'vitest'
+import { cleanup, render } from '@testing-library/react'
+import html from 'diffable-html'
+import { Yarn } from '@/components/icons/index.js'
+
+expect.addSnapshotSerializer({
+  serialize: v => html(v),
+  test: () => true,
+})
+
+afterEach(() => {
+  cleanup()
+})
+
+test('yarn icon render default', () => {
+  render(<Yarn />)
+  expect(window.document.body.innerHTML).toMatchSnapshot()
+})
+
+test('yarn icon render custom class', () => {
+  render(<Yarn className="custom-class" />)
+  expect(window.document.body.innerHTML).toMatchSnapshot()
+})
