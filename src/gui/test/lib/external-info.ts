8f57318be5b72e2189fba7169d76765c5f716d10
Ruy Adorno <ruy@vlt.sh>
2025-01-31 11:18:07 -0500

    Add selected item details (#368)
    * dep-id: add hydrate method to browser variant
    
    Add the `hydrate()` method to the isomorphic module that
    is exported as `@vltpkg/dep-id/browser` using
    `@vltpkg/spec/browser` in order to return a parsed spec.
    
    * types: add author and contributors fields
    
    * gui: add selected item details
    
    Add extra details that are fetched from remote APIs such
    as GitHub and the npm registry.
    
    Fixes: https://github.com/vltpkg/vltpkg/issues/245

---
+++ src/gui/test/lib/external-info.ts
@@ -0,0 +1,409 @@
+// write vitest tests for the external-info module
+import { test, expect, vi } from 'vitest'
+import { Spec } from '@vltpkg/spec/browser'
+import {
+  fetchDetails,
+  type DetailsInfo,
+  readAuthor,
+  readRepository,
+  retrieveGitHubAPIUrl,
+} from '@/lib/external-info.js'
+
+const sleep = (time: number) =>
+  new Promise<void>(resolve => {
+    setTimeout(() => resolve(), time)
+  })
+
+global.fetch = vi.fn(async url => ({
+  json: async () => {
+    switch (url) {
+      case 'https://api.npmjs.org/versions/my-package/last-week': {
+        return {
+          package: 'my-package',
+          downloads: { '1.0.0': 100 },
+        }
+      }
+      case 'https://registry.npmjs.org/my-package/1.0.0': {
+        return {
+          _npmUser: {
+            name: 'Ruy Adorno',
+            email: 'ruyadorno@example.com',
+          },
+        }
+      }
+      case 'https://api.npmjs.org/versions/async-request/last-week': {
+        await sleep(Math.random() * 100)
+        return {
+          package: 'my-package',
+          downloads: { '1.0.0': 100 },
+        }
+      }
+      case 'https://registry.npmjs.org/async-request/1.0.0': {
+        await sleep(Math.random() * 100)
+        return {
+          author: {
+            name: 'Ruy Adorno',
+            email: 'ruyadorno@example.com',
+          },
+          _npmUser: {
+            name: 'Ruy Adorno',
+            email: 'ruyadorno@example.com',
+          },
+        }
+      }
+      case 'https://registry.npmjs.org/missing-downloads/1.0.0': {
+        return {
+          _npmUser: {
+            name: 'Foo',
+            email: 'foo@bar.ca',
+          },
+        }
+      }
+      case 'https://registry.npmjs.org/favicon-repo-in-local-manifest/1.0.0': {
+        return {
+          _npmUser: {
+            name: 'Ruy Adorno',
+            email: 'ruyadorno@example.com',
+          },
+        }
+      }
+      case 'https://api.github.com/repos/ruyadorno/favicon-repo-in-local-manifest': {
+        return {
+          owner: {
+            avatar_url:
+              'https//example.com/favicon-repo-in-local-manifest.jpg',
+            login: 'ruyadorno',
+          },
+        }
+      }
+      case 'https://registry.npmjs.org/favicon-repo-in-remote-manifest/1.0.0': {
+        return {
+          repository:
+            'git+ssh://github.com/ruyadorno/favicon-repo-in-remote-manifest.git',
+        }
+      }
+      case 'https://api.github.com/repos/ruyadorno/favicon-repo-in-remote-manifest': {
+        return {
+          owner: {
+            avatar_url:
+              'https//example.com/favicon-repo-in-remote-manifest.jpg',
+            login: 'ruyadorno',
+          },
+        }
+      }
+      default: {
+        throw new Error('unexpected url')
+      }
+    }
+  },
+})) as unknown as typeof global.fetch
+
+test('readAuthor from author string pattern', () => {
+  const author =
+    'Ruy Adorno <ruyadorno@example.com> (https://ruyadorno.com)'
+  expect(readAuthor(author)).toEqual({
+    name: 'Ruy Adorno',
+    email: 'ruyadorno@example.com',
+    url: 'https://ruyadorno.com',
+  })
+})
+
+test('readAuthor from author string pattern missing url', () => {
+  const author = 'Ruy Adorno <ruyadorno@example.com>'
+  expect(readAuthor(author)).toEqual({
+    name: 'Ruy Adorno',
+    email: 'ruyadorno@example.com',
+  })
+})
+
+test('readAuthor from author string pattern missing email', () => {
+  const author = 'Ruy Adorno (https://ruyadorno.com)'
+  expect(readAuthor(author)).toEqual({
+    name: 'Ruy Adorno',
+    url: 'https://ruyadorno.com',
+  })
+})
+
+test('readAuthor from author string pattern name-only', () => {
+  const author = 'Ruy Adorno'
+  expect(readAuthor(author)).toEqual({
+    name: 'Ruy Adorno',
+  })
+})
+
+test('readAuthor from author object pattern', () => {
+  const author = {
+    name: 'Ruy Adorno',
+    email: 'ruyadorno@example.com',
+    url: 'https://ruyadorno.com',
+  }
+  expect(readAuthor(author)).toEqual({
+    name: 'Ruy Adorno',
+    email: 'ruyadorno@example.com',
+    url: 'https://ruyadorno.com',
+  })
+})
+
+test('readAuthor from author object pattern alternative web field', () => {
+  const author = {
+    name: 'Ruy Adorno',
+    email: 'ruyadorno@example.com',
+    web: 'https://ruyadorno.com',
+  }
+  expect(readAuthor(author)).toEqual({
+    name: 'Ruy Adorno',
+    email: 'ruyadorno@example.com',
+    url: 'https://ruyadorno.com',
+  })
+})
+
+test('readAuthor from author object pattern alternative mail field', () => {
+  const author = {
+    name: 'Ruy Adorno',
+    mail: 'ruyadorno@example.com',
+    web: 'https://ruyadorno.com',
+  }
+  expect(readAuthor(author)).toEqual({
+    name: 'Ruy Adorno',
+    email: 'ruyadorno@example.com',
+    url: 'https://ruyadorno.com',
+  })
+})
+
+test('readAuthor from author object pattern name-only', () => {
+  const author = {
+    name: 'Ruy Adorno',
+  }
+  expect(readAuthor(author)).toEqual({
+    name: 'Ruy Adorno',
+  })
+})
+
+test('readRepository from string pattern', () => {
+  const repository = 'git+ssh://github.com/org/repo.git'
+  expect(readRepository(repository)).toEqual(
+    'git+ssh://github.com/org/repo.git',
+  )
+})
+
+test('readRepository from object pattern', () => {
+  const repository = {
+    type: 'git',
+    url: 'git+ssh://github.com/org/repo.git',
+  }
+  expect(readRepository(repository)).toEqual(
+    'git+ssh://github.com/org/repo.git',
+  )
+})
+
+test('retrieveGitHubAPIUrl from git-style url', () => {
+  expect(
+    retrieveGitHubAPIUrl('git+ssh://github.com/org/repo.git'),
+  ).toEqual('https://api.github.com/repos/org/repo')
+})
+
+test('retrieveGitHubAPIUrl from github shortener path', () => {
+  expect(retrieveGitHubAPIUrl('org/repo')).toEqual(
+    'https://api.github.com/repos/org/repo',
+  )
+})
+
+test('retrieveGitHubAPIUrl from full github url', () => {
+  expect(retrieveGitHubAPIUrl('https://github.com/org/repo')).toEqual(
+    'https://api.github.com/repos/org/repo',
+  )
+})
+
+test('retrieveGitHubAPIUrl from https-git-style url', () => {
+  expect(retrieveGitHubAPIUrl('https://github.com/org/repo')).toEqual(
+    'https://api.github.com/repos/org/repo',
+  )
+})
+
+test('fetchDetails returns the correct details', async () => {
+  const mani = {
+    name: 'my-package',
+    version: '1.0.0',
+    description: 'my-package description',
+    license: 'MIT',
+    author: 'Ruy Adorno',
+  }
+  const spec = Spec.parse('my-package', '1.0.0')
+  const res: any = {}
+  for await (const details of fetchDetails(spec, mani)) {
+    for (const key of Object.keys(details)) {
+      res[key] = details[key as keyof DetailsInfo]
+    }
+  }
+  expect(res).toEqual({
+    author: {
+      name: 'Ruy Adorno',
+    },
+    downloads: {
+      weekly: 100,
+    },
+    publisher: {
+      name: 'Ruy Adorno',
+      email: 'ruyadorno@example.com',
+    },
+    publisherAvatar: {
+      src: 'https://gravatar.com/avatar/1f87aae035b22253b6f4051f68ade60229308d26c514816de0046566cdebe8fa?d=404',
+      alt: "Ruy Adorno's avatar",
+    },
+  })
+})
+
+test('fetchDetails from various async requests', async () => {
+  const mani = { name: 'async-request', version: '1.0.0' }
+  const spec = Spec.parse('async-request', '1.0.0')
+  const res: any = {}
+  for await (const details of fetchDetails(spec, mani)) {
+    for (const key of Object.keys(details)) {
+      res[key] = details[key as keyof DetailsInfo]
+    }
+  }
+  expect(res).toEqual({
+    author: {
+      name: 'Ruy Adorno',
+      email: 'ruyadorno@example.com',
+    },
+    downloads: {
+      weekly: 100,
+    },
+    publisher: {
+      name: 'Ruy Adorno',
+      email: 'ruyadorno@example.com',
+    },
+    publisherAvatar: {
+      src: 'https://gravatar.com/avatar/1f87aae035b22253b6f4051f68ade60229308d26c514816de0046566cdebe8fa?d=404',
+      alt: "Ruy Adorno's avatar",
+    },
+  })
+})
+
+test('unable to fetch remote details', async () => {
+  const mani = {
+    name: 'missing-info',
+    version: '1.0.0',
+    author: 'Ruy',
+  }
+  const spec = Spec.parse('missing-info', '1.0.0')
+  const res: any = {}
+  for await (const details of fetchDetails(spec, mani)) {
+    for (const key of Object.keys(details)) {
+      res[key] = details[key as keyof DetailsInfo]
+    }
+  }
+  expect(res).toEqual({
+    author: { name: 'Ruy' },
+  })
+})
+
+test('no info to retrieve from details', async () => {
+  const mani = { name: 'missing-info', version: '1.0.0' }
+  const spec = Spec.parse('missing-info', '1.0.0')
+  const res: any = {}
+  for await (const details of fetchDetails(spec, mani)) {
+    for (const key of Object.keys(details)) {
+      res[key] = details[key as keyof DetailsInfo]
+    }
+  }
+  expect(res).toEqual({})
+})
+
+test('fetchDetails with missing downloads', async () => {
+  const mani = { name: 'missing-downloads', version: '1.0.0' }
+  const spec = Spec.parse('missing-downloads', '1.0.0')
+  const res: any = {}
+  for await (const details of fetchDetails(spec, mani)) {
+    for (const key of Object.keys(details)) {
+      res[key] = details[key as keyof DetailsInfo]
+    }
+  }
+  expect(res).toEqual({
+    publisher: {
+      name: 'Foo',
+      email: 'foo@bar.ca',
+    },
+    publisherAvatar: {
+      src: 'https://gravatar.com/avatar/9d3c411537fa65b330e063d79b17e5568a0df4cd8a4174985aa94f4c35ceba20?d=404',
+      alt: "Foo's avatar",
+    },
+  })
+})
+
+test('fetchDetails with repository info in local manifest', async () => {
+  const mani = {
+    name: 'favicon-repo-in-local-manifest',
+    version: '1.0.0',
+    repository: {
+      type: 'git',
+      url: 'git+ssh://github.com/ruyadorno/favicon-repo-in-local-manifest.git',
+    },
+  }
+  const spec = Spec.parse('favicon-repo-in-local-manifest', '1.0.0')
+  const res: any = {}
+  for await (const details of fetchDetails(spec, mani)) {
+    for (const key of Object.keys(details)) {
+      res[key] = details[key as keyof DetailsInfo]
+    }
+  }
+  expect(res).toEqual({
+    favicon: {
+      src: 'https//example.com/favicon-repo-in-local-manifest.jpg',
+      alt: "ruyadorno's avatar",
+    },
+    publisher: {
+      name: 'Ruy Adorno',
+      email: 'ruyadorno@example.com',
+    },
+    publisherAvatar: {
+      src: 'https://gravatar.com/avatar/1f87aae035b22253b6f4051f68ade60229308d26c514816de0046566cdebe8fa?d=404',
+      alt: "Ruy Adorno's avatar",
+    },
+  })
+})
+
+test('fetchDetails with repository info in remote manifest', async () => {
+  const mani = {
+    name: 'favicon-repo-in-remote-manifest',
+    version: '1.0.0',
+  }
+  const spec = Spec.parse('favicon-repo-in-remote-manifest', '1.0.0')
+  const res: any = {}
+  for await (const details of fetchDetails(spec, mani)) {
+    for (const key of Object.keys(details)) {
+      res[key] = details[key as keyof DetailsInfo]
+    }
+  }
+  expect(res).toEqual({
+    favicon: {
+      src: 'https//example.com/favicon-repo-in-remote-manifest.jpg',
+      alt: "ruyadorno's avatar",
+    },
+  })
+})
+
+test('fetchDetails favicon defaults to img shortcut', async () => {
+  const mani = {
+    name: 'favicon-fallback',
+    version: '1.0.0',
+    repository: {
+      type: 'git',
+      url: 'git+ssh://github.com/ruyadorno/favicon-fallback.git',
+    },
+  }
+  const spec = Spec.parse('favicon-fallback', '1.0.0')
+  const res: any = {}
+  for await (const details of fetchDetails(spec, mani)) {
+    for (const key of Object.keys(details)) {
+      res[key] = details[key as keyof DetailsInfo]
+    }
+  }
+  expect(res).toEqual({
+    favicon: {
+      src: 'https://github.com/ruyadorno.png?size=128',
+      alt: 'avatar',
+    },
+  })
+})
