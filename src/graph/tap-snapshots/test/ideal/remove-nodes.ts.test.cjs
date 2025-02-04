0163006629e8d81fbccf86c409b4ca59e0d1a8a8
Ruy Adorno <ruy@vlt.sh>
2025-02-04 16:30:21 -0500

    graph: add ideal/remove-nodes tests (#377)

---
+++ src/graph/tap-snapshots/test/ideal/remove-nodes.ts.test.cjs
@@ -0,0 +1,16 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/ideal/remove-nodes.ts > TAP > removeNodes > should remove the node and edge from the graph 1`] = `
+Object {
+  "edges": Object {},
+  "nodes": Object {},
+  "options": Object {
+    "registries": Object {},
+  },
+}
+`
