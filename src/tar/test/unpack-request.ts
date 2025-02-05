df8dec85f359790f33c0e1d00b530921969b7dea
Luke Karrys <luke@lukekarrys.com>
2025-02-05 10:54:47 -0700

    test: only allow floating promises from tap.[test|skip]

--- src/tar/test/unpack-request.ts
+++ src/tar/test/unpack-request.ts
@@ -1,13 +1,15 @@
 import t from 'tap'
 import { UnpackRequest } from '../dist/esm/unpack-request.js'
 
-const a = new UnpackRequest(Buffer.from('aaaa'), './a')
-const b = new UnpackRequest(Buffer.from('bbbb'), './b')
+t.test('basic', async t => {
+  const a = new UnpackRequest(Buffer.from('aaaa'), './a')
+  const b = new UnpackRequest(Buffer.from('bbbb'), './b')
 
-t.equal(a.id, 1)
-t.equal(b.id, 2)
+  t.equal(a.id, 1)
+  t.equal(b.id, 2)
 
-t.rejects(a.promise, new Error('reason'))
-t.resolves(b.promise)
-a.reject(new Error('reason'))
-b.resolve()
+  await t.rejects(a.promise, new Error('reason'))
+  await t.resolves(b.promise)
+  a.reject(new Error('reason'))
+  b.resolve()
+})
