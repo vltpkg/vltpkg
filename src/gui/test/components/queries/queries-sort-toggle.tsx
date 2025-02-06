4d9fd5f1646a4a2d5db369e3ae922a2dc34d7285
Jason Korol <ejkorol@gmail.com>
2025-02-06 14:28:19 -0500

    adds tests for `queries-sort-toggle`

---
+++ src/gui/test/components/queries/queries-sort-toggle.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { vi, describe, it, expect, afterEach } from 'vitest'
+import { cleanup, render } from '@testing-library/react'
+import html from 'diffable-html'
+import { QueriesSortToggle } from '@/components/queries/queries-sort-toggle.jsx'
+
+vi.mock('@/components/ui/toggle.jsx', () => ({
+  Toggle: 'gui-toggle',
+}))
+
+vi.mock('lucide-react', () => ({
+  ArrowDownAz: 'gui-arrow-down-az-icon',
+  ArrowDownZa: 'gui-arrow-down-za-icon',
+}))
+
+expect.addSnapshotSerializer({
+  serialize: v => html(v),
+  test: () => true,
+})
+
+afterEach(() => {
+  cleanup()
+})
+
+describe('queries-sort-toggle', () => {
+  it('should render correctly', () => {
+    const mockSetFilteredItems = vi.fn()
+
+    const mockFilteredItems = [
+      {
+        name: 'mock-data-1',
+      },
+      {
+        name: 'mock-data-2',
+      },
+    ]
+
+    const Container = () => {
+      return (
+        <QueriesSortToggle
+          sortKey="name"
+          setFilteredItems={mockSetFilteredItems}
+          filteredItems={mockFilteredItems}
+        />
+      )
+    }
+
+    const { container } = render(<Container />)
+
+    expect(container.innerHTML).toMatchSnapshot()
+  })
+})
