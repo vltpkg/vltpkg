3c970389af8040c6b09113d8c146036a9647c137
Luke Karrys <luke@lukekarrys.com>
2025-02-20 14:29:08 -0700

    lint: dont lint for unresolved files in benchmarks (#468)

--- src/semver/benchmarks/this.js
+++ src/semver/benchmarks/this.js
@@ -1,5 +1,4 @@
 // This must import the dist file since it is run in a loop by hyperfine
 // and we don't want type stripping to contribute to the benchmark.
-
 import { satisfies } from '../dist/esm/index.js'
 console.log(satisfies('1.2.3', '^1.0'))
