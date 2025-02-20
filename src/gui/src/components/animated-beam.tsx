731182de7679f38baf198e2b9ec18930b88ae505
Luke Karrys <luke@lukekarrys.com>
2025-02-20 15:03:20 -0700

    fixup! feat: `select` ui chevron animation

--- src/gui/src/components/animated-beam.tsx
+++ src/gui/src/components/animated-beam.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion'
-import { type RefObject, useEffect, useId, useState } from 'react'
+import { useEffect, useId, useState } from 'react'
+import type { RefObject } from 'react'
 
 import { cn } from '@/lib/utils.js'
 
