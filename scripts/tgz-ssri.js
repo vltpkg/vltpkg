357016bc83ca45b9c449fa7da2646b7bce217e55
Luke Karrys <luke@lukekarrys.com>
2025-03-07 11:57:15 -0700

    release: list integrity next to package name/version

--- scripts/tgz-ssri.js
+++ scripts/tgz-ssri.js
@@ -3,10 +3,6 @@ import ssri from 'ssri'
 import { getWorkspaces, readPkgJson } from './utils.js'
 import { join } from 'node:path'
 
-const workspaces = getWorkspaces()
-  .map(dir => [dir, readPkgJson(dir)])
-  .sort(([, a], [, b]) => a.name.localeCompare(b.name))
-
 const findTgz = dir => {
   if (existsSync(dir)) {
     const tgz = readdirSync(dir).find(f => f.endsWith('.tgz'))
@@ -16,21 +12,38 @@ const findTgz = dir => {
   }
 }
 
+const findPackageTgz = dir => {
+  const pkg = readPkgJson(dir)
+  return [
+    findTgz(
+      pkg.publishConfig?.directory ?
+        join(dir, pkg.publishConfig.directory)
+      : dir,
+    ),
+    pkg,
+  ]
+}
+
+const ssriFromFile = file =>
+  ssri.fromData(readFileSync(file)).toString()
+
+if (process.argv[2]) {
+  const [tarball] = findPackageTgz(process.argv[2])
+  console.log(ssriFromFile(tarball))
+  process.exit(0)
+}
+
 const res = []
 
-for (const [dir, pkg] of workspaces) {
-  const tarball = findTgz(
-    pkg.publishConfig?.directory ?
-      join(dir, pkg.publishConfig.directory)
-    : dir,
-  )
+for (const dir of getWorkspaces()) {
+  const [tarball, pkg] = findPackageTgz(dir)
 
   if (!tarball) {
     continue
   }
 
   res.push(`${pkg.name}@${pkg.version}`)
-  res.push(ssri.fromData(readFileSync(tarball)).toString())
+  res.push(ssriFromFile(tarball))
   res.push('')
 }
 
