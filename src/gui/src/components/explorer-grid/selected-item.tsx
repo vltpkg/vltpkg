3eb9d24fbf7f2b2bfa4ea780433ea1c5f8953989
Jason Korol <ejkorol@gmail.com>
2025-03-07 22:53:58 -0500

    feat: animates `tab` primitive

--- src/gui/src/components/explorer-grid/selected-item.tsx
+++ src/gui/src/components/explorer-grid/selected-item.tsx
@@ -291,21 +291,22 @@ export const SelectedItem = ({ item }: GridItemOptions) => {
         </motion.div>
         <div className="w-full">
           <Tabs onValueChange={handleTabChange} value={activeTab}>
-            <TabsList variant="outline" className="w-full gap-2 px-6">
+            <TabsList variant="ghost" className="w-full gap-2 px-6">
               <TabsTrigger
-                variant="outline"
+                variant="ghost"
                 value="overview"
                 className="w-fit px-2">
                 Overview
               </TabsTrigger>
               <TabsTrigger
-                variant="outline"
+                variant="ghost"
                 value="package.json"
+                disabled={!item.to?.manifest}
                 className="w-fit px-2">
                 Manifest
               </TabsTrigger>
               <TabsTrigger
-                variant="outline"
+                variant="ghost"
                 value="versions"
                 disabled={
                   !details.versions || details.versions.length <= 0
@@ -329,15 +330,15 @@ export const SelectedItem = ({ item }: GridItemOptions) => {
                 </p>
               )}
             </TabsContent>
-            <TabsContent value="package.json">
-              <CodeBlock
-                code={
-                  item.to?.manifest ?
-                    JSON.stringify(item.to.manifest, null, 2)
-                  : ''
-                }
-                lang="json"
-              />
+            <TabsContent
+              value="package.json"
+              className="h-full rounded-b-lg bg-white dark:bg-black">
+              {item.to?.manifest && (
+                <CodeBlock
+                  code={JSON.stringify(item.to.manifest, null, 2)}
+                  lang="json"
+                />
+              )}
             </TabsContent>
             <TabsContent value="versions" className="px-6 py-4">
               <section className="flex flex-col gap-4">
