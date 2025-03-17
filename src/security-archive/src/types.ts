0ee0ebc718d5926b53786dc96f26ca32553ee437
Ruy Adorno <ruy@vlt.sh>
2025-03-17 17:27:57 -0400

    security-archive: add a security db cache system (#518)
    Add a new package `@vltpkg/security-archive` that handles accessing,
    fetching remote data and persisting security report data on
    packages from a given graph.
    
    Usage:
    
        const archive = await SecurityArchive.start({
            graph,
            specOptions
        })
        const report = archive.get(node.id)

---
+++ src/security-archive/src/types.ts
@@ -0,0 +1,84 @@
+import { error } from '@vltpkg/error-cause'
+import type { DepID } from '@vltpkg/dep-id'
+import type { GraphLike } from '@vltpkg/graph'
+import type { SpecOptions } from '@vltpkg/spec'
+
+/**
+ * Parameter options for initializing a security archive.
+ */
+export type SecurityArchiveRefreshOptions = {
+  /**
+   * A @link{GraphLike} instance to find what packages the
+   * security archive should have.
+   */
+  graph: GraphLike
+  /**
+   * A @link{SpecOptions} instance to use for resolving dependencies.
+   */
+  specOptions: SpecOptions
+}
+
+/**
+ * An interface for interacting with a security archive.
+ */
+export interface SecurityArchiveLike {
+  get: (depId: DepID) => PackageReportData | undefined
+  set: (depId: DepID, data: PackageReportData) => void
+  delete: (depId: DepID) => void
+  has: (depId: DepID) => boolean
+  clear: () => void
+}
+
+/**
+ * Package alert extra information.
+ */
+export type PackageAlertProps = {
+  lastPublish: string
+}
+
+/**
+ * A known alert for a given package.
+ */
+export type PackageAlert = {
+  key: string
+  type: string
+  severity: 'low' | 'middle' | 'high' | 'critical'
+  category: string
+  props: PackageAlertProps
+}
+
+/**
+ * The report data for a given package.
+ */
+export type PackageReportData = {
+  id: string
+  author: string[]
+  size: number
+  type: 'npm'
+  namespace?: `@${string}`
+  name: string
+  version: string
+  license: string
+  alerts: PackageAlert[]
+}
+
+export const isPackageReportData = (
+  o: unknown,
+): o is PackageReportData =>
+  typeof o === 'object' &&
+  o != null &&
+  'id' in o &&
+  'type' in o &&
+  'name' in o &&
+  'version' in o &&
+  'alerts' in o &&
+  o.type === 'npm'
+
+export const asPackageReportData = (
+  o: unknown,
+): PackageReportData => {
+  if (!isPackageReportData(o)) {
+    throw error('Invalid package report data', { found: o })
+  }
+  return o
+}
