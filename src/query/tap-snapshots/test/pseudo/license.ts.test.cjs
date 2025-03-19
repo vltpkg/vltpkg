4976636222cc02c8804299c8792972bc4ca4a3f4
Ruy Adorno <ruy@vlt.sh>
2025-03-19 18:11:02 -0300

    query: add license selector
    Add a `:license(<kind>)` selector that filters nodes based on
    license-related alert types from the security-archive.

---
+++ src/query/tap-snapshots/test/pseudo/license.ts.test.cjs
@@ -0,0 +1,29 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/pseudo/license.ts > TAP > selects packages with a specific license kind > filter out any node that does not have the license > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "e",
+    "e",
+  ],
+  "nodes": Array [
+    "e",
+  ],
+}
+`
+
+exports[`test/pseudo/license.ts > TAP > selects packages with a specific license kind > filter out using unquoted param > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "f",
+  ],
+  "nodes": Array [
+    "f",
+  ],
+}
+`
