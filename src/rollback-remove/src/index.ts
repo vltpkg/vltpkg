57cd5ed8d46c42c1bfbc94cc7882aed69af0f50b
Luke Karrys <luke@lukekarrys.com>
2025-03-24 16:37:14 -0700

    rollback-remove: fix running when compiled and in deno

--- src/rollback-remove/src/index.ts
+++ src/rollback-remove/src/index.ts
@@ -3,6 +3,10 @@ import { rename } from 'node:fs/promises'
 import { basename, dirname } from 'node:path'
 import { rimraf } from 'rimraf'
 import { __CODE_SPLIT_SCRIPT_NAME } from './remove.ts'
+import { pathToFileURL } from 'node:url'
+
+const isDeno =
+  (globalThis as typeof globalThis & { Deno?: any }).Deno != undefined
 
 export class RollbackRemove {
   #key = String(Math.random()).substring(2)
@@ -25,19 +29,42 @@ export class RollbackRemove {
     // nothing to confirm!
     if (!this.#paths.size) return
 
-    const child = spawn(
-      process.execPath,
-      [__CODE_SPLIT_SCRIPT_NAME],
-      {
-        stdio: ['pipe', 'ignore', 'ignore'],
-        detached: true,
-      },
-    )
-    child.unref()
+    const env = { ...process.env }
+    const args = []
+    // Deno on Windows does not support detached processes
+    // https://github.com/denoland/deno/issues/25867
+    // TODO: figure out something better to do here?
+    const detached = !(isDeno && process.platform === 'win32')
+    // When compiled the script to be run is passed as an
+    // environment variable and then routed by the main entry point
+    if (process.env.__VLT_INTERNAL_COMPILED) {
+      env.__VLT_INTERNAL_MAIN = pathToFileURL(
+        __CODE_SPLIT_SCRIPT_NAME,
+      ).toString()
+    } else {
+      // If we are running deno from source we need to add the
+      // unstable flags we need. The '-A' flag does not need
+      // to be passed in as Deno supplies that automatically.
+      if (isDeno) {
+        args.push(
+          '--unstable-node-globals',
+          '--unstable-bare-node-builtins',
+        )
+      }
+      args.push(__CODE_SPLIT_SCRIPT_NAME)
+    }
+    const child = spawn(process.execPath, args, {
+      stdio: ['pipe', 'ignore', 'ignore'],
+      detached,
+      env,
+    })
     for (const path of this.#paths.values()) {
       child.stdin.write(path + '\u0000')
     }
     child.stdin.end()
+    if (detached) {
+      child.unref()
+    }
     this.#paths.clear()
   }
 
