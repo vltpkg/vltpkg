de9e418241853330f25f5c9dadbd164222ae4ec1
Luke Karrys <luke@lukekarrys.com>
2025-03-24 16:37:14 -0700

    rollback-remove: fix running when compiled and in deno

--- src/rollback-remove/src/remove.ts
+++ src/rollback-remove/src/remove.ts
@@ -1,6 +1,11 @@
+import { pathToFileURL } from 'node:url'
 import { rimraf } from 'rimraf'
 
 export const __CODE_SPLIT_SCRIPT_NAME = import.meta.filename
+
+const isMain = (path?: string) =>
+  path === __CODE_SPLIT_SCRIPT_NAME ||
+  path === pathToFileURL(__CODE_SPLIT_SCRIPT_NAME).toString()
 
 // This is run as a background process, and all the paths to
 // be removed written into stdin. We can't pass on argv, because
@@ -17,6 +22,10 @@ const main = () => {
   })
 }
 
-if (process.argv[1] === import.meta.filename) {
+const g = globalThis as typeof globalThis & {
+  __VLT_INTERNAL_MAIN?: string
+}
+
+if (isMain(g.__VLT_INTERNAL_MAIN ?? process.argv[1])) {
   main()
 }
--- src/rollback-remove/test/index.ts
+++ src/rollback-remove/test/index.ts
@@ -1,70 +1,92 @@
+import assert from 'node:assert'
 import type { SpawnOptions } from 'node:child_process'
 import { readdirSync } from 'node:fs'
 import t from 'tap'
+import type { Test } from 'tap'
 
-const spawns: MockSpawn[] = []
-t.beforeEach(() => (spawns.length = 0))
-
-type MockSpawn = {
-  cmd: string
-  args: string[]
-  options: SpawnOptions
-  written: Buffer[]
-  stdinEnded: boolean
-  stdin: {
-    write: (chunk: Buffer) => any
-    end: () => any
-  }
-  reffed: boolean
-  unref: () => any
-}
-
-const mockSpawn = (
-  cmd: string,
-  args: string[],
-  options: SpawnOptions,
-): MockSpawn => {
-  const cp = {
-    cmd,
-    args,
-    options,
-    written: [] as Buffer[],
-    stdinEnded: false,
+const mockRollbackRemove = async (
+  t: Test,
+  { testdir }: { testdir: Parameters<Test['testdir']>[0] },
+) => {
+  type MockSpawn = {
+    cmd: string
+    args: string[]
+    options: SpawnOptions
+    written: Buffer[]
+    stdinEnded: boolean
     stdin: {
-      write: (chunk: Buffer) => cp.written.push(chunk),
-      end: () => (cp.stdinEnded = true),
-    },
-    reffed: true,
-    unref: () => (cp.reffed = false),
+      write: (chunk: Buffer) => any
+      end: () => any
+    }
+    reffed: boolean
+    unref: () => any
   }
-  spawns.push(cp)
-  return cp
+
+  let mockedSpawn: MockSpawn | null = null
+
+  const mockSpawn = (
+    cmd: string,
+    args: string[],
+    options: SpawnOptions,
+  ): MockSpawn => {
+    const cp = {
+      cmd,
+      args,
+      options,
+      written: [] as Buffer[],
+      stdinEnded: false,
+      stdin: {
+        write: (chunk: Buffer) => cp.written.push(chunk),
+        end: () => (cp.stdinEnded = true),
+      },
+      reffed: true,
+      unref: () => (cp.reffed = false),
+    }
+    mockedSpawn = cp
+    return cp
+  }
+
+  const { RollbackRemove } = await t.mockImport<
+    typeof import('../src/index.ts')
+  >('../src/index.ts', {
+    child_process: {
+      spawn: mockSpawn,
+    },
+  })
+
+  t.chdir(t.testdir(testdir))
+
+  return {
+    remover: new RollbackRemove(),
+    hasSpawned: () => mockedSpawn !== null,
+    getSpawn: () => {
+      assert(mockedSpawn)
+      return mockedSpawn
+    },
+  }
 }
 
 t.test('delete some stuff', async t => {
-  const { RollbackRemove } = await t.mockImport<
-    typeof import('../src/index.ts')
-  >('../src/index.ts', {
-    child_process: {
-      spawn: mockSpawn,
+  const { remover, getSpawn, hasSpawned } = await mockRollbackRemove(
+    t,
+    {
+      testdir: {
+        a: {
+          b: '',
+          c: '',
+        },
+        d: {
+          e: '',
+          f: '',
+        },
+      },
     },
-  })
-  t.chdir(
-    t.testdir({
-      a: {
-        b: '',
-        c: '',
-      },
-      d: {
-        e: '',
-        f: '',
-      },
-    }),
   )
-  const remover = new RollbackRemove()
+
   // no-op if nothing has been removed
   remover.confirm()
-  t.strictSame(spawns, [])
+  t.strictSame(hasSpawned(), false)
+
   await remover.rm('a/b')
   await remover.rm('d')
   await remover.rm('noent')
@@ -76,32 +98,24 @@ t.test('delete some stuff', async t => {
     new Set(readdirSync(t.testdirName)),
     new Set([/^\.VLT\.DELETE\.[0-9]+\.d$/, 'a']),
   )
+
   remover.confirm()
-  t.matchStrict(spawns, [
-    {
-      cmd: process.execPath,
-      args: [/[\\/]remove\.ts$/],
-      options: { detached: true },
-      written: [
-        /^a[\\/]\.VLT\.DELETE\.[0-9]+\.b\x00$/,
-        /^.[\\/]\.VLT\.DELETE\.[0-9]+\.d\x00$/,
-      ],
-      stdinEnded: true,
-      reffed: false,
-    },
-  ])
+  t.matchStrict(getSpawn(), {
+    cmd: process.execPath,
+    args: [/[\\/]remove\.ts$/],
+    options: { detached: true },
+    written: [
+      /^a[\\/]\.VLT\.DELETE\.[0-9]+\.b\x00$/,
+      /^.[\\/]\.VLT\.DELETE\.[0-9]+\.d\x00$/,
+    ],
+    stdinEnded: true,
+    reffed: false,
+  })
 })
 
 t.test('do not delete some stuff', async t => {
-  const { RollbackRemove } = await t.mockImport<
-    typeof import('../src/index.ts')
-  >('../src/index.ts', {
-    child_process: {
-      spawn: mockSpawn,
-    },
-  })
-  t.chdir(
-    t.testdir({
+  const { remover, hasSpawned } = await mockRollbackRemove(t, {
+    testdir: {
       a: {
         b: '',
         c: '',
@@ -110,12 +124,13 @@ t.test('do not delete some stuff', async t => {
         e: '',
         f: '',
       },
-    }),
-  )
-  const remover = new RollbackRemove()
+    },
+  })
+
   // no-op if nothing has been removed
   remover.confirm()
-  t.strictSame(spawns, [])
+  t.strictSame(hasSpawned(), false)
+
   await remover.rm('a/b')
   await remover.rm('d')
   await remover.rm('noent')
@@ -127,11 +142,113 @@ t.test('do not delete some stuff', async t => {
     new Set(readdirSync(t.testdirName)),
     new Set([/^\.VLT\.DELETE\.[0-9]+\.d$/, 'a']),
   )
+
   await remover.rollback()
-  t.strictSame(spawns, [])
+  t.strictSame(hasSpawned(), false)
   t.strictSame(new Set(readdirSync('a')), new Set(['b', 'c']))
   t.strictSame(
     new Set(readdirSync(t.testdirName)),
     new Set(['d', 'a']),
   )
+})
+
+t.test('compiled', async t => {
+  t.intercept(process, 'env', {
+    value: { __VLT_INTERNAL_COMPILED: 'true' },
+  })
+
+  const { remover, getSpawn } = await mockRollbackRemove(t, {
+    testdir: {
+      a: {
+        b: '',
+      },
+    },
+  })
+
+  await remover.rm('a/b')
+
+  remover.confirm()
+
+  t.matchStrict(getSpawn(), {
+    args: [],
+    options: {
+      detached: true,
+      env: {
+        __VLT_INTERNAL_MAIN: /^file:.*[\\/]remove\.ts$/,
+      },
+    },
+    reffed: false,
+  })
+})
+
+t.test('deno', async t => {
+  t.intercept(
+    globalThis as typeof globalThis & { Deno?: any },
+    'Deno',
+    {
+      value: {},
+    },
+  )
+
+  t.intercept(process, 'platform', { value: 'linux' })
+
+  const { remover, getSpawn } = await mockRollbackRemove(t, {
+    testdir: {
+      a: {
+        b: '',
+      },
+    },
+  })
+
+  await remover.rm('a/b')
+
+  remover.confirm()
+
+  t.matchStrict(getSpawn(), {
+    args: [
+      '--unstable-node-globals',
+      '--unstable-bare-node-builtins',
+      /[\\/]remove\.ts$/,
+    ],
+    options: {
+      detached: true,
+    },
+    reffed: false,
+  })
+})
+
+t.test('deno + windows', async t => {
+  t.intercept(
+    globalThis as typeof globalThis & { Deno?: any },
+    'Deno',
+    {
+      value: {},
+    },
+  )
+
+  t.intercept(process, 'platform', { value: 'win32' })
+
+  const { remover, getSpawn } = await mockRollbackRemove(t, {
+    testdir: {
+      a: {
+        b: '',
+      },
+    },
+  })
+
+  await remover.rm('a/b')
+
+  remover.confirm()
+
+  t.matchStrict(getSpawn(), {
+    args: [
+      '--unstable-node-globals',
+      '--unstable-bare-node-builtins',
+      /[\\/]remove\.ts$/,
+    ],
+    options: {
+      detached: false,
+    },
+    reffed: true,
+  })
 })
