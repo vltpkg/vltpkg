bc83f0b14ccad5e609b992a33199cf2cd3b4f93f
Jason Korol <ejkorol@gmail.com>
2025-03-26 11:24:29 -0400

    feat: moves 'versions' tab into separate component
    - creates tests and generates snapshots

---
+++ src/gui/test/components/explorer-grid/selected-item/tabs-versions.tsx
@@ -0,0 +1,105 @@
+import { test, expect, vi, beforeEach, afterEach } from 'vitest'
+import { cleanup, render } from '@testing-library/react'
+import html from 'diffable-html'
+import { useGraphStore as useStore } from '@/state/index.js'
+import { useSelectedItem } from '@/components/explorer-grid/selected-item/context.jsx'
+import { VersionsTabButton, VersionsTabContent } from '@/components/explorer-grid/selected-item/tabs-versions.jsx'
+import {
+  SELECTED_ITEM,
+  SELECTED_ITEM_DETAILS,
+} from './__fixtures__/item.ts'
+import type { DetailsInfo } from '@/lib/external-info.js'
+
+vi.mock(
+  '@/components/explorer-grid/selected-item/context.jsx',
+  () => ({
+    useSelectedItem: vi.fn(),
+    SelectedItemProvider: 'gui-selected-item-provider',
+  }),
+)
+
+vi.mock('@/components/ui/tabs.jsx', () => ({
+  TabsTrigger: 'gui-tabs-trigger',
+  TabsContent: 'gui-tabs-content',
+}))
+
+expect.addSnapshotSerializer({
+  serialize: v => html(v),
+  test: () => true,
+})
+
+beforeEach(() => {
+  vi.clearAllMocks()
+})
+
+afterEach(() => {
+  const CleanUp = () => (useStore(state => state.reset)(), '')
+  render(<CleanUp />)
+  cleanup()
+})
+
+test('VersionsTabButton renders default', () => {
+  vi.mocked(useSelectedItem).mockReturnValue({
+    selectedItem: SELECTED_ITEM,
+    selectedItemDetails: {
+      ...SELECTED_ITEM_DETAILS,
+      versions: ['1.0.0', '1.0.1', '1.0.2'],
+    } as DetailsInfo,
+    securityArchive: undefined,
+    activeTab: 'versions',
+    setActiveTab: vi.fn(),
+  })
+
+  const { container } = render(<VersionsTabButton />)
+  expect(container.innerHTML).toMatchSnapshot()
+})
+
+test('VersionsTabButton does not render when any verions are not available', () => {
+  vi.mocked(useSelectedItem).mockReturnValue({
+    selectedItem: SELECTED_ITEM,
+    selectedItemDetails: {
+      ...SELECTED_ITEM_DETAILS,
+      greaterVersions: undefined,
+      versions: undefined,
+    } as DetailsInfo,
+    securityArchive: undefined,
+    activeTab: 'versions',
+    setActiveTab: vi.fn(),
+  })
+
+  const { container } = render(<VersionsTabButton />)
+  expect(container.innerHTML).toBe("")
+})
+
+test('VersionsTabContent renders default', () => {
+  vi.mocked(useSelectedItem).mockReturnValue({
+    selectedItem: SELECTED_ITEM,
+    selectedItemDetails: {
+      ...SELECTED_ITEM_DETAILS,
+      versions: ['1.0.0', '1.0.1', '1.0.2'],
+    } as DetailsInfo,
+    securityArchive: undefined,
+    activeTab: 'versions',
+    setActiveTab: vi.fn(),
+  })
+
+  const { container } = render(<VersionsTabContent />)
+  expect(container.innerHTML).toMatchSnapshot()
+})
+
+test('VersionsTabContent does not render when any versions are not available', () => {
+  vi.mocked(useSelectedItem).mockReturnValue({
+    selectedItem: SELECTED_ITEM,
+    selectedItemDetails: {
+      ...SELECTED_ITEM_DETAILS,
+      greaterVersions: undefined,
+      versions: undefined,
+    } as DetailsInfo,
+    securityArchive: undefined,
+    activeTab: 'versions',
+    setActiveTab: vi.fn(),
+  })
+
+  const { container } = render(<VersionsTabContent />)
+  expect(container.innerHTML).toBe("")
+})
