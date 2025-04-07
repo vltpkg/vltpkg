d5ad1163ece83d16589eee322527aa87ff4f5683
Luke Karrys <luke@lukekarrys.com>
2025-04-07 13:54:43 -0700

    wip

--- src/cli-sdk/src/commands/run.ts
+++ src/cli-sdk/src/commands/run.ts
@@ -1,197 +1,42 @@
-import { PackageJson } from '@vltpkg/package-json'
 import { commandUsage } from '../config/usage.ts'
 import type { CommandFn, CommandUsage } from '../index.ts'
 import type { Views } from '../view.ts'
-import { error, isErrorWithCause } from '@vltpkg/error-cause'
-import { isRunResult } from '@vltpkg/run'
-import type {
-  run,
-  runFG,
-  exec,
-  execFG,
-  ExecOptions,
-  runExec,
-  runExecFG,
-  RunExecOptions,
-  RunOptions,
-  RunResult,
-} from '@vltpkg/run'
+import { ViewClass } from '../view.ts'
+import { error } from '@vltpkg/error-cause'
+import { isRunResult, run, runFG } from '@vltpkg/run'
+import type { RunFGResult, RunResult } from '@vltpkg/run'
 import { Monorepo } from '@vltpkg/workspaces'
 import type { Workspace } from '@vltpkg/workspaces'
-import { ansiToAnsi } from 'ansi-to-pre'
-import chalk from 'chalk'
-import type { LoadedConfig } from '../config/index.ts'
-import { stdout, stderr } from '../output.ts'
-import type { SpawnResultNoStdio } from '@vltpkg/promise-spawn'
+import { Box, render, Text } from 'ink'
+import type { Instance } from 'ink'
+import Spinner from 'ink-spinner'
+import {
+  createElement as $,
+  Fragment,
+  useLayoutEffect,
+  useState,
+} from 'react'
 
-type RunnerBG = typeof exec | typeof run | typeof runExec
-type RunnerFG = typeof execFG | typeof runExecFG | typeof runFG
-type RunnerOptions = ExecOptions & RunExecOptions & RunOptions
-type ExecResult =
-  | undefined
-  | SpawnResultNoStdio
-  | Record<string, RunResult>
+const App = () => {
+  useLayoutEffect(() => {}, [])
 
-class ExecCommand<B extends RunnerBG, F extends RunnerFG> {
-  bg: B
-  fg: F
-  arg0?: string
-  args: string[]
-  monorepo?: Monorepo
-  /** how many places are we doing things in? */
-  spaces: number
-  conf: LoadedConfig
-  projectRoot: string
+  return $(Box, null, $(Text, null, 'ok'))
+}
 
-  constructor(conf: LoadedConfig, bg: B, fg: F) {
-    this.conf = conf
-    this.bg = bg
-    this.fg = fg
-    const {
-      projectRoot,
-      positionals: [arg0, ...args],
-    } = conf
-    this.arg0 = arg0
-    this.args = args
+export class RunReporter extends ViewClass {
+  #instance: Instance | null = null
 
-    const paths = conf.get('workspace')
-    const groups = conf.get('workspace-group')
-    const recursive = conf.get('recursive')
-    this.monorepo =
-      paths?.length || groups?.length || recursive ?
-        Monorepo.load(projectRoot, { load: { paths, groups } })
-      : undefined
-    this.spaces = this.monorepo?.size ?? 1
-    this.projectRoot = projectRoot
+  start() {
+    this.#instance = render($(App))
   }
 
-  async run(): Promise<ExecResult> {
-    if (this.spaces === 1) {
-      const arg = this.fgArg()
-      if (!arg) return
-      const result = await this.fg(arg)
-      if (isRunResult(result)) {
-        this.setExitCode(result)
-      }
-      return result
-    }
-    const m = this.monorepo
-    if (!m || this.spaces === 0) {
-      throw error('no matching workspaces found', {
-        /* c8 ignore next - already guarded */
-        validOptions: [...(this.monorepo?.load().paths() ?? [])],
-      })
-    }
-    const arg0 = this.arg0
-    if (!arg0) {
-      this.noArgsMulti()
-      return
-    }
-    // run across workspaces
-    let failed = false
-    const resultMap = await m.run(async ws => {
-      if (!failed) stderr(`${ws.path} ${arg0}`)
-      const result = await this.bg(this.bgArg(ws)).catch(
-        (er: unknown) => {
-          if (isErrorWithCause(er) && isRunResult(er.cause)) {
-            this.printResult(ws, er.cause)
-          }
-          failed = true
-          throw er
-        },
-      )
-      if (!failed) this.printResult(ws, result)
-      return result
-    })
-
-    const results: Record<string, RunResult> = {}
-    for (const [ws, result] of resultMap) {
-      if (result.status === 0 && result.signal === null) {
-        result.stdout = ''
-        result.stderr = ''
-      }
-      results[ws.path] = result
-    }
-    return results
+  done() {
+    this.#instance?.unmount()
+    return undefined
   }
 
-  printResult(ws: Workspace, result: RunResult) {
-    if (result.status === 0 && result.signal === null) {
-      stdout(ws.path, 'ok')
-    } else {
-      stdout(chalk.bgWhiteBright.black.bold(ws.path + ' failure'), {
-        status: result.status,
-        signal: result.signal,
-      })
-      /* c8 ignore start */
-      if (result.stderr) stderr(ansiToAnsi(result.stderr))
-      if (result.stdout) stdout(ansiToAnsi(result.stdout))
-      /* c8 ignore stop */
-      this.setExitCode(result)
-    }
-  }
-
-  setExitCode(result: RunResult) {
-    /* c8 ignore next */
-    process.exitCode = process.exitCode || (result.status ?? 1)
-  }
-
-  /* c8 ignore start - env specific */
-  interactiveShell(): string {
-    return (
-      process.env.SHELL ??
-      this.conf.get('script-shell') ??
-      (process.platform === 'win32' ? 'cmd.exe' : '/bin/sh')
-    )
-  }
-  /* c8 ignore stop */
-
-  fgArg(): RunnerOptions | undefined {
-    const ws = this.monorepo?.values().next().value
-    const cwd = ws?.fullpath ?? this.projectRoot
-    const arg0 = this.arg0 ?? this.defaultArg0()
-    if (!arg0) {
-      return undefined
-    }
-
-    return {
-      cwd,
-      /* c8 ignore next - already guarded */
-      arg0,
-      args: this.args,
-      projectRoot: this.projectRoot,
-      packageJson: this.monorepo?.packageJson,
-      'script-shell':
-        this.arg0 ? this.conf.get('script-shell') : false,
-    }
-  }
-
-  /** If this returns undefined, then nothing to do */
-  bgArg(ws: Workspace): RunnerOptions {
-    /* c8 ignore start - already guarded */
-    if (!this.arg0)
-      throw error(
-        'Cannot spawn interactive shells in multiple workspaces',
-      )
-    /* c8 ignore stop */
-
-    return {
-      cwd: ws.fullpath,
-      acceptFail: !this.conf.get('bail'),
-      ignoreMissing: true,
-      arg0: this.arg0,
-      args: this.args,
-      projectRoot: this.projectRoot,
-      packageJson: this.monorepo?.packageJson,
-      'script-shell': this.conf.get('script-shell'),
-    }
-  }
-
-  // overridden in `vlt run` to print available scripts
-  noArgsMulti() {
-    throw error(
-      'Cannot spawn interactive shells in multiple workspaces',
-    )
+  error(err: unknown) {
+    this.#instance?.unmount(err as Error)
   }
 }
 
@@ -205,20 +50,20 @@ export const usage: CommandUsage = () =>
                   the script process.`,
   })
 
-export const views: Views<ExecResult> = {
-  json: g => g,
-  human: (res, _, conf) => {
-    if (conf.positionals.length === 0) {
-      return `Scripts available:\n  ${Object.entries(res ?? {})
-        .map(([k, v]) => `${k}: ${v}`)
-        .join('\n  ')}`
-    }
-  },
+const setExitCode = (result: RunResult) => {
+  process.exitCode = process.exitCode || (result.status ?? 1)
 }
 
 type CommandResult =
   | Record<string, string>
   | Map<Workspace, Record<string, string>>
+  | RunFGResult
+  | Map<Workspace, RunResult>
+
+export const views: Views<CommandResult> = {
+  json: g => g,
+  human: RunReporter,
+}
 
 export const command: CommandFn<CommandResult> = async conf => {
   const [arg0, ...args] = conf.positionals
@@ -231,72 +76,47 @@ export const command: CommandFn<CommandResult> = async conf => {
       Monorepo.load(projectRoot, { load: { paths, groups } })
     : undefined
 
-  const spaces = monorepo?.size ?? 1
-  // const ws = monorepo?.values().next().value
-  const cwd = ws?.fullpath ?? projectRoot
-
   if (!arg0) {
     return monorepo ?
         monorepo.runSync(ws => ws.manifest.scripts ?? {})
       : (packageJson.read(projectRoot).scripts ?? {})
   }
 
-  if (this.spaces === 1) {
-    const result = await this.fg({
+  const spaces = monorepo?.size ?? 1
+
+  if (!monorepo || spaces === 0) {
+    throw error('no matching workspaces found', {
+      validOptions: [...(monorepo?.load().paths() ?? [])],
+    })
+  }
+
+  if (spaces === 1) {
+    const ws = monorepo.values().next().value
+    const cwd = ws?.fullpath ?? projectRoot
+    const result = await runFG({
       cwd,
-      /* c8 ignore next - already guarded */
       arg0,
-      args: this.args,
-      projectRoot: this.projectRoot,
-      packageJson: this.monorepo?.packageJson,
-      'script-shell':
-        this.arg0 ? this.conf.get('script-shell') : false,
+      args,
+      projectRoot,
+      packageJson,
+      'script-shell': arg0 ? conf.get('script-shell') : false,
     })
     if (isRunResult(result)) {
-      this.setExitCode(result)
+      setExitCode(result)
     }
     return result
   }
-  const m = this.monorepo
-  if (!m || this.spaces === 0) {
-    throw error('no matching workspaces found', {
-      /* c8 ignore next - already guarded */
-      validOptions: [...(this.monorepo?.load().paths() ?? [])],
-    })
-  }
-  const arg0 = this.arg0
-  if (!arg0) {
-    this.noArgsMulti()
-    return
-  }
-  // run across workspaces
-  let failed = false
-  const resultMap = await m.run(async ws => {
-    if (!failed) stderr(`${ws.path} ${arg0}`)
-    const result = await this.bg(this.bgArg(ws)).catch(
-      (er: unknown) => {
-        if (isErrorWithCause(er) && isRunResult(er.cause)) {
-          this.printResult(ws, er.cause)
-        }
-        failed = true
-        throw er
-      },
-    )
-    if (!failed) this.printResult(ws, result)
-    return result
-  })
 
-  const results: Record<string, RunResult> = {}
-  for (const [ws, result] of resultMap) {
-    if (result.status === 0 && result.signal === null) {
-      result.stdout = ''
-      result.stderr = ''
-    }
-    results[ws.path] = result
-  }
-  return results
-
-  throw error('ok')
-
-  // return new RunCommand(conf).run()
+  return monorepo.run(ws =>
+    run({
+      cwd: ws.fullpath,
+      acceptFail: !conf.get('bail'),
+      ignoreMissing: true,
+      arg0,
+      args,
+      projectRoot,
+      packageJson,
+      'script-shell': conf.get('script-shell'),
+    }),
+  )
 }
