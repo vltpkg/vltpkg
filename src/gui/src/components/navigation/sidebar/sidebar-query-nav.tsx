4eaa8c9c9ec9f8ffed18a27ad3b49b13a1ed1cc2
Jason Korol <ejkorol@gmail.com>
2025-04-09 15:26:07 -0400

    address feedback

--- src/gui/src/components/navigation/sidebar/sidebar-query-nav.tsx
+++ src/gui/src/components/navigation/sidebar/sidebar-query-nav.tsx
@@ -29,28 +29,25 @@ const SidebarQueryNav = () => {
   const updateQuery = useGraphStore(state => state.updateQuery)
   const updateStamp = useGraphStore(state => state.updateStamp)
 
-  const projectQueries = savedQueries?.filter(query => {
-    return query.context === graph?.projectRoot
-  })
-
-  const globalQueries = savedQueries?.filter(query => {
-    return query.context.trim() === ''
-  })
+  const projectQueries = savedQueries?.filter(
+    query => query.context === graph?.projectRoot,
+  )
+  const globalQueries = savedQueries?.filter(
+    query => !query.context.trim(),
+  )
 
   const constructMenu = (
     parentName: string,
     queryItems: SavedQuery[],
     icon: LucideIcon,
-  ): MenuItem => {
-    return {
-      title: parentName,
-      icon: icon,
-      items: queryItems.map(item => ({
-        title: item.name,
-        onClick: () => void runQuery(item),
-      })),
-    }
-  }
+  ): MenuItem => ({
+    title: parentName,
+    icon: icon,
+    items: queryItems.map(item => ({
+      title: item.name,
+      onClick: () => void runQuery(item),
+    })),
+  })
 
   const runQuery = async (item: SavedQuery): Promise<void> => {
     if (!graph) return
