890cdb9732efcae88ac995bebe240995d4a5df35
Ruy Adorno <ruy@vlt.sh>
2025-04-08 23:51:57 -0400

    query: add published selector
    Adds a `:published("YYYY-MM-DD")` pseudo selector that matches
    packages based on their publication date.
    
    The date parameter can be prefixed with a comparator
    (>, <, >=, <=). If no comparator is provided, it will match
    exact dates.
    
    Examples:
    
        /* Matches packages published exactly on January 1st, 2024 */
        :published(2024-01-01)
    
        /* Matches packages published after January 1st, 2024 */
        :published(">2024-01-01")
    
        /* Matches packages published on or before December 31st, 2023 */
        :published("<=2023-12-31")
    
    Fixes: https://github.com/vltpkg/statusboard/issues/107

---
+++ src/query/tap-snapshots/test/pseudo/published.ts.test.cjs
@@ -0,0 +1,73 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/pseudo/published.ts > TAP > select from published definition > published exact date > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "a",
+  ],
+  "nodes": Array [
+    "a",
+  ],
+}
+`
+
+exports[`test/pseudo/published.ts > TAP > select from published definition > published greater than date > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "c",
+    "d",
+    "e",
+  ],
+  "nodes": Array [
+    "c",
+    "d",
+    "e",
+  ],
+}
+`
+
+exports[`test/pseudo/published.ts > TAP > select from published definition > published greater than or equal date > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "b",
+    "c",
+    "d",
+    "e",
+  ],
+  "nodes": Array [
+    "b",
+    "c",
+    "d",
+    "e",
+  ],
+}
+`
+
+exports[`test/pseudo/published.ts > TAP > select from published definition > published less than date > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "a",
+  ],
+  "nodes": Array [
+    "a",
+  ],
+}
+`
+
+exports[`test/pseudo/published.ts > TAP > select from published definition > published less than or equal date > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "a",
+    "b",
+  ],
+  "nodes": Array [
+    "a",
+    "b",
+  ],
+}
+`
---
+++ src/query/test/pseudo/published.ts
@@ -0,0 +1,265 @@
+import t from 'tap'
+import type { SpecOptions } from '@vltpkg/spec/browser'
+import { joinDepIDTuple } from '@vltpkg/dep-id'
+import type { NodeLike } from '@vltpkg/graph'
+import postcssSelectorParser from 'postcss-selector-parser'
+import {
+  published,
+  parseInternals,
+  queueNode,
+  retrieveRemoteDate,
+} from '../../src/pseudo/published.ts'
+import { asPostcssNodeWithChildren } from '../../src/types.ts'
+import type { ParserState } from '../../src/types.ts'
+import { getSemverRichGraph } from '../fixtures/graph.ts'
+
+const specOptions = {
+  registry: 'https://registry.npmjs.org',
+  registries: {
+    custom: 'http://example.com',
+  },
+} as SpecOptions
+
+global.fetch = (async (url: string) => {
+  if (url === 'https://registry.npmjs.org/h') {
+    return {
+      ok: false,
+    }
+  } else if (url === 'https://registry.npmjs.org/i') {
+    throw new Error('ERR')
+  } else if (url === 'https://registry.npmjs.org/c') {
+    return {
+      status: 404,
+      ok: false,
+    }
+  }
+  return {
+    ok: true,
+    json: async () => {
+      switch (url) {
+        case 'https://registry.npmjs.org/a': {
+          return {
+            time: {
+              '1.0.1': '2024-01-01T11:11:11.111Z',
+            },
+          }
+        }
+        case 'https://registry.npmjs.org/b': {
+          return {
+            time: {
+              '2.2.1': '2024-02-01T00:00:00.000Z',
+            },
+          }
+        }
+        case 'http://example.com/c': {
+          return {
+            time: {
+              '3.4.0': '2024-03-01T00:00:00.000Z',
+            },
+          }
+        }
+        case 'https://registry.npmjs.org/d': {
+          return {
+            time: {
+              '2.3.4': '2024-04-01T00:00:00.000Z',
+            },
+          }
+        }
+        case 'https://registry.npmjs.org/e': {
+          return {
+            time: {
+              '120.0.0': '2024-05-01T00:00:00.000Z',
+            },
+          }
+        }
+      }
+    },
+  }
+}) as unknown as typeof global.fetch
+
+const getState = (query: string, graph = getSemverRichGraph()) => {
+  const ast = postcssSelectorParser().astSync(query)
+  const current = asPostcssNodeWithChildren(ast.first.first)
+  const state: ParserState = {
+    current,
+    initial: {
+      edges: new Set(graph.edges.values()),
+      nodes: new Set(graph.nodes.values()),
+    },
+    partial: {
+      edges: new Set(graph.edges.values()),
+      nodes: new Set(graph.nodes.values()),
+    },
+    collect: {
+      edges: new Set(),
+      nodes: new Set(),
+    },
+    cancellable: async () => {},
+    walk: async i => i,
+    retries: 0,
+    securityArchive: undefined,
+    specOptions,
+  }
+  return state
+}
+
+t.test('select from published definition', async t => {
+  t.capture(console, 'warn')
+
+  await t.test('published exact date', async t => {
+    const res = await published(getState(':published(2024-01-01)'))
+    t.strictSame(
+      [...res.partial.nodes].map(n => n.name),
+      ['a'],
+      'should have expected result using exact date match',
+    )
+    t.matchSnapshot({
+      nodes: [...res.partial.nodes].map(n => n.name),
+      edges: [...res.partial.edges].map(e => e.name),
+    })
+  })
+
+  await t.test('published greater than date', async t => {
+    const res = await published(getState(':published(">2024-02-01")'))
+    t.strictSame(
+      [...res.partial.nodes].map(n => n.name),
+      ['c', 'd', 'e'],
+      'should have expected result using greater than date',
+    )
+    t.matchSnapshot({
+      nodes: [...res.partial.nodes].map(n => n.name),
+      edges: [...res.partial.edges].map(e => e.name),
+    })
+  })
+
+  await t.test('published greater than or equal date', async t => {
+    const res = await published(
+      getState(':published(">=2024-02-01")'),
+    )
+    t.strictSame(
+      [...res.partial.nodes].map(n => n.name),
+      ['b', 'c', 'd', 'e'],
+      'should have expected result using greater than date',
+    )
+    t.matchSnapshot({
+      nodes: [...res.partial.nodes].map(n => n.name),
+      edges: [...res.partial.edges].map(e => e.name),
+    })
+  })
+
+  await t.test('published less than date', async t => {
+    const res = await published(getState(':published("<2024-02-01")'))
+    t.strictSame(
+      [...res.partial.nodes].map(n => n.name),
+      ['a'],
+      'should have expected result using less than or equal date',
+    )
+    t.matchSnapshot({
+      nodes: [...res.partial.nodes].map(n => n.name),
+      edges: [...res.partial.edges].map(e => e.name),
+    })
+  })
+
+  await t.test('published less than or equal date', async t => {
+    const res = await published(
+      getState(':published("<=2024-02-01")'),
+    )
+    t.strictSame(
+      [...res.partial.nodes].map(n => n.name),
+      ['a', 'b'],
+      'should have expected result using less than or equal date',
+    )
+    t.matchSnapshot({
+      nodes: [...res.partial.nodes].map(n => n.name),
+      edges: [...res.partial.edges].map(e => e.name),
+    })
+  })
+
+  await t.test('invalid pseudo selector usage', async t => {
+    await t.rejects(
+      published(getState(':semver')),
+      /Failed to parse :published selector/,
+      'should throw an error for invalid pseudo selector usage',
+    )
+  })
+})
+
+t.test('parseInternals', async t => {
+  const ast = postcssSelectorParser().astSync(
+    ':published(">2024-01-01")',
+  )
+  const nodes = asPostcssNodeWithChildren(ast.first.first).nodes
+  const internals = parseInternals(nodes)
+  t.strictSame(
+    internals,
+    { relativeDate: '2024-01-01', comparator: '>' },
+    'should correctly parse internals from published selector',
+  )
+})
+
+t.test('retrieveRemoteDate', async t => {
+  t.capture(console, 'warn')
+
+  const missingName = {
+    id: joinDepIDTuple(['registry', '', 'a@1.0.0']),
+  } as NodeLike
+  t.strictSame(
+    await retrieveRemoteDate(missingName, specOptions),
+    undefined,
+    'should return undefined if missing essential info',
+  )
+
+  await t.test('no response from registry', async t => {
+    const node = {
+      name: 'h',
+      version: '1.0.0',
+      id: joinDepIDTuple(['registry', '', 'h@1.0.0']),
+    } as NodeLike
+    await t.rejects(
+      retrieveRemoteDate(node, specOptions),
+      /Failed to fetch packument/,
+      'should throw an internal error so that it may be retried',
+    )
+  })
+
+  await t.test('fetch error', async t => {
+    const node = {
+      name: 'i',
+      id: joinDepIDTuple(['registry', '', 'i@1.0.0']),
+    } as NodeLike
+    t.strictSame(
+      await retrieveRemoteDate(node, specOptions),
+      undefined,
+      'should return undefined if fetch throws',
+    )
+  })
+
+  await t.test('404', async t => {
+    const node = {
+      name: 'c',
+      version: '1.0.0',
+      id: joinDepIDTuple(['registry', '', 'c@1.0.0']),
+    } as NodeLike
+    await t.rejects(
+      retrieveRemoteDate(node, specOptions),
+      /Missing API/,
+      'should throw an internal error that will not be retried',
+    )
+  })
+})
+
+t.test('queueNode', async t => {
+  const missingName = {
+    id: joinDepIDTuple(['registry', '', 'a@1.0.0']),
+  } as NodeLike
+  t.strictSame(
+    await queueNode(
+      getState(':published("2024-01-01")'),
+      missingName,
+      '2024-01-01',
+      undefined,
+    ),
+    missingName,
+    'should return node if missing essential info',
+  )
+})
