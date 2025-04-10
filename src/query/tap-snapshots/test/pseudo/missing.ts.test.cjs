9335a9f33ce2c12a211a9ecb887bc9495b29bea4
Ruy Adorno <ruy@vlt.sh>
2025-04-10 10:58:27 -0400

    query: refactor missing pseudo selector (#634)
    Refactor `:missing` into its own file and add unit tests for it.

---
+++ src/query/tap-snapshots/test/pseudo/missing.ts.test.cjs
@@ -0,0 +1,40 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/pseudo/missing.ts > TAP > selects edges with no linked node > handles a mixed state with some linked and some unlinked edges > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "a",
+    "b",
+  ],
+  "nodes": Array [],
+}
+`
+
+exports[`test/pseudo/missing.ts > TAP > selects edges with no linked node > handles an empty partial state > must match snapshot 1`] = `
+Object {
+  "edges": Array [],
+  "nodes": Array [],
+}
+`
+
+exports[`test/pseudo/missing.ts > TAP > selects edges with no linked node > returns no edges in simple graph (all edges have linked nodes) > must match snapshot 1`] = `
+Object {
+  "edges": Array [],
+  "nodes": Array [],
+}
+`
+
+exports[`test/pseudo/missing.ts > TAP > selects edges with no linked node > selects edges with no linked node in missing node graph > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "a",
+    "b",
+  ],
+  "nodes": Array [],
+}
+`
