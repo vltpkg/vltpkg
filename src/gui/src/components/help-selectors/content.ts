3ee5cec174e80bf2a48608b4576fe94ad56d0ca5
Jason Korol <ejkorol@gmail.com>
2025-04-11 10:23:28 -0400

    gui: add all known selectors into help page (#614)
    extends on the #591 by adding all known selectors into the help page

--- src/gui/src/components/help-selectors/content.ts
+++ src/gui/src/components/help-selectors/content.ts
@@ -1,7 +1,4 @@
 export const selectorsContent = `
-### Selectors
-
 Selectors provide a powerful way to analyze your project’s dependencies using our custom query language.  
-
-For more information about our Selectors, see the [Selectors documentation](https://docs.vlt.sh/cli/selectors)
+For more information, see the [Selectors documentation](https://docs.vlt.sh/cli/selectors)
 `
