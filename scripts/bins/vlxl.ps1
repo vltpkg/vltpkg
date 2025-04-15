4ab25f230b6244cb6b50f3f72c0377399b450a61
Luke Karrys <luke@lukekarrys.com>
2025-04-15 14:43:30 -0700

    dev: create local powershell scripts

---
+++ scripts/bins/vlxl.ps1
@@ -0,0 +1,6 @@
+$ScriptDir = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
+$nodeArgs = $args
+& { 
+  $env:NODE_OPTIONS = "--no-warnings --experimental-strip-types --enable-source-maps"
+  node "$ScriptDir\infra\build\src\bins\vlxl.ts" @nodeArgs
+}
