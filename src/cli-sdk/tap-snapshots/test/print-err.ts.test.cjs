bc473d7a07e6c1bd7af8be43f45ad634d1589507
Luke Karrys <luke@lukekarrys.com>
2025-04-25 10:42:14 -0700

    tests

---
+++ src/cli-sdk/tap-snapshots/test/print-err.ts.test.cjs
@@ -0,0 +1,24 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/print-err.ts > TAP > EREQUEST > no cause > must match snapshot 1`] = `
+Array [
+  "Request Error: oh no! my request!",
+  "  URL: https://x.y/",
+  "  Method: GET",
+]
+`
+
+exports[`test/print-err.ts > TAP > EREQUEST > with cause > must match snapshot 1`] = `
+Array [
+  "Request Error: oh no! my request!",
+  "  Code: ECONNRESET",
+  "  Syscall: read",
+  "  URL: https://x.y/",
+  "  Method: GET",
+]
+`
