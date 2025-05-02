55ec0f35fdb4980f2bff7f2098e390ce5f436753
Jason Korol <ejkorol@gmail.com>
2025-05-02 12:20:23 -0400

    updates icons
    - removes `glyph-icon` and prefers svgs for rendering
    - prefers to use `createLucideIcon` utility from `lucide-react` to
      construct svgs from paths, this allows us to pass `lucide-props` into
      icons for easier styling.
    - updates icons snapshot tests

--- src/gui/src/components/icons/npm.tsx
+++ src/gui/src/components/icons/npm.tsx
@@ -1,13 +1,25 @@
-export const Npm = (props: React.SVGProps<SVGSVGElement>) => {
-  return (
-    <svg
-      {...props}
-      id="a"
-      xmlns="http://www.w3.org/2000/svg"
-      width="256"
-      height="256"
-      viewBox="0 0 256 256">
-      <path d="M0,0V256H256V0H0ZM213.08,213.1h-42.92V85.82h-42.92v127.28H42.9V42.92H213.08V213.1h0Z" />
-    </svg>
-  )
-}
+import { createLucideIcon } from 'lucide-react'
+
+export const Npm = createLucideIcon('Npm', [
+  [
+    'path',
+    {
+      d: 'M2 2V22H22V2H2ZM18.6469 18.6484H15.2938V8.70469H11.9406V18.6484H5.35156V5.35312H18.6469V18.6484Z',
+      key: '1',
+      fill: 'currentColor',
+      strokeWidth: '0',
+    },
+  ],
+])
+
+export const NpmWordmark = createLucideIcon('NpmWordmark', [
+  [
+    'path',
+    {
+      d: 'M8.15385 15.4103H10.7179V14.1282H13.2821V9H8.15385V15.4103ZM10.7179 10.2821H12V12.8462H10.7179V10.2821ZM14.3077 9V14.1282H16.8718V10.2821H18.1538V14.1282H19.4359V10.2821H20.7179V14.1282H22V9H14.3077ZM2 14.1282H4.5641V10.2821H5.84615V14.1282H7.12821V9H2V14.1282Z',
+      key: '1',
+      fill: 'currentColor',
+      strokeWidth: '0',
+    },
+  ],
+])
