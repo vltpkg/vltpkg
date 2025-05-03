4ddb4645287119dfdb005fb10298a2edce9bb5b2
Luke Karrys <luke@lukekarrys.com>
2025-05-03 12:54:57 -0700

    cli-sdk: better error message when reading unknown configs (#745)
    Fixes #744

---
+++ src/cli-sdk/tap-snapshots/test/index.ts.test.cjs
@@ -0,0 +1,52 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/index.ts > TAP > unknown config > must match snapshot 1`] = `
+Error: Unknown CLI flags. Run 'vlt help' for more information about available flags.
+  Found: --unknown
+  Wanted:
+    arch
+    bail
+    before
+    cache
+    color
+    config
+    dashboard-root
+    editor
+    expect-results
+    fallback-command
+    fetch-retries
+    fetch-retry-factor
+    fetch-retry-maxtimeout
+    fetch-retry-mintimeout
+    git-host-archives
+    git-hosts
+    git-shallow
+    help
+    identity
+    no-bail
+    no-color
+    node-version
+    os
+    package
+    recursive
+    registries
+    registry
+    save-dev
+    save-optional
+    save-peer
+    save-prod
+    scope-registries
+    script-shell
+    stale-while-revalidate-factor
+    tag
+    version
+    view
+    workspace
+    workspace-group
+    yes
+`
