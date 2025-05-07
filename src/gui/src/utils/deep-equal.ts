57427c6cc3fbbae0bdcb23a4fae9c553d48a8547
Jason Korol <ejkorol@gmail.com>
2025-05-07 12:15:01 -0400

    gui: refactor dependency item sidebar (#750)
    Refactors the existing code from `dependency-sidebar` and breaking it down into smaller components to make the state and backend logic easier to manage.
    
    Utilizes a similar design pattern to creating a scoped `zustand` store within context to initialize it via props in the react lifecycle and having a cleaner component tree
    
    - creates utility for deep equality checks
    - creates context and store for `dependency-sidebar`
    - refactors `dependency-sidebar` component with the store
    - removes old `dependency-side-bar` component
    - refactors `add-dependency` popover component

---
+++ src/gui/src/utils/deep-equal.ts
@@ -0,0 +1,19 @@
+import { isRecord } from '@/utils/typeguards.js'
+
+export const deepEqual = (a: any, b: any): boolean => {
+  if (a === b) return true
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length) return false
+    return a.every((val, index) => deepEqual(val, b[index]))
+  }
+
+  if (isRecord(a) && isRecord(b)) {
+    const keysA = Object.keys(a)
+    const keysB = Object.keys(b)
+    if (keysA.length !== keysB.length) return false
+    return keysA.every(key => deepEqual(a[key], b[key]))
+  }
+
+  return false
+}
