75a08ce145fbeb6771112906e191939123214d5c
isaacs <i@izs.me>
2025-05-09 09:10:45 -0700

    vlx: infer default executable when there is only one

--- src/vlx/src/infer-default-executable.ts
+++ src/vlx/src/infer-default-executable.ts
@@ -11,11 +11,18 @@ export const inferDefaultExecutable = ({
 }: Manifest): undefined | [string, string] => {
   if (!bin) return undefined
 
-  const binName = name?.startsWith('@') ? name.split('/')[1] : name
+  let binName = name?.startsWith('@') ? name.split('/')[1] : name
   if (!binName) return undefined
 
   if (typeof bin === 'string') {
     return [binName, bin]
+  }
+
+  // if it's exactly one key, that's the one,
+  // even if it doesn't match the name
+  const keys = Object.keys(bin)
+  if (keys.length === 1) {
+    binName = (keys as [string])[0]
   }
 
   bin = bin[binName]
