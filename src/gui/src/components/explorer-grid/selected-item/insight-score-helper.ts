49a6fbeffd5e919c0ca2206a90264954acbf2e43
Jason Korol <ejkorol@gmail.com>
2025-05-12 16:52:40 -0400

    updates `insight-score-helper` to return classes and styles

--- src/gui/src/components/explorer-grid/selected-item/insight-score-helper.ts
+++ src/gui/src/components/explorer-grid/selected-item/insight-score-helper.ts
@@ -1,6 +1,20 @@
-type Variant = 'default' | 'neutral' | 'warning' | 'error' | 'success'
+import type { ProgressCircleVariant } from '@/components/ui/progress-circle.jsx'
 
-export const getScoreColor = (score: number): Variant => {
+type ScoreVariant = Exclude<ProgressCircleVariant, undefined>
+
+type Color =
+  /** neutral - blue */
+  | 'oklch(55.1% 0.027 264.364)'
+  /** default - gray */
+  | 'oklch(62.3% 0.214 259.815)'
+  /** warning - amber */
+  | 'oklch(76.9% 0.188 70.08)'
+  /** error - red */
+  | 'oklch(63.7% 0.237 25.331)'
+  /** success - emerald */
+  | 'oklch(69.6% 0.17 162.48)'
+
+export const getScoreColor = (score: number): ScoreVariant => {
   if (score >= 80) {
     return 'success'
   } else if (score >= 60) {
@@ -12,4 +26,12 @@ export const getScoreColor = (score: number): Variant => {
   } else {
     return 'neutral'
   }
+}
+
+export const scoreColors: Record<ScoreVariant, Color> = {
+  default: 'oklch(62.3% 0.214 259.815)',
+  neutral: 'oklch(55.1% 0.027 264.364)',
+  warning: 'oklch(76.9% 0.188 70.08)',
+  error: 'oklch(63.7% 0.237 25.331)',
+  success: 'oklch(69.6% 0.17 162.48)',
 }
