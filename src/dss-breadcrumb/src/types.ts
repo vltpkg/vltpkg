ef8f042d08b538fa6a9bf02e362ece297855a661
Ruy Adorno <ruy@vlt.sh>
2025-06-02 15:34:38 -0400

    dss-breadcrumb: interactive refactor
    Refactored the breadcrumb module to better manage state and the
    parsed result.
    
    The exported class that represents the parsed result of a query
    is now named `Breadcrumb`, instances of this class now have a
    `interactive()` method that returns an `InteractiveBreadcrumb`
    which is now the object that is going to hold state on the
    current item and implements a `next()` method that allows for
    updating that state.
    
    Refs: https://github.com/vltpkg/statusboard/issues/64

--- src/dss-breadcrumb/src/types.ts
+++ src/dss-breadcrumb/src/types.ts
@@ -1,10 +1,34 @@
 /**
  * A valid item of a given breadcrumb.
  */
-export type InteractiveBreadcrumbItem = {
+export type ModifierBreadcrumbItem = {
+  name?: string
   value: string
   type: string
-  prev: InteractiveBreadcrumbItem | undefined
-  next: InteractiveBreadcrumbItem | undefined
   importer: boolean
+  prev: ModifierBreadcrumbItem | undefined
+  next: ModifierBreadcrumbItem | undefined
+}
+
+/**
+ * A breadcrumb is a linked list of items, where
+ * each item has a value and a type.
+ */
+export interface ModifierBreadcrumb
+  extends Iterable<ModifierBreadcrumbItem> {
+  clear(): void
+  comment: string | undefined
+  first: ModifierBreadcrumbItem
+  last: ModifierBreadcrumbItem
+  single: boolean
+  interactive: () => ModifierInteractiveBreadcrumb
+}
+
+/**
+ * An interactive breadcrumb that holds state on what is the current item.
+ */
+export type ModifierInteractiveBreadcrumb = {
+  current: ModifierBreadcrumbItem | undefined
+  done: boolean
+  next: () => ModifierInteractiveBreadcrumb
 }
