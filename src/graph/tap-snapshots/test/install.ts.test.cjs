1234e2d157f0cba4e60a99607d501cbbb86307a8
Ruy Adorno <ruy@vlt.sh>
2025-06-16 14:30:35 -0400

    added back deleted snapshot files

---
+++ src/graph/tap-snapshots/test/install.ts.test.cjs
@@ -0,0 +1,22 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/install.ts > TAP > install > should call build -> actual.load -> reify 1`] = `
+GraphModifier.maybeLoad
+buildideal result adds 0 new package(s)
+actual.load
+reify
+
+`
+
+exports[`test/install.ts > TAP > install > should call build adding new dependency 1`] = `
+GraphModifier.maybeLoad
+buildideal result adds 1 new package(s)
+actual.load
+reify
+
+`
