ef5d163f8fa7dd1d5dae45da08002cf8f8007d62
Luke Karrys <luke@lukekarrys.com>
2025-06-17 18:46:43 -0700

    dev: always use live reload when running local

--- scripts/bins/vlrx.ps1
+++ scripts/bins/vlrx.ps1
@@ -2,5 +2,6 @@ $ScriptArgs = $args
 $RootDir = (Split-Path -Parent (Split-Path -Parent $PSScriptRoot))
 & {
   $env:NODE_OPTIONS = "--no-warnings --enable-source-maps --experimental-strip-types"
+  $env:__VLT_INTERNAL_LIVE_RELOAD = "1"
   node "$RootDir\infra\build\src\bins\vlrx.ts" @ScriptArgs
 }
