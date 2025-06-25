06fe1418c152ddeaf80a5338338120c51148b8ec
Ruy Adorno <ruy@vlt.sh>
2025-06-24 22:39:32 -0400

    gui: change dep type colors (#913)
    Removes the transparent backgrounds in the dependency type colors.
    
    <img width="1527" alt="Screenshot 2025-06-24 at 10 30 59 PM"
    src="https://github.com/user-attachments/assets/c7a15532-045a-4f77-94cb-286c83719645"
    />

--- src/gui/src/components/explorer-grid/label-helper.ts
+++ src/gui/src/components/explorer-grid/label-helper.ts
@@ -2,27 +2,27 @@ export const labelClassNamesMap = new Map<string | undefined, string>(
   [
     [
       undefined,
-      'bg-gray-100 border-[1px] border-neutral-900 text-neutral-900 hover:bg-gray-100/80',
+      'border-[1px] border-neutral-900  bg-gray-100 text-neutral-900 hover:bg-gray-100/80',
     ],
     [
       'prod',
-      'bg-cyan-500/25 text-cyan-600 border-[1px] border-cyan-600 dark:border-cyan-500 hover:bg-cyan-500/40 dark:bg-cyan-500/30 dark:text-cyan-500 dark:hover:bg-cyan-500/40',
+      'border-transparent bg-cyan-100 text-neutral-900 hover:bg-cyan-100/80',
     ],
     [
       'dev',
-      'bg-fuchsia-500/20 border-[1px] dark:border-fuchsia-400 border-fuchsia-500 text-fuchsia-500 hover:bg-fuchsia-400/30 dark:bg-fuchsia-500/30 dark:text-fuchsia-400 dark:hover:bg-fuchsia-500/40',
+      'border-[1px] dark:border-fuchsia-600 border-fuchsia-500 bg-fuchsia-100 dark:bg-fuchsia-300 text-neutral-900 dark:text-fuchsia-900 hover:bg-fuchsia-100/80',
     ],
     [
       'optional',
-      'bg-orange-500/20 border-[1px] border-orange-500 dark:border-orange-400 text-orange-500 hover:bg-orange-400/30 dark:bg-orange-500/30 dark:text-orange-400 dark:hover:bg-orange-500/40',
+      'border-[1px] border-orange-500 dark:border-orange-400 bg-orange-100 text-neutral-900 dark:text-orange-900 hover:bg-orange-100/80',
     ],
     [
       'peer',
-      'bg-lime-500/25 border-[1px] border-lime-600 dark:border-lime-400 text-lime-600 hover:bg-lime-400/40 dark:bg-lime-500/30 dark:text-lime-400 dark:hover:bg-lime-500/40',
+      'border-[1px] border-lime-600 dark:border-lime-700 bg-lime-100 dark:bg-lime-300 text-neutral-900 dark:text-lime-900 hover:bg-lime-100/80',
     ],
     [
       'peerOptional',
-      'bg-yellow-500/25 border-[1px] border-yellow-600 dark:border-yellow-400 text-yellow-600 hover:bg-yellow-400/40 dark:bg-yellow-500/30 dark:text-yellow-400 dark:hover:bg-yellow-500/40',
+      'border-[1px] border-yellow-600 dark:border-yellow-400  bg-yellow-100 text-neutral-900 dark:text-yellow-900 hover:bg-yellow-100/80',
     ],
     [
       'workspace',
