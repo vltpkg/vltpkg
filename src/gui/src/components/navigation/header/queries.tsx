9c3457af0f619d38d22b7eafd378287739b6335c
Jason Korol <ejkorol@gmail.com>
2025-07-03 16:10:59 -0400

    feat: creates header for `queries`
    - creates a `queries` state store with zustand
    - creates `header/queries.tsx`
    - updates some styles on `queries.tsx`
    - updates tests and snapshots

---
+++ src/gui/src/components/navigation/header/queries.tsx
@@ -0,0 +1,76 @@
+import { NavLink } from 'react-router'
+import { FilterSearch } from '@/components/ui/filter-search.tsx'
+import { DeleteQuery } from '@/components/queries/delete-query.tsx'
+import { Button } from '@/components/ui/button.tsx'
+import { Plus, Tag } from 'lucide-react'
+import { Badge } from '@/components/ui/badge.tsx'
+import { SortToggle } from '@/components/sort-toggle.tsx'
+import { useGraphStore } from '@/state/index.ts'
+import { useQueriesStore } from '@/state/queries.ts'
+
+export const QueriesHeader = () => {
+  const savedLabels = useGraphStore(state => state.savedQueryLabels)
+  const savedQueries = useGraphStore(state => state.savedQueries)
+
+  const filteredQueries = useQueriesStore(
+    state => state.filteredQueries,
+  )
+  const setFilteredQueries = useQueriesStore(
+    state => state.setFilteredQueries,
+  )
+  const isCreating = useQueriesStore(state => state.isCreating)
+  const setIsCreating = useQueriesStore(state => state.setIsCreating)
+  const deleteDialogOpen = useQueriesStore(
+    state => state.deleteDialogOpen,
+  )
+  const setDeleteDialogOpen = useQueriesStore(
+    state => state.setDeleteDialogOpen,
+  )
+  const selectedQueries = useQueriesStore(
+    state => state.selectedQueries,
+  )
+
+  return (
+    <div className="flex justify-between gap-2">
+      <div className="flex gap-2">
+        <FilterSearch
+          placeholder="Filter Queries"
+          items={savedQueries}
+          setFilteredItems={setFilteredQueries}
+        />
+        <SortToggle
+          filteredItems={filteredQueries}
+          setFilteredItems={setFilteredQueries}
+          sortKey="name"
+        />
+        <div className="flex items-center">
+          <div className="flex items-center">
+            <DeleteQuery
+              deleteDialogOpen={deleteDialogOpen}
+              setDeleteDialogOpen={setDeleteDialogOpen}
+              selectedQueries={selectedQueries}
+              type="icon"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center gap-2">
+        {savedQueries?.length !== 0 && (
+          <Button
+            disabled={isCreating}
+            onClick={() => setIsCreating(true)}>
+            <span>New Query</span>
+            <Plus />
+          </Button>
+        )}
+        <Button asChild variant="outline">
+          <NavLink to="/labels">
+            <Tag />
+            <span>Labels</span>
+            <Badge variant="secondary">{savedLabels?.length}</Badge>
+          </NavLink>
+        </Button>
+      </div>
+    </div>
+  )
+}
