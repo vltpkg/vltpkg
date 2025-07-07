07b1379d5002e233965ea9a157509a1635ef7566
Ruy Adorno <ruy@vlt.sh>
2025-07-07 17:42:01 -0400

    gui: fix graph modifier navigation
    Stacking elements and navigating dependents was not working properly
    when using graph modifiers due to the possibility of finding forked
    nodes in the graph. This fixes it by properly handling it using the
    base dep ids instead.

--- src/gui/test/components/explorer-grid/selected-item/index.tsx
+++ src/gui/test/components/explorer-grid/selected-item/index.tsx
@@ -1,9 +1,16 @@
+import { inspect } from 'node:util'
 import { test, expect, vi, beforeEach, afterEach } from 'vitest'
 import { cleanup, render } from '@testing-library/react'
 import html from 'diffable-html'
 import { useGraphStore as useStore } from '@/state/index.ts'
-import { SelectedItem } from '@/components/explorer-grid/selected-item/index.tsx'
+import {
+  SelectedItem,
+  getDependentItems,
+} from '@/components/explorer-grid/selected-item/index.tsx'
+import { joinDepIDTuple } from '@vltpkg/dep-id/browser'
+import { Spec } from '@vltpkg/spec/browser'
 import type { GridItemData } from '@/components/explorer-grid/types'
+import type { QueryResponseNode } from '@vltpkg/query'
 
 vi.mock('@/components/explorer-grid/selected-item/item.tsx', () => ({
   Item: 'gui-selected-item',
@@ -60,4 +67,89 @@ test('SelectedItem renders default', () => {
 
   const { container } = render(<Container />)
   expect(container.innerHTML).toMatchSnapshot()
+})
+
+test('getDependentItems', () => {
+  const mockNode = {
+    id: joinDepIDTuple(['registry', '', 'item@1.0.0']),
+    edgesIn: new Set([
+      {
+        name: 'dependent',
+        type: 'prod',
+        spec: Spec.parse('dependent', '^1.0.0'),
+        from: {
+          id: joinDepIDTuple(['registry', '', 'dependent@1.0.0']),
+          name: 'dependent',
+          version: '1.0.0',
+          edgesIn: new Set(),
+        } as unknown as QueryResponseNode,
+      },
+      {
+        name: 'foo',
+        type: 'dev',
+        spec: Spec.parse('foo', '^1.0.0'),
+        from: {
+          id: joinDepIDTuple(['registry', '', 'foo@1.0.0']),
+          name: 'foo',
+          version: '1.0.0',
+          edgesIn: new Set([
+            {
+              name: 'foo',
+              type: 'dev',
+              spec: Spec.parse('foo', '^1.0.0'),
+              from: {
+                id: joinDepIDTuple(['file', '.']),
+                name: 'root',
+                version: '1.0.0',
+                edgesIn: new Set(),
+              } as unknown as QueryResponseNode,
+            },
+            {
+              name: 'foo',
+              type: 'dev',
+              spec: Spec.parse('foo', '^1.0.0'),
+              from: {
+                id: joinDepIDTuple(['workspace', 'a']),
+                name: 'a',
+                version: '1.0.0',
+                edgesIn: new Set(),
+              } as unknown as QueryResponseNode,
+            },
+          ]),
+        } as unknown as QueryResponseNode,
+      },
+      {
+        name: 'bar',
+        type: 'prod',
+        spec: Spec.parse('bar', '^1.0.0'),
+        from: {
+          id: joinDepIDTuple(['registry', '', 'bar@1.0.0']),
+          name: 'bar',
+          version: '1.0.0',
+          edgesIn: new Set(),
+        } as unknown as QueryResponseNode,
+      },
+      {
+        name: 'bar',
+        type: 'prod',
+        spec: Spec.parse('bar', '^1.0.0'),
+        from: {
+          id: joinDepIDTuple([
+            'registry',
+            '',
+            'bar@1.0.0',
+            '#modifier',
+          ]),
+          name: 'bar',
+          version: '2.0.0',
+          edgesIn: new Set(),
+        } as unknown as QueryResponseNode,
+      },
+    ]),
+    edgesOut: new Set(),
+  } as unknown as QueryResponseNode
+
+  const dependents = getDependentItems(mockNode)
+
+  expect(inspect(dependents, { depth: 10 })).toMatchSnapshot()
 })
