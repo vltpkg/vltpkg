0660253460b9e2575587dbd6689bee07000ab507
Jason Korol <ejkorol@gmail.com>
2025-07-17 11:10:01 -0400

    gui: create known url parsing utility (#1038)
    ## Overview
    
    This PR creates a `get-known-hostname` utility function in `@/utils`
    that takes an arbitrary `url` and returns a known hostname / domain from
    that url by utilizing new constants in `@/lib/constants`
    
    It updates the `tabs-dependencies/tabs-funding` and `tabs-overview` urls
    to use this new utility function.
    
    Resolves #1037
    
    #### Changelog
    - **feat: adds `get-known-hostname` utility function**
    - **feat: updates `tabs-dependencies/tabs-funding` urls**
    - **feat: updates `tabs-overview` urls**

---
+++ src/gui/src/lib/constants/known-hostnames.ts
@@ -0,0 +1,11 @@
+export const KNOWN_HOSTNAMES: Record<string, string> = {
+  github: 'GitHub',
+  opencollective: 'Open Collective',
+  'ko-fi': 'Ko-fi',
+  patreon: 'Patreon',
+  buymeacoffee: 'Buy Me a Coffee',
+  tidelift: 'Tidelift',
+  liberapay: 'Liberapay',
+  IssueHunt: 'IssueHunt',
+  'open-source': 'Open Source',
+}
