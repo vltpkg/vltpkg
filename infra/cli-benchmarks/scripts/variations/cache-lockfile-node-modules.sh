e5f617042237fb76151e7a19906f034b89f410ac
Ruy Adorno <ruy@vlt.sh>
2025-07-31 11:10:34 -0400

    chore: add continous benchmarking (#1052)
    Adds benchmark scripts along with GitHub actions using Bencher
    <https://bencher.dev> to track performance evolution of CLI installs in
    a matrix of fixtures & variations.
    
    Fixes: https://github.com/vltpkg/vltpkg/issues/1048

---
+++ infra/cli-benchmarks/scripts/variations/cache-lockfile-node-modules.sh
@@ -0,0 +1,17 @@
+# Exit on error
+set -Eeuxo pipefail
+
+# Load common variables
+source "$1/variations/common.sh"
+
+# Run the benchmark suite
+# When running a cache benchmark, we want to clean up only the node_modules
+# directory and the lockfiles between each run.
+hyperfine \
+  --export-json="$BENCH_OUTPUT_FOLDER/benchmarks.json" \
+  --warmup="$BENCH_WARMUP" \
+  --runs="$BENCH_RUNS" \
+  --setup="bash $BENCH_SCRIPTS/clean-helpers.sh clean_all" \
+  --prepare="sleep 1" \
+  --cleanup="bash $BENCH_SCRIPTS/clean-helpers.sh clean_all" \
+  "$BENCH_COMMAND_VLT"
---
+++ infra/cli-benchmarks/scripts/variations/cache-lockfile.sh
@@ -0,0 +1,18 @@
+# Exit on error
+set -Eeuxo pipefail
+
+# Load common variables
+source "$1/variations/common.sh"
+
+# Run the benchmark suite
+# When running a cache benchmark, we want to clean up only the node_modules
+# directory and the lockfiles between each run.
+hyperfine \
+  --export-json="$BENCH_OUTPUT_FOLDER/benchmarks.json" \
+  --warmup="$BENCH_WARMUP" \
+  --runs="$BENCH_RUNS" \
+  --setup="bash $BENCH_SCRIPTS/clean-helpers.sh clean_all" \
+  --prepare="sleep 1; bash $BENCH_SCRIPTS/clean-helpers.sh clean_node_modules" \
+  --conclude="sleep 1; bash $BENCH_SCRIPTS/clean-helpers.sh clean_node_modules" \
+  --cleanup="bash $BENCH_SCRIPTS/clean-helpers.sh clean_all" \
+  "$BENCH_COMMAND_VLT"
