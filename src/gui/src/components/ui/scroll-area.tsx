5c5b8dea82ab5c4dba1123817a40bf323e620c41
Jason Korol <ejkorol@gmail.com>
2025-08-21 23:30:29 -0400

    feat: updates some `ui` component styles
    - allows `scroll-area` to catch `className` props for the thumb styling
    - updates the `table` component for sensible defaults

--- src/gui/src/components/ui/scroll-area.tsx
+++ src/gui/src/components/ui/scroll-area.tsx
@@ -10,50 +10,84 @@ interface ScrollAreaProps
     typeof ScrollAreaPrimitive.Root
   > {
   viewportClassName?: string
+  scrollBarClassName?: string
+  scrollBarThumbClassName?: string
 }
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
   ScrollAreaProps
->(({ className, viewportClassName, children, ...props }, ref) => (
-  <ScrollAreaPrimitive.Root
-    ref={ref}
-    className={cn('relative overflow-hidden', className)}
-    {...props}>
-    <ScrollAreaPrimitive.Viewport
-      className={cn(
-        'h-full w-full rounded-[inherit]',
-        viewportClassName,
-      )}>
-      {children}
-    </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
-    <ScrollAreaPrimitive.Corner />
-  </ScrollAreaPrimitive.Root>
-))
+>(
+  (
+    {
+      className,
+      viewportClassName,
+      scrollBarClassName,
+      scrollBarThumbClassName,
+      children,
+      ...props
+    },
+    ref,
+  ) => (
+    <ScrollAreaPrimitive.Root
+      ref={ref}
+      className={cn('relative overflow-hidden', className)}
+      {...props}>
+      <ScrollAreaPrimitive.Viewport
+        className={cn(
+          'h-full w-full rounded-[inherit]',
+          viewportClassName,
+        )}>
+        {children}
+      </ScrollAreaPrimitive.Viewport>
+      <ScrollBar
+        className={scrollBarClassName}
+        scrollBarThumbClassName={scrollBarThumbClassName}
+      />
+      <ScrollAreaPrimitive.Corner />
+    </ScrollAreaPrimitive.Root>
+  ),
+)
 ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
 
 const ScrollBar = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
   React.ComponentPropsWithoutRef<
     typeof ScrollAreaPrimitive.ScrollAreaScrollbar
-  >
->(({ className, orientation = 'vertical', ...props }, ref) => (
-  <ScrollAreaPrimitive.ScrollAreaScrollbar
-    ref={ref}
-    orientation={orientation}
-    className={cn(
-      'flex touch-none select-none transition-colors',
-      orientation === 'vertical' &&
-        'h-full w-2.5 border-l border-l-transparent p-[1px]',
-      orientation === 'horizontal' &&
-        'h-2.5 flex-col border-t border-t-transparent p-[1px]',
+  > & {
+    scrollBarThumbClassName?: string
+  }
+>(
+  (
+    {
       className,
-    )}
-    {...props}>
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
-  </ScrollAreaPrimitive.ScrollAreaScrollbar>
-))
+      scrollBarThumbClassName,
+      orientation = 'vertical',
+      ...props
+    },
+    ref,
+  ) => (
+    <ScrollAreaPrimitive.ScrollAreaScrollbar
+      ref={ref}
+      orientation={orientation}
+      className={cn(
+        'flex touch-none select-none transition-colors',
+        orientation === 'vertical' &&
+          'h-full w-2.5 border-l border-l-transparent p-[1px]',
+        orientation === 'horizontal' &&
+          'h-2.5 flex-col border-t border-t-transparent p-[1px]',
+        className,
+      )}
+      {...props}>
+      <ScrollAreaPrimitive.ScrollAreaThumb
+        className={cn(
+          'relative flex-1 rounded-full bg-border',
+          scrollBarThumbClassName,
+        )}
+      />
+    </ScrollAreaPrimitive.ScrollAreaScrollbar>
+  ),
+)
 ScrollBar.displayName =
   ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
 
