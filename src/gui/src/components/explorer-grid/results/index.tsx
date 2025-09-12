a093f1768e1ff3f969d8a839745ec55be384d8db
Jason Korol <ejkorol@gmail.com>
2025-09-12 14:07:49 -0400

    feat: updates `results` view
    - prepares some pagination and sorting components placeholders for later
    - updates the rendering of results view in
      `@/components/explorer-grid/index.tsx`
    - adds some extra typing on the utility functions
    - updates snapshots

--- src/gui/src/components/explorer-grid/results/index.tsx
+++ src/gui/src/components/explorer-grid/results/index.tsx
@@ -1,11 +1,22 @@
 import { ResultItem } from '@/components/explorer-grid/results/result-item.tsx'
-import type { GridItemData } from '@/components/explorer-grid/types.ts'
-import { GridHeader } from '@/components/explorer-grid/header.tsx'
 import { EmptyResultsState } from '@/components/explorer-grid/results/empty-results-state.tsx'
-import { Badge } from '@/components/ui/badge.tsx'
+import { ResultsSort } from '@/components/explorer-grid/results/sort.tsx'
+import {
+  ResultsProvider,
+  useResultsStore,
+} from '@/components/explorer-grid/results/context.tsx'
+import { ResultsPaginationNavigation } from '@/components/explorer-grid/results/page-navigation.tsx'
+import { ResultPageOptions } from '@/components/explorer-grid/results/page-options.tsx'
+import { cn } from '@/lib/utils.ts'
 
-export const Results = ({ items }: { items: GridItemData[] }) => {
-  if (!items.length) {
+import type { GridItemData } from '@/components/explorer-grid/types.ts'
+
+export const Results = ({
+  allItems,
+}: {
+  allItems: GridItemData[]
+}) => {
+  if (!allItems.length) {
     return (
       <div className="px-8 py-4">
         <EmptyResultsState />
@@ -14,16 +25,55 @@ export const Results = ({ items }: { items: GridItemData[] }) => {
   }
 
   return (
-    <div className="mt-8 grid w-full max-w-8xl gap-4 px-8 py-4">
-      <div className="flex items-center gap-3">
-        <GridHeader className="h-fit">Results</GridHeader>
-        <Badge variant="default">{items.length}</Badge>
+    <ResultsProvider allItems={allItems}>
+      <section className="flex h-full flex-col">
+        <div className="mx-auto mt-2 w-full max-w-8xl grow px-8 py-4">
+          <ResultsHeader />
+          <ResultsList className="mt-4" />
+        </div>
+        <ResultsFooter />
+      </section>
+    </ResultsProvider>
+  )
+}
+
+const ResultsHeader = () => {
+  const allItems = useResultsStore(state => state.allItems)
+  return (
+    <div className="flex items-center justify-between gap-3">
+      <h3 className="inline-flex items-baseline gap-2 text-lg">
+        Results
+        <span className="font-mono text-sm tabular-nums text-muted-foreground">
+          ({allItems.length})
+        </span>
+      </h3>
+      <div className="flex items-center gap-2">
+        <ResultsSort />
       </div>
-      <div className="flex flex-col gap-5">
-        {items.map(item => (
-          <ResultItem item={item} key={item.id} />
-        ))}
-      </div>
+    </div>
+  )
+}
+
+const ResultsList = ({ className }: { className?: string }) => {
+  const pageItems = useResultsStore(state => state.pageItems)
+  return (
+    <div className={cn('flex flex-col gap-5', className)}>
+      {pageItems.map(item => (
+        <ResultItem item={item} key={item.id} />
+      ))}
+    </div>
+  )
+}
+
+const ResultsFooter = () => {
+  const allItems = useResultsStore(state => state.allItems)
+
+  if (allItems.length === 0) return null
+
+  return (
+    <div className="mx-auto grid w-full max-w-8xl grid-cols-12 items-center px-8 py-4">
+      <ResultPageOptions className="col-span-3" />
+      <ResultsPaginationNavigation className="col-span-6 col-start-4" />
     </div>
   )
 }
