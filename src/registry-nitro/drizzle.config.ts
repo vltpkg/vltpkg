025231ec4537b813bdb05bd744695fad63a3e245
Luke Karrys <luke@lukekarrys.com>
2025-09-16 14:42:32 -0700

    use nitro's .data dir for tarballs and db

--- src/registry-nitro/drizzle.config.ts
+++ src/registry-nitro/drizzle.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'drizzle-kit'
+import { mkdirSync } from 'node:fs'
+import { resolve } from 'node:path'
+
+mkdirSync(resolve(process.cwd(), '.data'), {
+  recursive: true,
+})
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'sqlite',
   dbCredentials: {
-    url: 'file:db.sqlite',
+    url: 'file:.data/db.sqlite',
   },
 })
