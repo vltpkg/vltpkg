0df9333e99589a5d3626ff3c54c61cf59935ab97
Luke Karrys <luke@lukekarrys.com>
2025-09-23 17:55:28 -0700

    patch nitro for waitUntil cache support

--- src/registry-nitro/server/plugins/logger.ts
+++ src/registry-nitro/server/plugins/logger.ts
@@ -4,4 +4,7 @@ export default defineNitroPlugin(nitro => {
   nitro.hooks.hook('request', event => {
     console.log(event.req.method, event.req.url)
   })
+  nitro.hooks.hook('error', err => {
+    console.log('Nitro error', err)
+  })
 })
