d6dc047e1a7551405aad4992c74692c8a32eaa53
Luke Karrys <luke@lukekarrys.com>
2025-10-09 11:42:14 -0700

    wip: local routes

---
+++ src/registry-nitro/src/handlers/local.ts
@@ -0,0 +1,41 @@
+import { getRouterParam, eventHandler } from 'h3'
+import type { HTTPEvent, EventHandlerRequest } from 'h3'
+import assert from 'node:assert'
+import type { EventHandler } from 'h3'
+
+const assertParam = (
+  event: HTTPEvent<EventHandlerRequest>,
+  name: string,
+) => {
+  const param = getRouterParam(event, name)
+  assert(param, `${name} parameter is required`)
+  return param
+}
+
+const packageOrVersionHandler: EventHandler = async event => {
+  const param1 = assertParam(event, 'param1')
+  const param2 = getRouterParam(event, 'param2')
+  const param3 = getRouterParam(event, 'param3')
+  return {
+    param1,
+    param2,
+    param3,
+  }
+}
+
+export const getPackageOrVersionHandler = eventHandler(
+  packageOrVersionHandler,
+)
+
+const tarballHandler: EventHandler = async event => {
+  const param1 = assertParam(event, 'param1')
+  const param2 = getRouterParam(event, 'param2')
+  const tarball = assertParam(event, 'tarball')
+  return {
+    param1,
+    param2,
+    tarball,
+  }
+}
+
+export const getTarballHandler = eventHandler(tarballHandler)
