905138302d972b5437035b790fdd72142eedc5ff
Luke Karrys <luke@lukekarrys.com>
2025-10-09 11:42:14 -0700

    wip: local routes

--- src/registry-nitro/server/routes/[...].ts
+++ src/registry-nitro/server/routes/[...].ts
@@ -1,16 +1,33 @@
 import { H3 } from 'h3'
-import {
-  getPackageOrVersionHandler,
-  getTarballHandler,
-} from '../../src/handlers/npm.ts'
+import * as npm from '../../src/handlers/npm.ts'
+import * as local from '../../src/handlers/local.ts'
 
 const app = new H3()
   .get('/', () => ({ ok: true }))
+  // npm
   .get('/npm', () => ({ ok: true }))
-  .get('/npm/:param1', getPackageOrVersionHandler)
-  .get('/npm/:param1/:param2', getPackageOrVersionHandler)
-  .get('/npm/:param1/:param2/:param3', getPackageOrVersionHandler)
-  .get('/npm/:param1/-/:tarball', getTarballHandler)
-  .get('/npm/:param1/:param2/-/:tarball', getTarballHandler)
+  .get('/npm/:param1', npm.getPackageOrVersionHandler)
+  .get('/npm/:param1/:param2', npm.getPackageOrVersionHandler)
+  .put('/npm/:param1/:param2', npm.getPackageOrVersionHandler)
+  .get('/npm/:param1/:param2/:param3', npm.getPackageOrVersionHandler)
+  .get('/npm/:param1/-/:tarball', npm.getTarballHandler)
+  .get('/npm/:param1/:param2/-/:tarball', npm.getTarballHandler)
+  // local
+  .get('/local/:param1', local.getPackageOrVersionHandler)
+  .get('/local/:param1/:param2', local.getPackageOrVersionHandler)
+  .put('/local/:param1/:param2', local.getPackageOrVersionHandler)
+  .get(
+    '/local/:param1/:param2/:param3',
+    local.getPackageOrVersionHandler,
+  )
+  .get('/local/:param1/-/:tarball', local.getTarballHandler)
+  .get('/local/:param1/:param2/-/:tarball', local.getTarballHandler)
+  // default (must come last)
+  .get('/:param1', npm.getPackageOrVersionHandler)
+  .get('/:param1/:param2', local.getPackageOrVersionHandler)
+  .put('/:param1/:param2', local.getPackageOrVersionHandler)
+  .get('/:param1/:param2/:param3', local.getPackageOrVersionHandler)
+  .get('/:param1/-/:tarball', local.getTarballHandler)
+  .get('/:param1/:param2/-/:tarball', local.getTarballHandler)
 
 export default app.handler
