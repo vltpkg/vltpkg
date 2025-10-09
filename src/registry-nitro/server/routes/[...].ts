02823b13b0004c81d07ab851f1401498eb0d369d
Luke Karrys <luke@lukekarrys.com>
2025-10-09 16:41:57 -0700

    proxy search for now

--- src/registry-nitro/server/routes/[...].ts
+++ src/registry-nitro/server/routes/[...].ts
@@ -1,11 +1,17 @@
 import { H3 } from 'h3'
 import * as npm from '../../src/handlers/npm.ts'
 import * as local from '../../src/handlers/local.ts'
+// import { clerkClient } from '../../src/auth.ts'
+
+// const auth = {
+//   middleware: [() => {
+
+//   }],
+// }
 
 const app = new H3()
   .get('/', () => ({ ok: true }))
   // npm
-  .get('/npm', () => ({ ok: true }))
   .get('/npm/:param1', npm.getPackageOrVersionHandler)
   .put('/npm/:param1', npm.putPackageHandler)
   .get('/npm/:param1/:param2', npm.getPackageOrVersionHandler)
@@ -13,10 +19,12 @@ const app = new H3()
   .get('/npm/:param1/:param2/:param3', npm.getPackageOrVersionHandler)
   .get('/npm/:param1/-/:tarball', npm.getTarballHandler)
   .get('/npm/:param1/:param2/-/:tarball', npm.getTarballHandler)
+  .get('/npm/-/v1/search', npm.searchHandler)
   // local
   .get('/local/:param1', local.getPackageOrVersionHandler)
+  .put('/local/:param1', local.putPackageHandler)
   .get('/local/:param1/:param2', local.getPackageOrVersionHandler)
-  .put('/local/:param1/:param2', local.getPackageOrVersionHandler)
+  .put('/local/:param1/:param2', local.putPackageHandler)
   .get(
     '/local/:param1/:param2/:param3',
     local.getPackageOrVersionHandler,
@@ -24,9 +32,10 @@ const app = new H3()
   .get('/local/:param1/-/:tarball', local.getTarballHandler)
   .get('/local/:param1/:param2/-/:tarball', local.getTarballHandler)
   // default (must come last)
-  .get('/:param1', npm.getPackageOrVersionHandler)
+  .get('/:param1', local.getPackageOrVersionHandler)
+  .put('/:param1', local.putPackageHandler)
   .get('/:param1/:param2', local.getPackageOrVersionHandler)
-  .put('/:param1/:param2', local.getPackageOrVersionHandler)
+  .put('/:param1/:param2', local.putPackageHandler)
   .get('/:param1/:param2/:param3', local.getPackageOrVersionHandler)
   .get('/:param1/-/:tarball', local.getTarballHandler)
   .get('/:param1/:param2/-/:tarball', local.getTarballHandler)
