b2cf1692adfd968c39a35372ae1e95424c97b2ca
Ruy Adorno <ruy@vlt.sh>
2025-12-03 18:49:01 -0500

    graph: fix workspaces in mermaid output (#1323)

--- src/graph/tap-snapshots/test/visualization/mermaid-output.ts.test.cjs
+++ src/graph/tap-snapshots/test/visualization/mermaid-output.ts.test.cjs
@@ -10,6 +10,9 @@ flowchart TD
 a("root:my-project")
 a -->|"bar#64;^1.0.0 (optional)"| g("npm:bar#64;1.0.0")
 g -->|"baz#64;custom:baz#64;^1.0.0"| m("custom:baz#64;1.0.0")
+a -->|"workspace-b#64;workspace:*"| b("workspace:workspace-b")
+a -->|"workspace-a#64;workspace:*"| c("workspace:workspace-a")
+c -->|"workspace-b#64;workspace:* (dev)"| b
 `
 
 exports[`test/visualization/mermaid-output.ts > TAP > actual graph > should print from an actual loaded graph 1`] = `
@@ -26,8 +29,8 @@ a -->|"#64;scoped/b#64;^1.0.0"| i("npm:#64;scoped/b#64;1.0.0")
 i -->|"#64;scoped/c#64;^1.0.0"| n("npm:#64;scoped/c#64;1.0.0")
 a -->|"#64;scoped/a#64;^1.0.0"| j("npm:#64;scoped/a#64;1.0.0")
 a -->|"missing#64;^1.0.0"| missing-1(Missing)
-b("workspace:workspace-b")
-c("workspace:workspace-a")
+a -->|"workspace-b#64;workspace:*"| b("workspace:workspace-b")
+a -->|"workspace-a#64;workspace:*"| c("workspace:workspace-a")
 c -->|"workspace-b#64;workspace:* (dev)"| b
 c -->|"ipsum#64;^1.0.0 (dev)"| k("npm:ipsum#64;1.0.0")
 c -->|"foo#64;^1.0.0 (dev)"| e
@@ -54,6 +57,15 @@ a -->|"missing#64;^1.0.0"| missing-0(Missing)
 exports[`test/visualization/mermaid-output.ts > TAP > workspaces > should print workspaces mermaid output 1`] = `
 flowchart TD
 a("root:my-project")
-b("workspace:b")
-c("workspace:a")
+a -->|"b#64;workspace:*"| b("workspace:b")
+a -->|"a#64;workspace:*"| c("workspace:a")
+`
+
+exports[`test/visualization/mermaid-output.ts > TAP > workspaces with dependencies > should print workspaces with dependencies 1`] = `
+flowchart TD
+a("root:test-workspaces")
+a -->|"b#64;workspace:*"| b("workspace:b")
+a -->|"a#64;workspace:*"| c("workspace:a")
+c -->|"which#64;^6.0.0"| d("npm:which#64;6.0.0")
+d -->|"isexe#64;^3.1.1"| e("npm:isexe#64;3.1.1")
 `
