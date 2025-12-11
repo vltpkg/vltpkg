336c1237a2913a0aaea26a29cf59a8992c72f59f
Luke Karrys <luke@lukekarrys.com>
2025-12-10 17:32:02 -0700

    remove patch and fix docs entry point locations

--- www/docs/typedoc.workspace.mjs
+++ www/docs/typedoc.workspace.mjs
@@ -35,7 +35,9 @@ export default cwd => {
     readme: join(cwd, './README.md'),
     // get entry points from package.json exports
     entryPoints: Object.values(exports)
+      .map(p => (typeof p === 'string' ? p : p.import.default))
       .filter(p => !p.endsWith('package.json'))
+      .map(p => (console.log(p), p))
       .map(p => join(cwd, p)),
   }
 }
