0f6580e09fa47b718953908792453c1aec1e1566
Jason Korol <ejkorol@gmail.com>
2026-01-06 14:22:46 -0500

    feat: updates some imports for consistency

--- src/gui/src/app/search/search-results.tsx
+++ src/gui/src/app/search/search-results.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useSearchParams } from 'react-router'
-import { SearchResults as SearchResultsView } from '@/components/search/search-results/results.tsx'
+import { SearchResultsProvider as SearchResultsView } from '@/components/search/search-results/results.tsx'
 
 export const SearchResults = () => {
   const [searchParams, setSearchParams] = useSearchParams()
@@ -22,9 +22,5 @@ export const SearchResults = () => {
     }
   }, [query, searchParams, setSearchParams])
 
-  return (
-    <section>
-      <SearchResultsView />
-    </section>
-  )
+  return <SearchResultsView />
 }
