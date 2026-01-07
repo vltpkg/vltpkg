440e5daabd6185f7055805ad7f08aff748542ba4
Luke Karrys <luke@lukekarrys.com>
2026-01-07 13:07:30 -0700

    try and fix windows coverage

--- src/promise-spawn/src/index.ts
+++ src/promise-spawn/src/index.ts
@@ -266,6 +266,7 @@ export class SpawnPromise<
         if (status !== undefined) {
           errorResult.status = status
         }
+        /* c8 ignore next 3 - hard to test on win32 so coverage fails */
         if (signal !== undefined) {
           errorResult.signal = signal
         }
@@ -292,10 +293,10 @@ export class SpawnPromise<
       proc.on('error', reject)
       proc.stdout
         ?.on('data', c => stdout.push(c as Buffer))
-        .on('error', er => reject(er))
+        .on('error', reject)
       proc.stderr
         ?.on('data', c => stderr.push(c as Buffer))
-        .on('error', er => reject(er))
+        .on('error', reject)
       proc.on('close', (status, signal) => {
         if ((status || signal) && !opts.acceptFail) {
           reject(undefined, status, signal)
