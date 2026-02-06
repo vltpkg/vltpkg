fda44a1b5facc29324020a37c431bfd7e48896d5
Luke Karrys <luke@lukekarrys.com>
2026-02-06 15:27:55 -0700

    chore: use vlx in local bins (#1462)

--- scripts/bins/bundle/vlxl.ps1
+++ scripts/bins/bundle/vlxl.ps1
@@ -3,7 +3,7 @@ $RootDir = (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $PSScript
 if (-not (Test-Path "$RootDir\.build-bundle\vlxl.js")) {
   & {
     Set-Location "$RootDir"
-    pnpm vlt-build --bins=vlxl --outdir=".build-bundle" bundle > $null 2>&1
+    vlx vlt-build --bins=vlxl --outdir=".build-bundle" bundle > $null 2>&1
   }
 }
 & {
