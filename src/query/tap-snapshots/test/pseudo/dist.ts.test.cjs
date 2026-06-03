c0f0afa1a55b97ab2f9bee05a51901ee1cc52ec7
darcyclarke <darcy@darcyclarke.me>
2026-06-03 00:44:27 -0400

    feat(query): add :dist selector

---
+++ src/query/tap-snapshots/test/pseudo/dist.ts.test.cjs
@@ -0,0 +1,30 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/pseudo/dist.ts > TAP > :dist pseudo-selector > matches nodes at the beta dist-tag > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "b",
+  ],
+  "nodes": Array [
+    "b",
+  ],
+}
+`
+
+exports[`test/pseudo/dist.ts > TAP > :dist pseudo-selector > matches nodes at the latest dist-tag > must match snapshot 1`] = `
+Object {
+  "edges": Array [
+    "a",
+    "f",
+  ],
+  "nodes": Array [
+    "a",
+    "f",
+  ],
+}
+`
